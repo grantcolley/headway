1994e3be0cd8bf079d9b5ea3e5b12cb9bc0e3e10
grantcolley <grantcolley@yahoo.com>
2021-01-16 21:10:03 +0000

    Add project Headway.Services
    Add project Headway.Services

---
+++ src/Headway.Services/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Headway.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>
+                (await _httpClient.GetStreamAsync($"WeatherForecast"), new JsonSerializerOptions(JsonSerializerDefaults.Web));
+        }
+    }
+}
