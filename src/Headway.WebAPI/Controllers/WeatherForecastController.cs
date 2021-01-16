261b273ed4c485a30ef5b8849ad2780849d884f3
grantcolley <grantcolley@yahoo.com>
2021-01-16 21:09:35 +0000

    Add project Headway.WebAPI
    Add project Headway.WebAPI

---
+++ src/Headway.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Headway.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastRepository _weatherForecastRepository;
+
+        public WeatherForecastController(IWeatherForecastRepository weatherForecastRepository, ILogger<WeatherForecastController> logger)
+        {
+            _weatherForecastRepository = weatherForecastRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _weatherForecastRepository.GetWeatherForecasts();
+        }
+    }
+}
