b205dbdf76fda89f8196e18f4c4d4dba66be8bb6
grantcolley <grantcolley@yahoo.com>
2021-04-05 18:53:26 +0100

    Create the Headway.WebApi project
    Create the Headway.WebApi project

---
+++ src/Headway.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Headway.WebApi.Controllers
+{
+    [ApiController]
+    [EnableCors("local")]
+    [Route("[controller]")]
+    [Authorize(Roles = "weatheruser")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> logger;
+        private readonly IWeatherForecastRepository weatherForecastRepository;
+
+        public WeatherForecastController(
+            IWeatherForecastRepository weatherForecastRepository,
+            ILogger<WeatherForecastController> logger)
+        {
+            this.weatherForecastRepository = weatherForecastRepository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return weatherForecastRepository.GetWeatherForecasts();
+        }
+    }
+}
---
+++ src/Headway.WebApi/Startup.cs
@@ -0,0 +1,75 @@
+using Headway.Core.Interface;
+using Headway.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Headway.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("local",
+                    builder =>
+                        builder.WithOrigins("https://localhost:44300", "https://localhost:44310")
+                               .AllowAnyHeader());
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                    options.Audience = "weatherapi";
+                });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Headway.WebApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Headway.WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("local");
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
