d2a6469c232120b683fe8e6a6d5e36091c8067e8
grantcolley <grantcolley@yahoo.com>
2021-04-05 18:58:28 +0100

    Create the Headway.Services project
    Create the Headway.Services project

---
+++ src/Headway.Services/WeatherForecastService.cs
@@ -0,0 +1,42 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Headway.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly HttpClient httpClient;
+        private readonly TokenProvider tokenProvider;
+        private readonly bool useAccessToken;
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            useAccessToken = false;
+        }
+
+        public WeatherForecastService(HttpClient httpClient, TokenProvider tokenProvider)
+        {
+            this.httpClient = httpClient;
+            this.tokenProvider = tokenProvider;
+            useAccessToken = true;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts()
+        {
+            if (useAccessToken)
+            {
+                var token = tokenProvider.AccessToken;
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            }
+
+            return await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>
+                (await httpClient.GetStreamAsync($"WeatherForecast"),
+                    new JsonSerializerOptions(JsonSerializerDefaults.Web));
+        }
+    }
+}
