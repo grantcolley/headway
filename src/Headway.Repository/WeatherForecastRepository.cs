3e4091c158d1a353d296f322a5fc24be9d97c204
grantcolley <grantcolley@yahoo.com>
2021-04-05 17:19:34 +0100

    Create the Headway.Repository project
    Create the Headway.Repository project

---
+++ src/Headway.Repository/WeatherForecastRepository.cs
@@ -0,0 +1,29 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Headway.Repository
+{
+    public class WeatherForecastRepository : IWeatherForecastRepository
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+            "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> GetWeatherForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
