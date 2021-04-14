da99262acf311fd9e93733b4cfb7ad26d93767a2
grantcolley <grantcolley@yahoo.com>
2021-04-14 20:52:01 +0100

    Create MenuItem, IMenuService, MenuService, and MenuRepository
    Create MenuItem, IMenuService, MenuService, and MenuRepository

---
+++ src/Headway.Services/MenuService.cs
@@ -0,0 +1,42 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Headway.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly HttpClient httpClient;
+        private readonly TokenProvider tokenProvider;
+        private readonly bool useAccessToken;
+
+        public MenuService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            useAccessToken = false;
+        }
+
+        public MenuService(HttpClient httpClient, TokenProvider tokenProvider)
+        {
+            this.httpClient = httpClient;
+            this.tokenProvider = tokenProvider;
+            useAccessToken = true;
+        }
+
+        public async Task<IEnumerable<MenuItem>> GetMenuItemsAsync()
+        {
+            if (useAccessToken)
+            {
+                var token = tokenProvider.AccessToken;
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            }
+
+            return await JsonSerializer.DeserializeAsync<IEnumerable<MenuItem>>
+                (await httpClient.GetStreamAsync($"Menu"),
+                    new JsonSerializerOptions(JsonSerializerDefaults.Web));
+        }
+    }
+}
