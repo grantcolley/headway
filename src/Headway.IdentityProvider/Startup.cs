b6be3ac1a07b28874b598401c2b71c52bf19bfc0
grantcolley <grantcolley@yahoo.com>
2021-04-25 15:18:37 +0100

    Remove google sign-in option
    Remove google sign-in option

--- src/Headway.IdentityProvider/Startup.cs
+++ src/Headway.IdentityProvider/Startup.cs
@@ -58,18 +58,6 @@ namespace Headway.IdentityProvider
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
-
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
         }
 
         public void Configure(IApplicationBuilder app)
