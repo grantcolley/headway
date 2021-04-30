b73bb6ed4efd90e1a61ecab340ab09398b087481
grantcolley <grantcolley@yahoo.com>
2021-04-30 20:33:42 +0100

    Add new Headway.IdentityProvider project
    Add new Headway.IdentityProvider project

---
+++ src/Headway.IdentityProvider/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Headway.IdentityProvider.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Headway.IdentityProvider.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ src/Headway.IdentityProvider/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Headway.IdentityProvider.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
