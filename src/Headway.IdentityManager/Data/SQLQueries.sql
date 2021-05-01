3baaf3a1408682e05498902fd09a54a29fa59dd4
grantcolley <grantcolley@yahoo.com>
2021-05-01 14:20:41 +0100

    Add Headway.IdentityManager
    Add Headway.IdentityManager

---
+++ src/Headway.IdentityManager/Data/SQLQueries.sql
@@ -0,0 +1,17 @@
+﻿--delete from aspnetroles
+--delete from aspnetroleclaims
+--delete from aspnetusers
+--delete from aspnetuserclaims
+--delete from aspnetuserroles
+--delete from aspnetuserlogins
+--delete from aspnetusertokens
+
+select * from aspnetroles
+select * from aspnetroleclaims
+select * from aspnetusers
+select * from aspnetuserclaims
+select * from aspnetuserroles
+select * from aspnetuserlogins
+select * from aspnetusertokens
+
+
