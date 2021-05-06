7eba197fb6e685bd2e033935ef7f61177ab502a2
grantcolley <grantcolley@yahoo.com>
2021-05-06 23:21:58 +0100

    Headway Identity Manager to register and manage users
    Headway Identity Manager to register and manage users

---
+++ src/Headway.IdentityManager/Data/HeadwayUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Headway.IdentityManager.Data
+{
+    public class HeadwayUser
+    {
+        public HeadwayUser(IdentityUser identityUser)
+        {
+            IdentityUser = identityUser;
+        }
+
+        public IdentityUser IdentityUser { get; }
+        public string Roles { get; set; }
+
+        public string Id
+        {
+            get { return IdentityUser?.Id; }
+            set { IdentityUser.Id = value; }
+        }
+
+        public string Email
+        {
+            get { return IdentityUser?.Email; }
+            set { IdentityUser.Email = value; }
+        }
+
+        public string UserName 
+        {
+            get { return IdentityUser?.UserName; }
+            set { IdentityUser.UserName = value; }
+        }
+    }
+}
--- src/Headway.IdentityManager/Startup.cs
+++ src/Headway.IdentityManager/Startup.cs
@@ -40,7 +40,6 @@ namespace Headway.IdentityManager
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
