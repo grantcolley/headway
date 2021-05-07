7a6e01a5a1fefe45d85d91e10f7e0a113d2c3311
grantcolley <grantcolley@yahoo.com>
2021-05-07 19:51:34 +0100

    Remove Sqlite from Headway.IdentityProvider
    Remove Sqlite from Headway.IdentityProvider

--- src/Headway.IdentityProvider/Program.cs
+++ src/Headway.IdentityProvider/Program.cs
@@ -37,24 +37,7 @@ namespace Headway.IdentityProvider
 
             try
             {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
-
                 var host = CreateHostBuilder(args).Build();
-
-                if (seed)
-                {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
-                }
-
                 Log.Information("Starting host...");
                 host.Run();
                 return 0;
