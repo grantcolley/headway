2c2c65b42c21407500834e58e56977025e16d888
grantcolley <grantcolley@yahoo.com>
2021-05-09 18:52:14 +0100

    Create Users.razor and UsersBase
    Create Users.razor and UsersBase

---
+++ src/Headway.RazorComponents/Pages/UsersBase.cs
@@ -0,0 +1,23 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.RazorComponents.Pages
+{
+    public class UsersBase : ComponentBase
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        public IEnumerable<User> Users { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Users = await UserService.GetUsersAsync();
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
