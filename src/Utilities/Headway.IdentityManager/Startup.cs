affb2d0021d54eff9888c5c8fe4391c63ab94d49
grantcolley <grantcolley@yahoo.com>
2021-05-14 20:37:16 +0100

    Move IdentityManager and IdentityProvider to Utilities folder
    Move IdentityManager and IdentityProvider to Utilities folder

--- src/Utilities/Headway.IdentityManager/Startup.cs
+++ src/Utilities/Headway.IdentityManager/Startup.cs
@@ -1,20 +1,13 @@
 using Headway.IdentityManager.Areas.Identity;
 using Headway.IdentityManager.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Headway.IdentityManager
 {
