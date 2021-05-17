9599c79d5c00cefcd20c941c83dbe73af4b4d678
grantcolley <grantcolley@yahoo.com>
2021-05-17 19:03:57 +0100

    Create ApplicationDbContext.cs

---
+++ src/Headway.Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Headway.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Headway.Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+    }
+}
