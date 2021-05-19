a10c4fe285aaa09723749c8a6f2d37fcdb8cec3c
grantcolley <grantcolley@yahoo.com>
2021-05-19 19:25:39 +0100

    Add Headway.MigrationsSqlite
    Add Headway.MigrationsSqlite

---
+++ src/Utilities/Headway.MigrationsSqlite/20210519175812_UpdateHeadway.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.Repository.Migrations
+{
+    public partial class UpdateHeadway : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
