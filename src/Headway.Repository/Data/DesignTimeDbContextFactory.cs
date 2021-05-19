5610731d02e61a97367fa1e6c78d86eae6297294
grantcolley <grantcolley@yahoo.com>
2021-05-19 19:25:11 +0100

    Add Headway.MigrationsSqlServer
    Add Headway.MigrationsSqlServer

--- src/Headway.Repository/Data/DesignTimeDbContextFactory.cs
+++ src/Headway.Repository/Data/DesignTimeDbContextFactory.cs
@@ -11,10 +11,18 @@ namespace Headway.Repository.Data
         {
             IConfigurationRoot configuration
                 = new ConfigurationBuilder().SetBasePath(
-                    Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../../Headway.WebApi/appsettings.json").Build();
+                    Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../Headway.WebApi/appsettings.json").Build();
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-            builder.UseSqlServer(connectionString, x => x.MigrationsAssembly("Headway.DatabaseMigrations"));
+            if(connectionString.Contains("Headway.db"))
+            {
+                builder.UseSqlite(connectionString, x => x.MigrationsAssembly("Headway.MigrationsSqlite"));
+            }
+            else
+            {
+                builder.UseSqlServer(connectionString, x => x.MigrationsAssembly("Headway.MigrationsSqlServer"));
+            }
+
             return new ApplicationDbContext(builder.Options);
         }
     }
