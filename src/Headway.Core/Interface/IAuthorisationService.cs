47cc2b18570a74073f6724438af05545510a3994
grantcolley <grantcolley@yahoo.com>
2021-05-21 10:57:39 +0100

    Update IAuthorisationService.cs

--- src/Headway.Core/Interface/IAuthorisationService.cs
+++ src/Headway.Core/Interface/IAuthorisationService.cs
@@ -10,5 +10,13 @@ namespace Headway.Core.Interface
         Task<User> GetUserAsync(string userName);
         Task<User> SaveUserAsync(User user);
         Task DeleteUserAsync(string userName);
+        Task<IEnumerable<Permission>> GetPermissionsAsync();
+        Task<User> GetPermissionAsync(int permissionId);
+        Task<User> SavePermissionAsync(Permission permission);
+        Task DeletePermissionAsync(int permissionId);
+        Task<IEnumerable<Role>> GetRolesAsync();
+        Task<User> GetRoleAsync(int roleId);
+        Task<User> SaveRoleAsync(Role role);
+        Task DeleteRoleAsync(int roleId);
     }
 }
