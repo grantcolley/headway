27a0af4c540526959f5d8066a3824a4a42a57bc0
grantcolley <grantcolley@yahoo.com>
2021-05-21 21:12:16 +0100

    Create RepositoryBase.cs

---
+++ src/Headway.Repository/RepositoryBase.cs
@@ -0,0 +1,30 @@
+﻿using Headway.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Repository
+{
+    public abstract class RepositoryBase
+    {
+        protected ApplicationDbContext applicationDbContext;
+
+        protected RepositoryBase(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        protected async Task ValidateClaim(string userName, string permission)
+        {
+            var user = await applicationDbContext.Users.FirstOrDefaultAsync(
+                u => u.UserName.Equals(userName)
+                && u.Permissions.Any(p => p.Name.Equals(permission)))
+                .ConfigureAwait(false);
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException(userName);
+            }
+        }
+    }
+}
