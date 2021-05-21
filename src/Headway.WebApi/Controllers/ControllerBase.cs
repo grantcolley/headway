e0cf23e55bc675de1d1118b868081ca7a4755b6f
grantcolley <grantcolley@yahoo.com>
2021-05-21 21:12:21 +0100

    Create ControllerBase.cs

---
+++ src/Headway.WebApi/Controllers/ControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Headway.WebApi.Controllers
+{
+    public abstract class ControllerBase : Controller
+    {
+        protected string GetUserClaim()
+        {
+            var identity = (ClaimsIdentity)HttpContext.User.Identity;
+            var claim = identity.FindFirst(ClaimTypes.Email);
+            return claim.Value;
+        }
+    }
+}
