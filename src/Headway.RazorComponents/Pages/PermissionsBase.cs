6c7bc087ad9c83285226486b048b5126526fd433
grantcolley <grantcolley@yahoo.com>
2021-05-21 10:57:46 +0100

    Update PermissionsBase.cs

--- src/Headway.RazorComponents/Pages/PermissionsBase.cs
+++ src/Headway.RazorComponents/Pages/PermissionsBase.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace Headway.RazorComponents.Pages
 {
     public class PermissionsBase : ComponentBase
     {
+        [Inject]
+        public IAuthorisationService AuthorisationService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
     }
 }
