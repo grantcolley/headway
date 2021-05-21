f1387ebe97e2e8d3234b55386be8c7658643efac
grantcolley <grantcolley@yahoo.com>
2021-05-21 10:57:54 +0100

    Create ServiceBase.cs

---
+++ src/Headway.Services/ServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using Headway.Core.Model;
+using System.Net.Http;
+
+namespace Headway.Services
+{
+    public abstract class ServiceBase
+    {
+        protected readonly HttpClient httpClient;
+        protected readonly TokenProvider tokenProvider;
+        protected readonly bool useAccessToken;
+
+        protected ServiceBase(HttpClient httpClient, bool useAccessToken, TokenProvider tokenProvider)
+            : this(httpClient, useAccessToken)
+        {
+            this.tokenProvider = tokenProvider;
+        }
+
+        protected ServiceBase(HttpClient httpClient, bool useAccessToken)
+        {
+            this.httpClient = httpClient;
+            this.useAccessToken = useAccessToken;
+        }
+
+        protected void AddHttpClientAuthorisationHeader()
+        {
+            if (useAccessToken)
+            {
+                var token = tokenProvider.AccessToken;
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            }
+        }
+    }
+}
