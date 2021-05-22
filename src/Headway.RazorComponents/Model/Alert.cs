5689d8d80b9fdfca422215df9f39d523c6afd61a
grantcolley <grantcolley@yahoo.com>
2021-05-22 21:27:31 +0100

    Create Alert.razor component and Alert.cs
    Create Alert.razor component and Alert.cs

---
+++ src/Headway.RazorComponents/Model/Alert.cs
@@ -0,0 +1,16 @@
+﻿namespace Headway.RazorComponents.Model
+{
+    public class Alert
+    {
+        public string AlertType { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Page
+        {
+            get
+            {
+                return $"/alert/{AlertType ?? string.Empty}/{Title ?? string.Empty}/{Message ?? string.Empty}";
+            }
+        }
+    }
+}
