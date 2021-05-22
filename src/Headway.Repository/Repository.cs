47cd3cb4902bc718e44012b3b5d3bfe93e503c4f
grantcolley <grantcolley@yahoo.com>
2021-05-22 19:48:55 +0100

    Create Repository.cs

---
+++ src/Headway.Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Headway.Core.Interface;
+using Headway.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Repository
+{
+    public abstract class Repository : IRepository
+    {
+        protected ApplicationDbContext applicationDbContext;
+
+        protected Repository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<bool> IsAuthorisedAsync(string userName, string permission)
+        {
+            return await applicationDbContext.Users.AnyAsync(
+                u => u.UserName.Equals(userName)
+                && u.Permissions.Any(p => p.Name.Equals(permission)))
+                .ConfigureAwait(false);
+        }
+    }
+}
