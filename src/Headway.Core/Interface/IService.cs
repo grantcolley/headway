3360b6594217ae21b244b30e82b2de86ae1298f5
grantcolley <grantcolley@yahoo.com>
2021-05-22 19:47:56 +0100

    Create IService.cs

---
+++ src/Headway.Core/Interface/IService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+
+namespace Headway.Core.Interface
+{
+    public interface IService
+    {
+        void AddHttpClientAuthorisationHeader();
+        bool IsSuccessStatusCode(HttpResponseMessage httpResponseMessage);
+    }
+}
