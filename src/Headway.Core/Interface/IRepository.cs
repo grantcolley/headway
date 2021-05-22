fbacbe664accf12c7d9a062a2aebad273230c848
grantcolley <grantcolley@yahoo.com>
2021-05-22 19:47:53 +0100

    Create IRepository.cs

---
+++ src/Headway.Core/Interface/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IRepository
+    {
+        Task<bool> IsAuthorisedAsync(string userName, string permission);
+    }
+}
