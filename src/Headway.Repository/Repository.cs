2ebaa79ae2840e6d3f8dde03f0b06f145389e8fa
grantcolley <grantcolley@yahoo.com>
2021-05-23 20:26:32 +0100

    Update Repository.cs

--- src/Headway.Repository/Repository.cs
+++ src/Headway.Repository/Repository.cs
@@ -15,10 +15,10 @@ namespace Headway.Repository
             this.applicationDbContext = applicationDbContext;
         }
 
-        public async Task<bool> IsAuthorisedAsync(string userName, string permission)
+        public async Task<bool> IsAuthorisedAsync(string claim, string permission)
         {
             return await applicationDbContext.Users.AnyAsync(
-                u => u.UserName.Equals(userName)
+                u => u.Email.Equals(claim)
                 && u.Permissions.Any(p => p.Name.Equals(permission)))
                 .ConfigureAwait(false);
         }
