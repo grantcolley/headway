6a00866317cbf50581b7d71d81a39a77c5d3bc90
grantcolley <grantcolley@yahoo.com>
2021-05-23 14:18:45 +0100

    Update Permission.cs

--- src/Headway.Core/Model/Permission.cs
+++ src/Headway.Core/Model/Permission.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Headway.Core.Model
 {
@@ -11,8 +12,11 @@ namespace Headway.Core.Model
         }
 
         public int PermissionId { get; set; }
-        public string Name { get; set; }
         public List<User> Users { get; set; }
         public List<Role> Roles { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Name between 1 and 20 characters")]
+        public string Name { get; set; }
     }
 }
