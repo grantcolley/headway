5889ca2c0a5fcc47794959dacd31d452c8a86fac
grantcolley <grantcolley@yahoo.com>
2021-05-24 17:54:52 +0100

    Update UsersController.cs

--- src/Headway.WebApi/Controllers/UsersController.cs
+++ src/Headway.WebApi/Controllers/UsersController.cs
@@ -12,93 +12,96 @@ namespace Headway.WebApi.Controllers
     [EnableCors("local")]
     [Route("[controller]")]
     [Authorize(Roles = "headwayuser")]
-    public class UsersController : ControllerBase
+    public class UsersController : ApiControllerBase<UsersController>
     {
-        private readonly ILogger<UsersController> logger;
         private readonly IAuthorisationRepository authorisationRepository;
 
         public UsersController(
             IAuthorisationRepository authorisationRepository,
             ILogger<UsersController> logger)
+            : base(logger)
         {
             this.authorisationRepository = authorisationRepository;
-            this.logger = logger;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> Get()
         {
-            var claim = GetUserClaim();
-
-            if (await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
-                                                .ConfigureAwait(false))
-            {
-                var users = await authorisationRepository
-                                    .GetUsersAsync(claim)
-                                    .ConfigureAwait(false);
-                return Ok(users);
-            }
-            else
+            var authorised = await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
+                                                            .ConfigureAwait(false);
+            if (!authorised)
             {
                 return Unauthorized();
             }
+
+            var users = await authorisationRepository
+                                .GetUsersAsync(claim)
+                                .ConfigureAwait(false);
+            return Ok(users);
         }
 
         [HttpGet("{userId}")]
-        public async Task<IActionResult> GetUser(int userId)
+        public async Task<IActionResult> Get(int userId)
         {
-            var claim = GetUserClaim();
-
-            if (await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
-                                                .ConfigureAwait(false))
-            {
-                var user = await authorisationRepository
-                                    .GetUserAsync(claim, userId)
-                                    .ConfigureAwait(false);
-                return Ok(user);
-            }
-            else
+            var authorised = await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
+                                                            .ConfigureAwait(false);
+            if (!authorised)
             {
                 return Unauthorized();
             }
+
+            var user = await authorisationRepository
+                                .GetUserAsync(claim, userId)
+                                .ConfigureAwait(false);
+            return Ok(user);
         }
 
-        [HttpPost("{user}")]
-        public async Task<IActionResult> SaveUser(User user)
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
         {
-            var claim = GetUserClaim();
-
-            if (await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
-                                                .ConfigureAwait(false))
-            {
-                var savedUser = await authorisationRepository
-                                        .SaveUserAsync(claim, user)
-                                        .ConfigureAwait(false);
-                return Ok(savedUser);
-            }
-            else
+            var authorised = await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
+                                                            .ConfigureAwait(false);
+            if (!authorised)
             {
                 return Unauthorized();
             }
+
+            var savedUser = await authorisationRepository
+                                        .AddUserAsync(claim, user)
+                                        .ConfigureAwait(false);
+            return Ok(savedUser);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] User user)
+        {
+            var authorised = await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
+                                                            .ConfigureAwait(false);
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var savedUser = await authorisationRepository
+                                    .UpdateUserAsync(claim, user)
+                                    .ConfigureAwait(false);
+            return Ok(savedUser);
         }
 
         [HttpDelete("{userId}")]
         public async Task<IActionResult> DeleteUser(int userId)
         {
-            var claim = GetUserClaim();
-
-            if (await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
-                                                .ConfigureAwait(false))
-            {
-                var result = await authorisationRepository
-                                    .DeleteUserAsync(claim, userId)
-                                    .ConfigureAwait(false);
-                return Ok(result);
-            }
-            else
+            var authorised = await authorisationRepository.IsAuthorisedAsync(claim, "Admin")
+                                                            .ConfigureAwait(false);
+            if (!authorised)
             {
                 return Unauthorized();
             }
+
+            var result = await authorisationRepository
+                                    .DeleteUserAsync(claim, userId)
+                                    .ConfigureAwait(false);
+            return Ok(result);
         }
     }
 }
