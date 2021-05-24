128d79301b8f4cd49248a4f6a257847c5ea91a87
grantcolley <grantcolley@yahoo.com>
2021-05-24 17:54:25 +0100

    Create ApiControllerBase.cs

---
+++ src/Headway.WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace Headway.WebApi.Controllers
+{
+    [ApiController]
+    public abstract class ApiControllerBase<T> : ControllerBase
+    {
+        protected readonly ILogger<T> logger;
+        protected readonly string claim;
+
+        protected ApiControllerBase(ILogger<T> logger)
+        {
+            this.logger = logger;
+            claim = GetUserClaim();
+        }
+
+        protected string GetUserClaim()
+        {
+            var identity = (ClaimsIdentity)HttpContext.User.Identity;
+            var claim = identity.FindFirst(ClaimTypes.Email);
+            return claim.Value;
+        }
+    }
+}
