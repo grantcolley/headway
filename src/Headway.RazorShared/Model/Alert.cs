e4b9fa2deea7025a0d577fb0832ffe7dc5106c32
grantcolley <grantcolley@yahoo.com>
2021-05-25 20:07:12 +0100

    Support redirection from an alert
    Support redirection from an alert

--- src/Headway.RazorShared/Model/Alert.cs
+++ src/Headway.RazorShared/Model/Alert.cs
@@ -5,11 +5,13 @@
         public string AlertType { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
+        public string RedirectText { get; set; }
+        public string RedirectPage { get; set; }
         public string Page
         {
             get
             {
-                return $"/alert/{AlertType ?? string.Empty}/{Title ?? string.Empty}/{Message ?? string.Empty}";
+                return $"/alert/{AlertType ?? string.Empty}/{Title ?? string.Empty}/{Message ?? string.Empty}/{RedirectText ?? string.Empty}/{RedirectPage ?? string.Empty}";
             }
         }
     }
