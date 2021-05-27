1e1533bacdb84e2bbe3a4a7392c59ce84a22c660
grantcolley <grantcolley@yahoo.com>
2021-05-27 12:38:07 +0100

    Create RoleDetails component and RoleDetailsBase base class
    Create RoleDetails component and RoleDetailsBase base class

---
+++ src/Headway.RazorAdmin/Pages/RoleDetailsBase.cs
@@ -0,0 +1,90 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Headway.RazorShared.Model;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Headway.RazorAdmin.Pages
+{
+    public class RoleDetailsBase : ComponentBase
+    {
+        [Inject]
+        public IAuthorisationService AuthorisationService { get; set; }
+
+        [Parameter]
+        public int RoleId { get; set; }
+
+        protected Role role { get; set; }
+
+        protected Alert alert { get; set; }
+
+        protected bool IsSaveInProgress = false;
+        protected bool IsDeleteInProgress = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (RoleId.Equals(0))
+            {
+                role = new Role();
+            }
+            else
+            {
+                role = await AuthorisationService.GetRoleAsync(RoleId).ConfigureAwait(false);
+            }
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected async Task SubmitRole()
+        {
+            IsSaveInProgress = true;
+
+            if (role.RoleId.Equals(0))
+            {
+                role = await AuthorisationService.AddRoleAsync(role).ConfigureAwait(false);
+
+                alert = new Alert
+                {
+                    AlertType = "info",
+                    Title = $"{role.Name}",
+                    Message = $"has been added.",
+                    RedirectText = "Roles",
+                    RedirectPage = "/roles"
+                };
+            }
+            else
+            {
+                role = await AuthorisationService.UpdateRoleAsync(role).ConfigureAwait(false);
+
+                alert = new Alert
+                {
+                    AlertType = "info",
+                    Title = $"{role.Name}",
+                    Message = $"has been updated.",
+                    RedirectText = "Roles",
+                    RedirectPage = "/roles"
+                };
+            }
+
+            IsSaveInProgress = false;
+        }
+
+        public async Task DeleteRole(Role role)
+        {
+            IsDeleteInProgress = true;
+
+            await AuthorisationService.DeleteRoleAsync(role.RoleId).ConfigureAwait(false);
+
+            alert = new Alert
+            {
+                AlertType = "info",
+                Title = $"{role.Name}",
+                Message = $"has been deleted.",
+                RedirectText = "Roles",
+                RedirectPage = "/roles"
+            };
+
+            IsDeleteInProgress = false;
+        }
+    }
+}
