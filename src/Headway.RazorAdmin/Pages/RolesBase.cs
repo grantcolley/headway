f6eaed901f59f1aa64e487e899e0652fbefcc612
grantcolley <grantcolley@yahoo.com>
2021-05-27 12:37:43 +0100

    Create Roles component and RolesBase base class
    Create Roles component and RolesBase base class

--- src/Headway.RazorAdmin/Pages/RolesBase.cs
+++ src/Headway.RazorAdmin/Pages/RolesBase.cs
@@ -1,8 +1,36 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Headway.RazorAdmin.Pages
 {
     public class RolesBase : ComponentBase
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IAuthorisationService AuthorisationService { get; set; }
+
+        public IEnumerable<Role> Roles { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Roles = await AuthorisationService.GetRolesAsync().ConfigureAwait(false);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected void AddRole()
+        {
+            NavigationManager.NavigateTo("/roledetails");
+        }
+
+        protected void UpdateRole(int roleId)
+        {
+            NavigationManager.NavigateTo($"/roledetails/{roleId}");
+        }
     }
 }
