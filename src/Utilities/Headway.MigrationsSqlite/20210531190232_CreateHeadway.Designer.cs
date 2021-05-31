984a209e8c9af5433a69aa2362f507cd2ee9ad56
grantcolley <grantcolley@yahoo.com>
2021-05-31 20:25:28 +0100

    Add new migrations for Sqlite
    Add new migrations for Sqlite

--- src/Utilities/Headway.MigrationsSqlite/20210531190232_CreateHeadway.Designer.cs
+++ src/Utilities/Headway.MigrationsSqlite/20210531190232_CreateHeadway.Designer.cs
@@ -2,14 +2,16 @@
 using Headway.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Headway.MigrationsSqlite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210531190232_CreateHeadway")]
+    partial class CreateHeadway
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
