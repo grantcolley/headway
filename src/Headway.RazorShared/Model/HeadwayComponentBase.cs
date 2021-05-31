5a5dd0b39c3129573e79effa069e69109cdd277e
grantcolley <grantcolley@yahoo.com>
2021-05-31 10:53:28 +0100

    Create HeadwayComponentBase
    Create HeadwayComponentBase

---
+++ src/Headway.RazorShared/Model/HeadwayComponentBase.cs
@@ -0,0 +1,31 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.RazorShared.Model
+{
+    public class HeadwayComponentBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected T GetResponse<T>(IServiceResult<T> result)
+        {
+            if (!result.IsSuccess)
+            {
+                var alert = new Alert
+                {
+                    AlertType = "danger",
+                    Title = "Error",
+                    Message = result.Message
+                };
+
+                NavigationManager.NavigateTo(alert.Page);
+                return default(T);
+            }
+            else
+            {
+                return result.Result;
+            }
+        }
+    }
+}
