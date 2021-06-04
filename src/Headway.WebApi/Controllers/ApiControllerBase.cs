7e187ce60243c7ed1a1826e22e0a556bd8c7d716
grantcolley <grantcolley@yahoo.com>
2021-06-04 21:14:10 +0100

    Update ApiControllerBase.cs

--- src/Headway.WebApi/Controllers/ApiControllerBase.cs
+++ src/Headway.WebApi/Controllers/ApiControllerBase.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Headway.WebApi.Controllers
 {
     [ApiController]
     public abstract class ApiControllerBase<T> : ControllerBase
     {
+        protected readonly IRepository repository;
         protected readonly ILogger<T> logger;
 
-        protected ApiControllerBase(ILogger<T> logger)
+        protected ApiControllerBase(IRepository repository, ILogger<T> logger)
         {
+            this.repository = repository;
             this.logger = logger;
         }
 
@@ -19,6 +23,15 @@ namespace Headway.WebApi.Controllers
             var identity = (ClaimsIdentity)HttpContext.User.Identity;
             var claim = identity.FindFirst(ClaimTypes.Email);
             return claim.Value;
+        }
+
+        protected async Task<bool> IsAuthorisedAsync(string permission)
+        {
+            var claim = GetUserClaim();
+
+            return await repository
+                .IsAuthorisedAsync(claim, permission)
+                .ConfigureAwait(false);
         }
     }
 }
