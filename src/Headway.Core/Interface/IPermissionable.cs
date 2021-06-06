f2f564794ad728927740cc8e429d1c3389e07032
grantcolley <grantcolley@yahoo.com>
2021-06-06 20:38:51 +0100

    Create IPermissionable.cs

---
+++ src/Headway.Core/Interface/IPermissionable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Interface
+{
+    public interface IPermissionable
+    {
+        bool IsPermitted(IEnumerable<string> permissions);
+    }
+}
