6297c3c188faaf72ea6bd50a47fe323e8eff6068
grantcolley <grantcolley@yahoo.com>
2021-06-08 20:31:00 +0100

    Update Permission.cs

--- src/Headway.Core/Model/Permission.cs
+++ src/Headway.Core/Model/Permission.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Headway.Core.Attributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Headway.Core.Model
@@ -11,14 +12,18 @@ namespace Headway.Core.Model
             Roles = new List<Role>();
         }
 
-        public int PermissionId { get; set; }
         public List<User> Users { get; set; }
         public List<Role> Roles { get; set; }
 
+        [DynamicProperty]
+        public int PermissionId { get; set; }
+
+        [DynamicProperty]
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(10, ErrorMessage = "Name must be between 1 and 10 characters")]
         public string Name { get; set; }
 
+        [DynamicProperty]
         [Required(ErrorMessage = "Description is required.")]
         [StringLength(20, ErrorMessage = "Description must be between 1 and 20 characters")]
         public string Description { get; set; }
