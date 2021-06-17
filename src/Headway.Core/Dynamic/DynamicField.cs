c6b6a34f33a6255e9a31dbeb629d74204934dbb2
grantcolley <grantcolley@yahoo.com>
2021-06-17 20:01:58 +0100

    Create DynamicField.cs

---
+++ src/Headway.Core/Dynamic/DynamicField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Headway.Core.Dynamic
+{
+    public class DynamicField
+    {
+        public object Model { get; set; }
+        public int Order { get; set; }
+        public string PropertyName { get; set; }
+        public PropertyInfo PropertyInfo { get; set; }
+        public string DynamicComponentTypeName { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+        public Type DynamicComponent { get; set; }
+    }
+}
