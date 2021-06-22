f10c0268d7e3a9dcf269bb2fe29d2bb6c149acdb
grantcolley <grantcolley@yahoo.com>
2021-06-22 14:24:54 +0100

    Create Details.razor component and base class

---
+++ src/Headway.RazorAdmin/Pages/Details.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Headway.RazorAdmin.Pages
+{
+    public partial class DetailsBase : ComponentBase
+    {
+        [Parameter]
+        public string TypeName { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected Type DetailsType
+        {
+            get { return Type.GetType(TypeName); }
+        }
+    }
+}
