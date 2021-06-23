1747d2c2100116c1f3f4c05a56a47ac40a45d13d
grantcolley <grantcolley@yahoo.com>
2021-06-23 09:40:13 +0100

    Create IDynamicConfigService and DynamicConfigService
    Create IDynamicConfigService and DynamicConfigService

---
+++ src/Headway.Core/Interface/IDynamicConfigService.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Dynamic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IDynamicConfigService
+    {
+        Task<IServiceResult<DynamicModelConfig>> GetDynamicModelConfigAsync<T>();
+    }
+}
---
+++ src/Headway.Services/DynamicConfigService.cs
@@ -0,0 +1,28 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Headway.Services
+{
+    public class DynamicConfigService : ServiceBase, IDynamicConfigService
+    {
+        public DynamicConfigService(HttpClient httpClient)
+            : base(httpClient, false)
+        {
+        }
+
+        public DynamicConfigService(HttpClient httpClient, TokenProvider tokenProvider)
+            : base(httpClient, true, tokenProvider)
+        {
+        }
+
+        public async Task<IServiceResult<DynamicModelConfig>> GetDynamicModelConfigAsync<T>()
+        {
+            var model = typeof(T).Name;
+            var httpResponseMessage = await httpClient.GetAsync($"DynamicConfig/{model}").ConfigureAwait(false);
+            return await GetServiceResult<DynamicModelConfig>(httpResponseMessage);
+        }
+    }
+}
