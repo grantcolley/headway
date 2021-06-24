166aa879d649f323445b94cef1df030ca6631465
grantcolley <grantcolley@yahoo.com>
2021-06-24 20:15:59 +0100

    Update DynamicFieldConfig.cs

--- src/Headway.Core/Dynamic/DynamicFieldConfig.cs
+++ src/Headway.Core/Dynamic/DynamicFieldConfig.cs
@@ -3,8 +3,9 @@
     public class DynamicFieldConfig
     {
         public int Id { get; set; }
-        public string PropertyName { get; set; }
         public int Order { get; set; }
+        public bool IsTitleField { get; set; }
+        public string PropertyName { get; set; }
         public string DynamicComponentTypeName { get; set; }
     }
 }
