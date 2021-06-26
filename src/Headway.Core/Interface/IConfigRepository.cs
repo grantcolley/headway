848f15b8abdce627becae510fd065bcc95d29fe9
grantcolley <grantcolley@yahoo.com>
2021-06-26 10:20:47 +0100

    Create IConfigRepository.cs

---
+++ src/Headway.Core/Interface/IConfigRepository.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Model;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IConfigRepository : IRepository
+    {
+        Task<ModelConfig> GetModelConfigAsync(string model);
+    }
+}
