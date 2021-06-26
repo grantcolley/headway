8d67d6a6143c85cf678292aff1f7ad1c457c2eff
grantcolley <grantcolley@yahoo.com>
2021-06-26 10:20:42 +0100

    Create IConfigService.cs

---
+++ src/Headway.Core/Interface/IConfigService.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IConfigService
+    {
+        Task<IServiceResult<ModelConfig>> GetModelConfigAsync<T>(HttpClient httpClient, TokenProvider tokenProvider);
+    }
+}
