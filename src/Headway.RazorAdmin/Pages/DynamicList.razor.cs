b777d3fda14cc0a768162b31b9ad6188c92ec685
grantcolley <grantcolley@yahoo.com>
2021-06-28 19:00:23 +0100

    Create DynamicList.razor.cs

---
+++ src/Headway.RazorAdmin/Pages/DynamicList.razor.cs
@@ -0,0 +1,38 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Headway.RazorShared.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.RazorAdmin.Pages
+{
+    public partial class DynamicListBase<T> : HeadwayComponentBase
+    {
+        [Inject]
+        public IAuthorisationService AuthorisationService { get; set; }
+
+        [Parameter]
+        public string DetailsType { get; set; }
+
+        protected IEnumerable<DynamicModel<T>> DynamicModels;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //var result = await AuthorisationService.GetPermissionsAsync().ConfigureAwait(false);
+            //Permissions = GetResponse(result);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected void AddPermission()
+        {
+            NavigationManager.NavigateTo($"/details/{DetailsType}");
+        }
+
+        protected void UpdatePermission(int permissionId)
+        {
+            NavigationManager.NavigateTo($"/details/{DetailsType}/{permissionId}");
+        }
+    }
+}
