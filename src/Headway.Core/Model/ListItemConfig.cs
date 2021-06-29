7eb7db3214827fdaf8c28e7b7e61ce3956cbc2c5
grantcolley <grantcolley@yahoo.com>
2021-06-29 19:20:47 +0100

    Create ListItemConfig.cs

---
+++ src/Headway.Core/Model/ListItemConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Headway.Core.Model
+{
+    public class ListItemConfig
+    {
+        public int ListItemConfigId { get; set; }
+        public string PropertyName { get; set; }
+        public string HeaderName { get; set; }
+        public bool IsArgument { get; set; }
+    }
+}
