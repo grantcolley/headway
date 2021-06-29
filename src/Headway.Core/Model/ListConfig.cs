e14ee011c1e47cc70cfb6d78fae8f1e41697cb8d
grantcolley <grantcolley@yahoo.com>
2021-06-29 19:20:50 +0100

    Create ListConfig.cs

---
+++ src/Headway.Core/Model/ListConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class ListConfig
+    {
+        public ListConfig()
+        {
+            ListItemConfigs = new List<ListItemConfig>();
+        }
+
+        public int ListConfigId { get; set; }
+        public string ListName { get; set; }
+        public string ConfigApiPath { get; set; }
+        public string DynamicComponentTypeName { get; set; }
+        public List<ListItemConfig> ListItemConfigs { get; set; }
+    }
+}
