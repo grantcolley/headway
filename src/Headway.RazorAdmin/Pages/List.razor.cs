0b6798d9ceceb8f0e69e7cd85708d970c3aa3121
grantcolley <grantcolley@yahoo.com>
2021-06-30 18:49:49 +0100

    Create List.razor.cs

---
+++ src/Headway.RazorAdmin/Pages/List.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Headway.RazorAdmin.Pages
+{
+    public partial class ListBase : ComponentBase
+    {
+        [Parameter]
+        public string TypeName { get; set; }
+
+        protected Type ListType
+        {
+            get { return Type.GetType(TypeName); }
+        }
+    }
+}
