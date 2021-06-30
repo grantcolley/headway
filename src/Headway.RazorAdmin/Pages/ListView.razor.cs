828697cb54cb7b40c17c28fba6d4860f5619baca
grantcolley <grantcolley@yahoo.com>
2021-06-30 18:49:54 +0100

    Create ListView.razor.cs

---
+++ src/Headway.RazorAdmin/Pages/ListView.razor.cs
@@ -0,0 +1,41 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using Headway.RazorShared.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.RazorAdmin.Pages
+{
+    public partial class ListViewBase<T> : HeadwayComponentBase
+    {
+        [Inject]
+        public IAuthorisationService AuthorisationService { get; set; }
+
+        [Parameter]
+        public string DetailsType { get; set; }
+
+        protected DynamicList<T> DynamicList;
+
+        protected List<ListItemConfig> ListItemConfigs;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //var result = await AuthorisationService.GetPermissionsAsync().ConfigureAwait(false);
+            //Permissions = GetResponse(result);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected void Add()
+        {
+            NavigationManager.NavigateTo($"/details/{DetailsType}");
+        }
+
+        protected void Update(object id)
+        {
+            NavigationManager.NavigateTo($"/details/{DetailsType}/{id}");
+        }
+    }
+}
