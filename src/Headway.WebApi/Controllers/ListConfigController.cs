4e7ba60af71e360bc616cec8ab7a404184dd19cd
grantcolley <grantcolley@yahoo.com>
2021-07-01 20:39:49 +0100

    Create ListConfigController.cs

---
+++ src/Headway.WebApi/Controllers/ListConfigController.cs
@@ -0,0 +1,44 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    [ApiController]
+    [EnableCors("local")]
+    [Route("[controller]")]
+    [Authorize(Roles = "headwayuser")]
+    public class ListConfigController : ApiControllerBase<ModelConfigController>
+    {
+        private readonly IConfigRepository configRepository;
+
+        public ListConfigController(
+            IConfigRepository configRepository,
+            ILogger<ModelConfigController> logger)
+            : base(configRepository, logger)
+        {
+            this.configRepository = configRepository;
+        }
+
+        [HttpGet("{model}")]
+        public async Task<IActionResult> Get(string model)
+        {
+            var authorised = await IsAuthorisedAsync("Admin")
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var config = await configRepository
+                .GetListConfigAsync(model)
+                .ConfigureAwait(false);
+
+            return Ok(config);
+        }
+    }
+}
