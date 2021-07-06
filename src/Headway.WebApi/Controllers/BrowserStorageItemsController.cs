73a9b3a7e187364e2ee6a1eb47efda76fe8d1184
grantcolley <grantcolley@yahoo.com>
2021-07-06 19:16:56 +0100

    Create BrowserStorageItemsController.cs

---
+++ src/Headway.WebApi/Controllers/BrowserStorageItemsController.cs
@@ -0,0 +1,47 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    [ApiController]
+    [EnableCors("local")]
+    [Route("[controller]")]
+    [Authorize(Roles = "headwayuser")]
+    public class BrowserStorageItemsController : ApiControllerBase<BrowserStorageItemsController>
+    {
+        private readonly IBrowserStorageRepository browserStorageRepository;
+
+        public BrowserStorageItemsController(
+            IBrowserStorageRepository browserStorageRepository,
+            ILogger<BrowserStorageItemsController> logger)
+            : base(browserStorageRepository, logger)
+        {
+            this.browserStorageRepository = browserStorageRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var authorised = await IsAuthorisedAsync("User")
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var browserStorageItems = await browserStorageRepository
+                .GetBrowserStorageItemsAsync()
+                .ConfigureAwait(false);
+
+            return Ok(browserStorageItems);
+        }
+    }
+}
