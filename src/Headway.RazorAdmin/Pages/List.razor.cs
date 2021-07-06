9dbc39dc7a15599f1862bb20631d36b8f6cd437b
grantcolley <grantcolley@yahoo.com>
2021-07-06 19:17:12 +0100

    Update List.razor.cs

--- src/Headway.RazorAdmin/Pages/List.razor.cs
+++ src/Headway.RazorAdmin/Pages/List.razor.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.RazorShared.Base;
+using Microsoft.AspNetCore.Components;
 using System;
+using System.Threading.Tasks;
 
 namespace Headway.RazorAdmin.Pages
 {
-    public partial class ListBase : ComponentBase
+    public partial class ListBase : DynamicTypeComponentBase
     {
         [Parameter]
         public string TypeName { get; set; }
 
+        protected string typeFullName;
+
+        protected override async Task OnInitializedAsync()
+        {
+            typeFullName = await GetTypeFullName(TypeName).ConfigureAwait(false);
+            await base.OnInitializedAsync();
+        }
+
         protected RenderFragment RenderListView() => __builder =>
         {
-            var type = Type.GetType("Headway.Core.Model.Permission, Headway.Core");
+            var type = Type.GetType(typeFullName);
             var genericType = typeof(ListView<>).MakeGenericType(new[] { type });
             __builder.OpenComponent(1, genericType);
             __builder.AddAttribute(2, "TypeName", TypeName);
