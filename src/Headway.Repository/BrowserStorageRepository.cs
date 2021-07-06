bd409d7c14d5925cc0584a8066ec792b8a781009
grantcolley <grantcolley@yahoo.com>
2021-07-06 19:16:20 +0100

    Create BrowserStorageRepository.cs

---
+++ src/Headway.Repository/BrowserStorageRepository.cs
@@ -0,0 +1,31 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Headway.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Repository
+{
+    public class BrowserStorageRepository : RepositoryBase, IBrowserStorageRepository
+    {
+        public BrowserStorageRepository(ApplicationDbContext applicationDbContext)
+            : base(applicationDbContext)
+        {
+        }
+
+        public Task<IEnumerable<BrowserStorageItem>> GetBrowserStorageItemsAsync()
+        {
+            //return await applicationDbContext.BrowserStorageItems
+            //    .ToListAsync()
+            //    .ConfigureAwait(false);
+
+            var browserStorageItems = new List<BrowserStorageItem>
+            {
+                { new BrowserStorageItem { Key = "Permission", Value = "Headway.Core.Model.Permission, Headway.Core" } }
+            };
+
+            return Task.FromResult((IEnumerable<BrowserStorageItem>)browserStorageItems);
+        }
+    }
+}
