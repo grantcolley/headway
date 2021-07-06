702657c94de3ac503811a2867ba1357f1570ed99
grantcolley <grantcolley@yahoo.com>
2021-07-06 19:16:40 +0100

    Create BrowserStorageService.cs

---
+++ src/Headway.Services/BrowserStorageService.cs
@@ -0,0 +1,27 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Headway.Services
+{
+    public class BrowserStorageService : ServiceBase, IBrowserStorageService
+    {
+        public BrowserStorageService(HttpClient httpClient)
+            : base(httpClient, false)
+        {
+        }
+
+        public BrowserStorageService(HttpClient httpClient, TokenProvider tokenProvider)
+            : base(httpClient, true, tokenProvider)
+        {
+        }
+
+        public async Task<IServiceResult<IEnumerable<BrowserStorageItem>>> GetBrowserStorageItemsAsync()
+        {
+            var httpResponseMessage = await httpClient.GetAsync($"BrowserStorageItems").ConfigureAwait(false);
+            return await GetServiceResult<IEnumerable<BrowserStorageItem>>(httpResponseMessage);
+        }
+    }
+}
