3697d55aea6b11f9f0b43db535528d381b2c0a3f
grantcolley <grantcolley@yahoo.com>
2021-07-06 19:17:06 +0100

    Create DynamicTypeComponentBase.cs

---
+++ src/Headway.RazorShared/Base/DynamicTypeComponentBase.cs
@@ -0,0 +1,48 @@
+﻿using Headway.Core.Interface;
+using Headway.RazorShared.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Headway.RazorShared.Base
+{
+    public abstract class DynamicTypeComponentBase : HeadwayComponentBase
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IBrowserStorageService BrowserStorageService { get; set; }
+
+        public async Task<string> GetTypeFullName(string typeName)
+        {
+            var browserStorageItem = await JSRuntime.InvokeAsync<string>("localStorage.getItem", typeName).ConfigureAwait(false);
+
+            if (string.IsNullOrWhiteSpace(browserStorageItem))
+            {
+                var serviceResult = await BrowserStorageService.GetBrowserStorageItemsAsync().ConfigureAwait(false);
+
+                var browserStorageItems = GetResponse(serviceResult);
+
+                foreach(var storageItem in browserStorageItems)
+                {
+                    await JSRuntime.InvokeVoidAsync("localStorage.setItem", storageItem.Key, storageItem.Value);
+                }
+
+                browserStorageItem = await JSRuntime.InvokeAsync<string>("localStorage.getItem", typeName).ConfigureAwait(false);
+            }
+
+            if(string.IsNullOrWhiteSpace(browserStorageItem))
+            {
+                RaiseAlert($"Failed to map {typeName} to a fully qualified type name.");
+                return default;
+            }
+
+            return browserStorageItem;
+        }
+    }
+}
