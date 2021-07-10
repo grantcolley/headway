e6f57ec63e02a379cf6544d5261eaad0bd5408e6
grantcolley <grantcolley@yahoo.com>
2021-07-10 20:25:57 +0100

    Fix sequence in __builder.AddAttribute(3, "Id", Id)
    Fix sequence in __builder.AddAttribute(3, "Id", Id)

--- src/Headway.RazorShared/Pages/Model.razor.cs
+++ src/Headway.RazorShared/Pages/Model.razor.cs
@@ -30,7 +30,7 @@ namespace Headway.RazorShared.Pages
             var genericType = typeof(ModelView<>).MakeGenericType(new[] { type });
             __builder.OpenComponent(1, genericType);
             __builder.AddAttribute(2, "TypeName", TypeName);
-            __builder.AddAttribute(2, "Id", Id);
+            __builder.AddAttribute(3, "Id", Id);
             __builder.CloseComponent();
         };
     }
