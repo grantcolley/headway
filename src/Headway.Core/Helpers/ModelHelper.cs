1635e4392e0651d892fc01e051a9963a101e2da1
grantcolley <grantcolley@yahoo.com>
2021-07-10 21:44:34 +0100

    Create ModelHelper to get all models from Headway.Core where the class has the DynamiCmodelAttribute #18
    Create ModelHelper to get all models from Headway.Core where the class has the DynamiCmodelAttribute #18

---
+++ src/Headway.Core/Helpers/ModelHelper.cs
@@ -0,0 +1,26 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Headway.Core.Helpers
+{
+    public static class ModelHelper
+    {
+        public static IEnumerable<BrowserStorageItem> GetBrowserStorageItems()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var browserStorageItems = (from t in assembly.GetTypes()
+                                      let attributes = t.GetCustomAttributes(typeof(DynamicModelAttribute), true)
+                                       where attributes != null && attributes.Length > 0
+                                       select new BrowserStorageItem
+                                       {
+                                           Key = t.Name,
+                                           Value = $"{t.FullName}, {assembly.GetName().Name}"
+                                       }).ToList();
+            return browserStorageItems;
+        }
+    }
+}
