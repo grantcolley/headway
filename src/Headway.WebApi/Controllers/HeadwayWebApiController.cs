18ce3d7e066e53a2ed5cc066429f05385e7c5492
grantcolley <grantcolley@yahoo.com>
2021-07-11 18:20:11 +0100

    Create HeadwayWebApiController.cs

---
+++ src/Headway.WebApi/Controllers/HeadwayWebApiController.cs
@@ -0,0 +1,44 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    [ApiController]
+    [EnableCors("local")]
+    [Route("[controller]")]
+    [Authorize(Roles = "headwayuser")]
+    public class HeadwayWebApiController : ApiControllerBase<HeadwayWebApiController>
+    {
+        private readonly IHeadwayWebApiRepository headwayWebApiRepository;
+
+        public HeadwayWebApiController(
+            IHeadwayWebApiRepository headwayWebApiRepository,
+            ILogger<HeadwayWebApiController> logger)
+            : base(headwayWebApiRepository, logger)
+        {
+            this.headwayWebApiRepository = headwayWebApiRepository;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            var authorised = await IsAuthorisedAsync("Admin")
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var controllers = TypeAttributeHelper.GetGetEntryAssemblyAttributeImplemters(typeof(DynamicApiControllerAttribute));
+
+            return Ok(controllers);
+        }
+    }
+}
