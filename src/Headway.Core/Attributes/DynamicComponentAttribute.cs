4e918e0cd24857f9b028cb96c89890af6e581d88
grantcolley <grantcolley@yahoo.com>
2021-07-11 18:40:19 +0100

    Update DynamicComponentAttribute.cs

--- src/Headway.Core/Attributes/DynamicComponentAttribute.cs
+++ src/Headway.Core/Attributes/DynamicComponentAttribute.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Headway.Core.Enums;
+using System;
 
 namespace Headway.Core.Attributes
 {
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class DynamicComponentAttribute : Attribute
     {
+        public DynamicComponentAttribute(ComponentType ComponentType)
+        {
+            this.ComponentType = ComponentType;
+        }
+
+        public ComponentType ComponentType { get; set; }
     }
 }
