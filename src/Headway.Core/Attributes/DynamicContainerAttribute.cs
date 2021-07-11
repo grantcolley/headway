b0ec4691e795dbb350508be220355f797b3c80d7
grantcolley <grantcolley@yahoo.com>
2021-07-11 19:16:30 +0100

    Rename DynamicComponentAttribute to DynamicContainerAttribute
    Rename DynamicComponentAttribute to DynamicContainerAttribute

---
+++ src/Headway.Core/Attributes/DynamicContainerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Headway.Core.Enums;
+using System;
+
+namespace Headway.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class DynamicContainerAttribute : Attribute
+    {
+        public DynamicContainerAttribute(ContainerType ContainerType)
+        {
+            this.ContainerType = ContainerType;
+        }
+
+        public ContainerType ContainerType { get; set; }
+    }
+}
