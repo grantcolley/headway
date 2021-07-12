ada790305a4b8a181ffda4acbbf3e03607307487
grantcolley <grantcolley@yahoo.com>
2021-07-12 19:01:58 +0100

    Update Configure page
    Update Configure page

--- src/Headway.RazorShared/Pages/Configure.razor.cs
+++ src/Headway.RazorShared/Pages/Configure.razor.cs
@@ -1,8 +1,23 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Headway.RazorShared.Pages
 {
     public partial class ConfigureBase : ComponentBase
     {
+        protected IEnumerable<DynamicType> models;
+        protected IEnumerable<DynamicType> containers;
+
+        protected override async Task OnInitializedAsync()
+        {
+            models = TypeAttributeHelper.GetExecutingAssemblyDynamicTypesByAttribute(typeof(DynamicModelAttribute));
+            containers = TypeAttributeHelper.GetCallingAssemblyDynamicTypesByAttribute(typeof(DynamicContainerAttribute));
+
+            await base.OnInitializedAsync();
+        }
     }
 }
