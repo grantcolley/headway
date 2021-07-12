317ae1a84daf45f0048ec8f888e5dd7995412ce6
grantcolley <grantcolley@yahoo.com>
2021-07-12 19:32:06 +0100

    Update IConfigRepository to fetch all Model and List configs
    Update IConfigRepository to fetch all Model and List configs

--- src/Headway.Core/Interface/IConfigRepository.cs
+++ src/Headway.Core/Interface/IConfigRepository.cs
@@ -1,11 +1,14 @@
 ﻿using Headway.Core.Model;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Headway.Core.Interface
 {
     public interface IConfigRepository : IRepository
     {
+        Task<IEnumerable<ListConfig>> GetListConfigsAsync();
         Task<ListConfig> GetListConfigAsync(string name);
+        Task<IEnumerable<ModelConfig>> GetModelConfigsAsync();
         Task<ModelConfig> GetModelConfigAsync(string model);
     }
 }
--- src/Headway.Repository/ConfigRepository.cs
+++ src/Headway.Repository/ConfigRepository.cs
@@ -2,6 +2,7 @@
 using Headway.Core.Model;
 using Headway.Repository.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Headway.Repository
@@ -13,12 +14,28 @@ namespace Headway.Repository
         {
         }
 
+        public async Task<IEnumerable<ListConfig>> GetListConfigsAsync()
+        {
+            return await applicationDbContext.ListConfigs
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
         public async Task<ListConfig> GetListConfigAsync(string name)
         {
             return await applicationDbContext.ListConfigs
                 .Include(l => l.ListItemConfigs)
                 .AsNoTracking()
                 .SingleAsync(l => l.Name.Equals(name))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<ModelConfig>> GetModelConfigsAsync()
+        {
+            return await applicationDbContext.ModelConfigs
+                .AsNoTracking()
+                .ToListAsync()
                 .ConfigureAwait(false);
         }
 
