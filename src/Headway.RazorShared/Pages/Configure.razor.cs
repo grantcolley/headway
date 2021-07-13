4a9adfecd843da89e3d2090fda739d8c69b23ef5
grantcolley <grantcolley@yahoo.com>
2021-07-13 18:09:38 +0100

    Update the Configure razor component
    Update the Configure razor component

--- src/Headway.RazorShared/Pages/Configure.razor.cs
+++ src/Headway.RazorShared/Pages/Configure.razor.cs
@@ -1,7 +1,9 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Helpers;
 using Headway.Core.Model;
+using Headway.RazorShared.Model;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,15 +11,33 @@ namespace Headway.RazorShared.Pages
 {
     public partial class ConfigureBase : ComponentBase
     {
-        protected IEnumerable<DynamicType> models;
-        protected IEnumerable<DynamicType> containers;
+        //protected IEnumerable<DynamicType> models;
+        protected IEnumerable<DynamicType> configurations;
+        protected DynamicComponentConfiguration selectedConfiguration;
+
+        private Dictionary<string, DynamicComponentConfiguration> dynamicComponents;
 
         protected override async Task OnInitializedAsync()
         {
-            models = TypeAttributeHelper.GetExecutingAssemblyDynamicTypesByAttribute(typeof(DynamicModelAttribute));
-            containers = TypeAttributeHelper.GetCallingAssemblyDynamicTypesByAttribute(typeof(DynamicContainerAttribute));
+            //models = TypeAttributeHelper.GetExecutingAssemblyDynamicTypesByAttribute(typeof(DynamicModelAttribute));
+            configurations = TypeAttributeHelper.GetCallingAssemblyDynamicTypesByAttribute(typeof(DynamicConfigurationAttribute));
+
+            dynamicComponents = new Dictionary<string, DynamicComponentConfiguration>();
+
+            foreach(var configuration in configurations)
+            {
+                dynamicComponents.Add(configuration.Name, new DynamicComponentConfiguration { ComponentType = Type.GetType(configuration.Namespace) });
+            }
 
             await base.OnInitializedAsync();
+        }
+
+        protected void ContainerSelectionChanged(ChangeEventArgs e)
+        {
+            if(e.Value != null)
+            {
+                selectedConfiguration = dynamicComponents[e.Value.ToString()];
+            }
         }
     }
 }
