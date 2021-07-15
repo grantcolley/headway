a9505e78baef473e20921721b1f0cc115cdc61ad
grantcolley <grantcolley@yahoo.com>
2021-07-15 13:49:37 +0100

    Update DynamicType.cs

--- src/Headway.Core/Model/DynamicType.cs
+++ src/Headway.Core/Model/DynamicType.cs
@@ -3,6 +3,7 @@
     public class DynamicType
     {
         public string Name { get; set; }
+        public string DisplayName { get; set; }
         public string Namespace { get; set; }
     }
 }
