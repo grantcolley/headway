07e3f1b946e1503cff443ac1d76180edbb03daab
grantcolley <grantcolley@yahoo.com>
2021-07-16 09:45:48 +0100

    Update TypeAttributeHelper.cs

--- src/Headway.Core/Helpers/TypeAttributeHelper.cs
+++ src/Headway.Core/Helpers/TypeAttributeHelper.cs
@@ -1,5 +1,4 @@
-﻿using Headway.Core.Attributes;
-using Headway.Core.Model;
+﻿using Headway.Core.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,36 +8,7 @@ namespace Headway.Core.Helpers
 {
     public static class TypeAttributeHelper
     {
-        public static IEnumerable<DynamicType> GetCallingAssemblyDynamicRazorComponentsByAttribute(Type attributeType)
-        {
-            var assembly = Assembly.GetCallingAssembly();
-            var dynamicTypes = (from t in assembly.GetTypes()
-                                let attributes = t.GetCustomAttributes(attributeType, true)
-                                where attributes != null && attributes.Length > 0
-                                select new DynamicType
-                                {
-                                    Name = t.Name.Replace("Base", string.Empty),
-                                    DisplayName = t.Name.Replace("Base", string.Empty),
-                                    Namespace = $"{t.FullName.Replace("Base", string.Empty)}, {assembly.GetName().Name}"
-                                }).ToList();
-            return dynamicTypes;
-        }
-
-        public static IEnumerable<DynamicType> GetCallingAssemblyDynamicTypesByAttribute(Type attributeType)
-        {
-            var assembly = Assembly.GetCallingAssembly();
-            var dynamicTypes = (from t in assembly.GetTypes()
-                                let attributes = t.GetCustomAttributes(attributeType, true)
-                                where attributes != null && attributes.Length > 0
-                                select new DynamicType
-                                {
-                                    Name = t.Name,
-                                    Namespace = $"{t.FullName}, {assembly.GetName().Name}"
-                                }).ToList();
-            return dynamicTypes;
-        }
-
-        public static IEnumerable<string> GetEntryAssemblyTypesByAttribute(Type attributeType)
+        public static IEnumerable<string> GetEntryAssemblyTypeNamesByAttribute(Type attributeType)
         {
             var assembly = Assembly.GetEntryAssembly();
             var types = (from t in assembly.GetTypes()
@@ -48,17 +18,55 @@ namespace Headway.Core.Helpers
             return types;
         }
 
-        public static IEnumerable<DynamicType> GetExecutingAssemblyDynamicTypesByAttribute(Type attributeType)
+        public static IEnumerable<DynamicType> GetHeadwayDynamicComponentsByAttribute(Type attributeType)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var dynamicTypes = (from t in assembly.GetTypes()
-                                let attributes = t.GetCustomAttributes(attributeType, true)
-                                where attributes != null && attributes.Length > 0
-                                select new DynamicType
-                                {
-                                    Name = t.Name,
-                                    Namespace = $"{t.FullName}, {assembly.GetName().Name}"
-                                }).ToList();
+            var dynamicTypes = new List<DynamicType>();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a.GetName().Name.StartsWith("Headway"))
+                .ToList();
+
+            foreach (var assembly in assemblies)
+            {
+                var types = (from t in assembly.GetTypes()
+                             let attributes = t.GetCustomAttributes(attributeType, true)
+                             where attributes != null && attributes.Length > 0
+                             select new DynamicType
+                             {
+                                 Name = t.Name.Replace("Base", string.Empty),
+                                 DisplayName = t.Name.Replace("Base", string.Empty),
+                                 Namespace = $"{t.FullName.Replace("Base", string.Empty)}, {assembly.GetName().Name}"
+                             }).ToList();
+
+                dynamicTypes.AddRange(types);
+            }
+
+            return dynamicTypes;
+        }
+
+        public static IEnumerable<DynamicType> GetHeadwayTypesByAttribute(Type attributeType)
+        {
+            var dynamicTypes = new List<DynamicType>();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a.GetName().Name.StartsWith("Headway"))
+                .ToList();
+
+            foreach (var assembly in assemblies)
+            {
+                var types = (from t in assembly.GetTypes()
+                                    let attributes = t.GetCustomAttributes(attributeType, true)
+                                    where attributes != null && attributes.Length > 0
+                                    select new DynamicType
+                                    {
+                                        Name = t.Name,
+                                        DisplayName = t.Name,
+                                        Namespace = $"{t.FullName}, {assembly.GetName().Name}"
+                                    }).ToList();
+
+                dynamicTypes.AddRange(types);
+            }
+
             return dynamicTypes;
         }
     }
