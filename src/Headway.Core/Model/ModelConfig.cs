68e4029ca8d8aebab3ea5021c4b97c5dab4efd2a
grantcolley <grantcolley@yahoo.com>
2021-07-17 20:02:23 +0100

    Update ModelConfig.cs

--- src/Headway.Core/Model/ModelConfig.cs
+++ src/Headway.Core/Model/ModelConfig.cs
@@ -9,9 +9,10 @@ namespace Headway.Core.Model
             FieldConfigs = new List<FieldConfig>();
         }
 
-        public int ModelConfigId { get; set; } 
-        public string ModelName { get; set; }
-        public string ConfigApiPath { get; set; }
+        public int ModelConfigId { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string ConfigApi { get; set; }
         public string NavigateText { get; set; }
         public string NavigateTo { get; set; }
         public List<FieldConfig> FieldConfigs { get; set; }
