9ef3a7260dbe846ad21cd93638413b1cd6f11798
grantcolley <grantcolley@yahoo.com>
2021-07-18 20:44:35 +0100

    Update List.razor.cs

--- src/Headway.Razor.Components/Pages/List.razor.cs
+++ src/Headway.Razor.Components/Pages/List.razor.cs
@@ -1,5 +1,7 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Enums;
+using Headway.Core.Interface;
+using Headway.Core.Model;
 using Headway.Razor.Components.Base;
 using Headway.Razor.Components.DynamicComponents;
 using Microsoft.AspNetCore.Components;
@@ -11,23 +13,34 @@ namespace Headway.Razor.Components.Pages
     [DynamicContainer(ContainerType.List)]
     public partial class ListBase : DynamicTypeComponentBase
     {
-        [Parameter]
-        public string TypeName { get; set; }
+        [Inject]
+        public IConfigurationService ConfigurationService { get; set; }
 
-        protected string typeFullName;
+        [Parameter]
+        public string Name { get; set; }
+
+        protected string modelNameSpace;
+
+        protected ListConfig listConfig;
 
         protected override async Task OnInitializedAsync()
         {
-            typeFullName = await GetTypeFullName(TypeName).ConfigureAwait(false);
+            var response = await ConfigurationService.GetListConfigAsync(Name).ConfigureAwait(false);
+
+            listConfig = GetResponse(response);
+
+            modelNameSpace = GetModelNameSpace(listConfig.Model);
+
             await base.OnInitializedAsync();
         }
 
         protected RenderFragment RenderListView() => __builder =>
         {
-            var type = Type.GetType(typeFullName);
+            var type = Type.GetType(modelNameSpace);
             var genericType = typeof(ListView<>).MakeGenericType(new[] { type });
             __builder.OpenComponent(1, genericType);
-            __builder.AddAttribute(2, "TypeName", TypeName);
+            __builder.AddAttribute(2, "ConfigName", listConfig.Name);
+            __builder.AddAttribute(3, "ModelName", listConfig.Model);
             __builder.CloseComponent();
         };
     }
