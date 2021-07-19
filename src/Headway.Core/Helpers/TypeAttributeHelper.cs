d34f68f00686eab4dbcc2d03bf9ecd2e42deffd3
grantcolley <grantcolley@yahoo.com>
2021-07-19 20:47:34 +0100

    Update TypeAttributeHelper.cs

--- src/Headway.Core/Helpers/TypeAttributeHelper.cs
+++ src/Headway.Core/Helpers/TypeAttributeHelper.cs
@@ -18,32 +18,6 @@ namespace Headway.Core.Helpers
             return types;
         }
 
-        public static IEnumerable<DynamicType> GetHeadwayDynamicComponentsByAttribute(Type attributeType)
-        {
-            var dynamicTypes = new List<DynamicType>();
-
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.GetName().Name.StartsWith("Headway"))
-                .ToList();
-
-            foreach (var assembly in assemblies)
-            {
-                var types = (from t in assembly.GetTypes()
-                             let attributes = t.GetCustomAttributes(attributeType, true)
-                             where attributes != null && attributes.Length > 0
-                             select new DynamicType
-                             {
-                                 Name = t.Name.Replace("Base", string.Empty),
-                                 DisplayName = t.Name.Replace("Base", string.Empty),
-                                 Namespace = $"{t.FullName.Replace("Base", string.Empty)}, {assembly.GetName().Name}"
-                             }).ToList();
-
-                dynamicTypes.AddRange(types);
-            }
-
-            return dynamicTypes;
-        }
-
         public static IEnumerable<DynamicType> GetHeadwayTypesByAttribute(Type attributeType)
         {
             var dynamicTypes = new List<DynamicType>();
@@ -59,15 +33,29 @@ namespace Headway.Core.Helpers
                                     where attributes != null && attributes.Length > 0
                                     select new DynamicType
                                     {
-                                        Name = t.Name,
-                                        DisplayName = t.Name,
-                                        Namespace = $"{t.FullName}, {assembly.GetName().Name}"
+                                        Name = GetName(t),
+                                        DisplayName = GetName(t),
+                                        Namespace = GetFullNamespace(t, assembly)
                                     }).ToList();
 
                 dynamicTypes.AddRange(types);
             }
 
             return dynamicTypes;
+        }
+
+        private static string GetName(Type type)
+        {
+            return (type.IsAbstract && type.Name.Contains("Base"))
+                ? type.Name.Replace("Base", string.Empty)
+                : type.Name;
+        }
+
+        private static string GetFullNamespace(Type type, Assembly assembly)
+        {
+            return (type.IsAbstract && type.Name.Contains("Base"))
+                ? $"{type.FullName.Replace("Base", string.Empty)}, {assembly.GetName().Name}"
+                : $"{type.Name}, {assembly.GetName().Name}";
         }
     }
 }
