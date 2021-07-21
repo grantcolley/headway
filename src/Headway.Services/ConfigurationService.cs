46f7fc5bc3168b8702cb2e825ea3e7e15affb571
grantcolley <grantcolley@yahoo.com>
2021-07-21 17:21:11 +0100

    Update ConfigurationService.cs

--- src/Headway.Services/ConfigurationService.cs
+++ src/Headway.Services/ConfigurationService.cs
@@ -8,26 +8,53 @@ namespace Headway.Services
 {
     public class ConfigurationService : ServiceBase, IConfigurationService
     {
-        public ConfigurationService(HttpClient httpClient)
+        private readonly IConfigCache configCache;
+
+        public ConfigurationService(HttpClient httpClient, IConfigCache configCache)
             : base(httpClient, false)
         {
+            this.configCache = configCache;
         }
 
-        public ConfigurationService(HttpClient httpClient, TokenProvider tokenProvider)
+        public ConfigurationService(HttpClient httpClient, TokenProvider tokenProvider, IConfigCache configCache)
             : base(httpClient, true, tokenProvider)
         {
+            this.configCache = configCache;
         }
 
         public async Task<IServiceResult<Config>> GetConfigAsync(string name)
         {
+            var config = configCache.GetConfig(name);
+
+            if(config != null)
+            {
+                return GetServiceResult<Config>(config);
+            }
+
             var httpResponseMessage = await httpClient.GetAsync($"Configuration/{name}").ConfigureAwait(false);
-            return await GetServiceResult<Config>(httpResponseMessage);
+
+            var serviceResult = await GetServiceResult<Config>(httpResponseMessage).ConfigureAwait(false);
+
+            if(serviceResult.IsSuccess)
+            {
+                configCache.AddConfig(serviceResult.Result);
+            }
+
+            return serviceResult;
         }
 
         public async Task<IServiceResult<IEnumerable<Config>>> GetConfigsAsync()
         {
             var httpResponseMessage = await httpClient.GetAsync($"Configuration").ConfigureAwait(false);
-            return await GetServiceResult<IEnumerable<Config>>(httpResponseMessage);
+
+            var serviceResult = await GetServiceResult<IEnumerable<Config>>(httpResponseMessage);
+
+            if (serviceResult.IsSuccess)
+            {
+                configCache.AddConfigs(serviceResult.Result);
+            }
+
+            return serviceResult;
         }
 
         /// <summary>
