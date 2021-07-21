8342bca6b65714bf2055413079a5deec5401de91
grantcolley <grantcolley@yahoo.com>
2021-07-21 11:22:40 +0100

    Update DynamicTypeComponentBase.cs

--- src/Headway.Razor.Components/Base/DynamicTypeComponentBase.cs
+++ src/Headway.Razor.Components/Base/DynamicTypeComponentBase.cs
@@ -1,15 +1,39 @@
-﻿using Headway.Core.Cache;
+﻿using Headway.Core.Attributes;
+using Headway.Core.Cache;
+using Headway.Core.Interface;
+using Headway.Core.Model;
 using Microsoft.AspNetCore.Components;
 using System;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Components.Base
 {
     public abstract class DynamicTypeComponentBase : HeadwayComponentBase
-    {       
+    {
+        [Inject]
+        public IConfigurationService ConfigurationService { get; set; }
+
         [Inject]
         DynamicTypeCache DynamicTypeCache { get; set; }
 
-        public string GetTypeNamespace(string name, Type attribute)
+        protected Config config;
+
+        protected string modelNameSpace;
+
+        protected string componentNameSpace;
+
+        protected async Task GetConfig(string configName)
+        {
+            var response = await ConfigurationService.GetConfigAsync(configName).ConfigureAwait(false);
+
+            config = GetResponse(response);
+
+            modelNameSpace = GetTypeNamespace(config.Model, typeof(DynamicModelAttribute));
+
+            componentNameSpace = GetTypeNamespace(config.Component, typeof(DynamicComponentAttribute));
+        }
+
+        protected string GetTypeNamespace(string name, Type attribute)
         {
             var dynamicType = DynamicTypeCache.GetDynamicType(name, attribute);
 
