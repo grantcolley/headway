4e44dd84edb2affa10cd58467df0db09db63b800
grantcolley <grantcolley@yahoo.com>
2021-07-21 14:59:04 +0100

    Create ConfigCache
    Create ConfigCache

---
+++ src/Headway.Core/Cache/ConfigCache.cs
@@ -0,0 +1,42 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Cache
+{
+    public class ConfigCache : IConfigCache
+    {
+        private readonly IConfigurationService configurationService;
+
+        private readonly Dictionary<string, Config> cache = new();
+
+        public ConfigCache(IConfigurationService configurationService)
+        {
+            this.configurationService = configurationService;
+        }
+
+        public async Task<Config> GetConfigAsync(string configName)
+        {
+            var config = cache.GetValueOrDefault(configName);
+
+            if (config != null)
+            {
+                return config;
+            }
+
+            var result = await configurationService.GetConfigAsync(configName)
+                .ConfigureAwait(false);
+
+            if(result.IsSuccess)
+            {
+                if (!cache.ContainsKey(result.Result.Name))
+                {
+                    cache.Add(result.Result.Name, result.Result);
+                }
+            }
+
+            return cache.GetValueOrDefault(configName);
+        }
+    }
+}
---
+++ src/Headway.Core/Interface/IConfigCache.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Model;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IConfigCache
+    {
+        Task<Config> GetConfigAsync(string configName);
+    }
+}
