15d5b4ef72bfefe14af0848cf283e2b921325f49
grantcolley <grantcolley@yahoo.com>
2021-07-28 10:38:04 +0100

    Update IConfigurationService.cs

--- src/Headway.Core/Interface/IConfigurationService.cs
+++ src/Headway.Core/Interface/IConfigurationService.cs
@@ -10,12 +10,5 @@ namespace Headway.Core.Interface
         Task<IServiceResult<IEnumerable<Config>>> GetConfigsAsync();
         Task<IServiceResult<Config>> GetConfigAsync(string name);
         Task<IServiceResult<IEnumerable<Config>>> GetConfigsByTypeAsync(int configTypeId);
-
-
-        //// Obsolete
-        //Task<IServiceResult<IEnumerable<ListConfig>>> GetListConfigsAsync();
-        //Task<IServiceResult<ListConfig>> GetListConfigAsync(string name);
-        //Task<IServiceResult<IEnumerable<ModelConfig>>> GetModelConfigsAsync();
-        //Task<IServiceResult<ModelConfig>> GetModelConfigAsync(string model);
     }
 }
