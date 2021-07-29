6d1d3c12eb193f6e7319b1acc2c6632ed2503846
grantcolley <grantcolley@yahoo.com>
2021-07-29 12:50:55 +0100

    Add ConfigItems migrations to Sql Server
    Add ConfigItems migrations to Sql Server

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210729114844_HeadwayConfigItems1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class HeadwayConfigItems1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NavigateToPropertyName",
+                table: "Configs",
+                newName: "NavigateToProperty");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NavigateBackProperty",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NavigateBackProperty",
+                table: "Configs");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigateToProperty",
+                table: "Configs",
+                newName: "NavigateToPropertyName");
+        }
+    }
+}
