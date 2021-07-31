d59ea77ad37f3b89062907c6ae658edc798c4ee8
grantcolley <grantcolley@yahoo.com>
2021-07-31 19:08:03 +0100

    Update Config.cs

--- src/Headway.Core/Model/Config.cs
+++ src/Headway.Core/Model/Config.cs
@@ -1,5 +1,6 @@
 ﻿using Headway.Core.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Headway.Core.Model
 {
@@ -13,11 +14,6 @@ namespace Headway.Core.Model
 
         public int ConfigId { get; set; }
         public ConfigType ConfigType { get; set; }
-        public string Name { get; set; }
-        public string Title { get; set; }
-        public string Model { get; set; }
-        public string ModelApi { get; set; }
-        public string Component { get; set; }
         public string NavigateTo { get; set; }
         public string NavigateToProperty { get; set; }
         public string NavigateToConfig { get; set; }
@@ -25,5 +21,25 @@ namespace Headway.Core.Model
         public string NavigateBackProperty { get; set; }
         public string NavigateBackConfig { get; set; }
         public List<ConfigItem> ConfigItems { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(20, ErrorMessage = "Name must be between 1 and 20 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(20, ErrorMessage = "Title must be between 1 and 20 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Model is required.")]
+        [StringLength(20, ErrorMessage = "Model must be between 1 and 20 characters")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Model Api is required.")]
+        [StringLength(20, ErrorMessage = "Model Api must be between 1 and 20 characters")]
+        public string ModelApi { get; set; }
+
+        [Required(ErrorMessage = "Component is required.")]
+        [StringLength(100, ErrorMessage = "Component must be between 1 and 20 characters")]
+        public string Component { get; set; }
     }
 }
