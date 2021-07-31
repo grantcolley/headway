9bf7aa8c9c38ac087013b91274f41fd667335a04
grantcolley <grantcolley@yahoo.com>
2021-07-31 20:02:45 +0100

    Delete obsolete classes
    Delete obsolete classes

--- src/Headway.Core/_obsolete/IFragmentContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Headway.Core.Interface
-{
-    public interface IFragmentContainer : IFragment
-    {
-        List<IFragment> Fragments { get; set; }
-    }
-}
