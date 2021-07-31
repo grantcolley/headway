f49ef993a44bcfdae250eceb2943567c73d6a054
grantcolley <grantcolley@yahoo.com>
2021-07-31 19:49:57 +0100

    Add SqlServer migration
    Add SqlServer migration

--- src/Utilities/Headway.MigrationsSqlServer/Migrations/20210731181534_HeadwayConfig1_4.Designer.cs
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210731181534_HeadwayConfig1_4.Designer.cs
@@ -4,14 +4,16 @@ using Headway.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Headway.MigrationsSqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210731181534_HeadwayConfig1_4")]
+    partial class HeadwayConfig1_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210731181534_HeadwayConfig1_4.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class HeadwayConfig1_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Configs_Name",
+                table: "Configs");
+
+            migrationBuilder.DropColumn(
+                name: "Component",
+                table: "Configs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Configs",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Configs",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModelApi",
+                table: "Configs",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Configs",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Container",
+                table: "Configs",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configs_Name",
+                table: "Configs",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Configs_Name",
+                table: "Configs");
+
+            migrationBuilder.DropColumn(
+                name: "Container",
+                table: "Configs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Configs",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModelApi",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Component",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configs_Name",
+                table: "Configs",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+    }
+}
