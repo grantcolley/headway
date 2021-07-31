e707482ddcce661fb0d4a840ac2bc9b16d1dcc3b
grantcolley <grantcolley@yahoo.com>
2021-07-31 19:49:13 +0100

    Update DynamicContainerAttribute.cs

--- src/Headway.Core/Attributes/DynamicContainerAttribute.cs
+++ src/Headway.Core/Attributes/DynamicContainerAttribute.cs
@@ -1,16 +1,9 @@
-﻿using Headway.Core.Enums;
-using System;
+﻿using System;
 
 namespace Headway.Core.Attributes
 {
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class DynamicContainerAttribute : Attribute
     {
-        public DynamicContainerAttribute(ContainerType ContainerType)
-        {
-            this.ContainerType = ContainerType;
-        }
-
-        public ContainerType ContainerType { get; set; }
     }
 }
