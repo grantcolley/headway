d8761244668581ab9354cd9d7e3d4fc28aaa328c
grantcolley <grantcolley@yahoo.com>
2021-07-31 19:49:06 +0100

    Create DynamicPageAttribute.cs

---
+++ src/Headway.Core/Attributes/DynamicPageAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Headway.Core.Enums;
+using System;
+
+namespace Headway.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class DynamicPageAttribute : Attribute
+    {
+        public DynamicPageAttribute(PageType pageType)
+        {
+            this.PageType = pageType;
+        }
+
+        public PageType PageType { get; set; }
+    }
+}
