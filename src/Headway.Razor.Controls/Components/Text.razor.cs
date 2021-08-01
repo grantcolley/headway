9d8470475438f696d3beea2dee8f151fb1814ff5
grantcolley <grantcolley@yahoo.com>
2021-08-01 19:43:51 +0100

    Refactor LabelText into Text with code behind
    Refactor LabelText into Text with code behind

---
+++ src/Headway.Razor.Controls/Components/Text.razor.cs
@@ -0,0 +1,34 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Components
+{
+    public partial class TextBase : ComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
+
+        public void OnValueChanged(string value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+}
