9b6260d12e8c8f6fe01d8af36a517f0ef4865ff1
grantcolley <grantcolley@yahoo.com>
2021-08-01 09:51:11 +0100

    Update ConfigItem.cs

--- src/Headway.Core/Model/ConfigItem.cs
+++ src/Headway.Core/Model/ConfigItem.cs
@@ -1,14 +1,22 @@
-﻿namespace Headway.Core.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Headway.Core.Model
 {
     public class ConfigItem
     {
         public int ConfigItemId { get; set; }
-        public string PropertyName { get; set; }
-        public string Label { get; set; }
         public bool? IsIdentity { get; set; }
         public bool? IsTitle { get; set; }
         public int Order { get; set; }
         public string Component { get; set; }
+
+        [Required(ErrorMessage = "Property Name is required.")]
+        [StringLength(20, ErrorMessage = "Property Name must be between 1 and 20 characters")]
+        public string PropertyName { get; set; }
+
+        [Required(ErrorMessage = "Label is required.")]
+        [StringLength(20, ErrorMessage = "Label must be between 1 and 20 characters")]
+        public string Label { get; set; }
 
     }
 }
