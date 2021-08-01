e6d20ff19e17d4f5fb3e6bfe4f9c465ff5758d6c
grantcolley <grantcolley@yahoo.com>
2021-08-01 19:51:35 +0100

    Implement code behind in a partial class
    Implement code behind in a partial class

---
+++ src/Headway.Razor.Controls/Pages/Alert.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Headway.Razor.Controls.Pages
+{
+    public partial class Alert : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string AlertType { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        [Parameter]
+        public string RedirectText { get; set; }
+
+        [Parameter]
+        public string RedirectPage { get; set; }
+
+        public string Class { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Class = $"alert alert-{AlertType} mt-4";
+
+            base.OnInitialized();
+        }
+
+        private void Redirect()
+        {
+            NavigationManager.NavigateTo($@"{RedirectPage}");
+        }
+    }
+}
