31c45f611becd221808d97ab4b60974788b6b528
grantcolley <grantcolley@yahoo.com>
2021-08-01 19:43:27 +0100

    Refactor LabelData into Label with code behind
    Refactor LabelData into Label with code behind

---
+++ src/Headway.Razor.Controls/Components/Label.razor.cs
@@ -0,0 +1,19 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.Razor.Controls.Components
+{
+    public partial class LabelBase : ComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model).ToString();
+            }
+        }
+    }
+}
