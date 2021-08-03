20aaadcd5d99651401ebeb3f8b3ef617855c933d
grantcolley <grantcolley@yahoo.com>
2021-08-03 20:41:24 +0100

    Create ModelOptionItems.cs

---
+++ src/Headway.Services/Options/ModelOptionItems.cs
@@ -0,0 +1,14 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Services.Options
+{
+    public class ModelOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
