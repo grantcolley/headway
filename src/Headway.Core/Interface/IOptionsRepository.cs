b12e213c3fb1bc167d432a18f4031c995ba85a51
grantcolley <grantcolley@yahoo.com>
2021-08-03 20:19:19 +0100

    Create IOptionsRepository and OptionsRepository
    Create IOptionsRepository and OptionsRepository

---
+++ src/Headway.Core/Interface/IOptionsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IOptionsRepository : IRepository
+    {
+        Task<IServiceResult<IEnumerable<OptionItem>>> GetOptionItemsAsync(string source);
+    }
+}
---
+++ src/Headway.Repository/OptionsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Headway.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Repository
+{
+    public class OptionsRepository : RepositoryBase, IOptionsRepository
+    {
+        public OptionsRepository(ApplicationDbContext applicationDbContext)
+            : base(applicationDbContext)
+        {
+        }
+
+        public Task<IServiceResult<IEnumerable<OptionItem>>> GetOptionItemsAsync(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
