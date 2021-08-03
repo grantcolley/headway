59891e6bcde38c5aa7e328ca8bf809ef6ef86cd2
grantcolley <grantcolley@yahoo.com>
2021-08-03 20:41:34 +0100

    Create OptionsController.cs

---
+++ src/Headway.WebApi/Controllers/OptionsController.cs
@@ -0,0 +1,38 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    public class OptionsController : ApiControllerBase<OptionsController>
+    {
+        private readonly IOptionsRepository optionsRepository;
+
+        public OptionsController(
+            IOptionsRepository optionsRepository,
+            ILogger<OptionsController> logger)
+            : base(optionsRepository, logger)
+        {
+            this.optionsRepository = optionsRepository;
+        }
+
+        [HttpGet("{optionsCode}")]
+        public async Task<IActionResult> Get(string optionsCode)
+        {
+            var authorised = await IsAuthorisedAsync("Admin")
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var permissions = await optionsRepository
+                .GetOptionItemsAsync(optionsCode)
+                .ConfigureAwait(false);
+
+            return Ok(permissions);
+        }
+    }
+}
