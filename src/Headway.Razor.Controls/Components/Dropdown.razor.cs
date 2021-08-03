d392925ca4397ceaa40f1d61209e4ed6e32ce3d9
grantcolley <grantcolley@yahoo.com>
2021-08-03 20:42:10 +0100

    Create Dropdown.razor
    Create Dropdown.razor

---
+++ src/Headway.Razor.Controls/Components/Dropdown.razor.cs
@@ -0,0 +1,34 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+using System;
+
+namespace Headway.Razor.Controls.Components
+{
+    public partial class Dropdown : ComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
+
+        public void OnValueChanged(string value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+}
