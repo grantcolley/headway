76edb5a875696a053312bfac9e0c4f031c4966f7
grantcolley <grantcolley@yahoo.com>
2021-08-05 21:02:53 +0100

    Create LayoutConfigItem.cs

---
+++ src/Headway.Core/Model/LayoutConfigItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class LayoutConfigItem
+    {
+        public int LayoutConfigItemId { get; set; }
+        public string Name { get; set; }
+        public string Component { get; set; } 
+        public int ContainerRowPosition { get; set; }
+        public int ContainerColumnPosition { get; set; }
+        public List<LayoutConfigItem> Containers { get; set; }             
+    }
+}
