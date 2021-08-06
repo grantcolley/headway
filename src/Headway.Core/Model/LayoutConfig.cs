6856696304341efddd1285d8b6d960c9ce72951e
grantcolley <grantcolley@yahoo.com>
2021-08-06 22:15:19 +0100

    Update LayoutConfig.cs

--- src/Headway.Core/Model/LayoutConfig.cs
+++ src/Headway.Core/Model/LayoutConfig.cs
@@ -4,6 +4,11 @@ namespace Headway.Core.Model
 {
     public class LayoutConfig
     {
+        public LayoutConfig()
+        {
+            LayoutConfigItems = new List<LayoutConfigItem>();
+        }
+
         public int LayoutConfigId { get; set; }
         public string Name { get; set; }
         public List<LayoutConfigItem> LayoutConfigItems { get; set; }
