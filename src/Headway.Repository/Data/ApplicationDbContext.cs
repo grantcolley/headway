6a236b3b3a610387ca3d30224e701dae27aef92f
grantcolley <grantcolley@yahoo.com>
2021-08-06 22:10:48 +0100

    Update ApplicationDbContext.cs

--- src/Headway.Repository/Data/ApplicationDbContext.cs
+++ src/Headway.Repository/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace Headway.Repository.Data
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<Config> Configs { get; set; }
         public DbSet<ConfigItem> ConfigItems { get; set; }
+        public DbSet<LayoutConfig> LayoutConfigs { get; set; }
+        public DbSet<LayoutConfigItem> LayoutConfigItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -42,15 +44,19 @@ namespace Headway.Repository.Data
                 .IsUnique();
 
             builder.Entity<Category>()
-                .HasIndex(p => p.Name)
+                .HasIndex(c => c.Name)
                 .IsUnique();
 
             builder.Entity<MenuItem>()
-                .HasIndex(p => p.Name)
+                .HasIndex(m => m.Name)
                 .IsUnique();
 
             builder.Entity<Config>()
-                .HasIndex(p => p.Name)
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            builder.Entity<LayoutConfig>()
+                .HasIndex(l => l.Name)
                 .IsUnique();
         }
     }
