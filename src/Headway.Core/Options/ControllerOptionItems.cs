50f4f9003c5d95d3339ce955747bf3bd855b534a
grantcolley <grantcolley@yahoo.com>
2021-08-09 10:30:38 +0100

    Create ControllerOptionItems.cs

---
+++ src/Headway.Core/Options/ControllerOptionItems.cs
@@ -0,0 +1,27 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Options
+{
+    public class ControllerOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync()
+        {
+            var controllers = TypeAttributeHelper.GetEntryAssemblyTypeNamesByAttribute(typeof(DynamicApiControllerAttribute));
+
+            var optionItems = from c in controllers
+                              select new OptionItem
+                              {
+                                  Id = c.Name,
+                                  Display = c.DisplayName
+                              };
+
+            return Task.FromResult(optionItems);
+        }
+    }
+}
