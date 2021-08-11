b5ef6d4f0d0926b9ec2be9b82d3dada7b1a248c8
grantcolley <grantcolley@yahoo.com>
2021-08-11 18:05:12 +0100

    Create ConfigContainer.cs

---
+++ src/Headway.Core/Model/ConfigContainer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class ConfigContainer
+    {
+        public int ConfigContainerId { get; set; }
+        public string Container { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public List<ConfigContainer> ConfigContainers { get; set; }
+    }
+}
