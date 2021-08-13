f9128ebdf16617321c59dd8e11908184528ee601
grantcolley <grantcolley@yahoo.com>
2021-08-13 19:42:05 +0100

    Update ConfigContainer.cs

--- src/Headway.Core/Model/ConfigContainer.cs
+++ src/Headway.Core/Model/ConfigContainer.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Headway.Core.Model
 {
     public class ConfigContainer
     {
         public int ConfigContainerId { get; set; }
-        public string Container { get; set; }
         public int Row { get; set; }
         public int Column { get; set; }
         public List<ConfigContainer> ConfigContainers { get; set; }
+
+        [Required(ErrorMessage = "Container is required.")]
+        [StringLength(150, ErrorMessage = "Container must be between 1 and 150 characters")]
+        public string Container { get; set; }
     }
 }
