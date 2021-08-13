aa702f03ad7c2821cbe87eb2403cb0545dff7975
grantcolley <grantcolley@yahoo.com>
2021-08-13 19:47:28 +0100

    Remove unnecessary usings
    Remove unnecessary usings

--- src/Headway.Core/Model/Category.cs
+++ src/Headway.Core/Model/Category.cs
@@ -1,8 +1,6 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Interface;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace Headway.Core.Model
 {
