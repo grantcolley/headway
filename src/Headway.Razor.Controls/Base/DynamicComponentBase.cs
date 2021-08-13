4130d91ee7d9c83573bf8fa00927699d7ae86f4b
grantcolley <grantcolley@yahoo.com>
2021-08-13 20:17:15 +0100

    Update DynamicComponentBase.cs

--- src/Headway.Razor.Controls/Base/DynamicComponentBase.cs
+++ src/Headway.Razor.Controls/Base/DynamicComponentBase.cs
@@ -7,5 +7,15 @@ namespace Headway.Razor.Controls.Base
     {
         [Parameter]
         public DynamicModel<T> DynamicModel { get; set; }
+
+        protected RenderFragment RenderView() => __builder =>
+        {
+            var type = Type.GetType("model");
+            var component = Type.GetType("container");
+            var genericType = component.MakeGenericType(new[] { type });
+            __builder.OpenComponent(1, genericType);
+            __builder.AddAttribute(2, "DynamicModel", DynamicModel);
+            __builder.CloseComponent();
+        };
     }
 }
