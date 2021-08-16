6f2e2762845e1027ff1a6ead6d15c6de957cdeb8
grantcolley <grantcolley@yahoo.com>
2021-08-16 20:11:51 +0100

    Create DynamicContainer.cs

---
+++ src/Headway.Core/Dynamic/DynamicContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Dynamic
+{
+    public class DynamicContainer
+    {
+        public int Row { get; set; }
+        public int Column {  get; set; }
+        public string DynamicContainerTypeName { get; set; }
+        public Type DynamicComponent { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+        public List<DynamicField> DynamicFields {  get; set; }
+        public List<DynamicContainer> DynamicContainers {  get; set; }
+    }
+}
