e008bfed82b05b1c08a9d9410a31bcd59768d2ad
grantcolley <grantcolley@yahoo.com>
2021-08-17 16:44:05 +0100

    Add SqlServer migrations
    Add SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210817152937_HeadwayConfigContainer_3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class HeadwayConfigContainer_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRootContainer",
+                table: "ConfigContainers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRootContainer",
+                table: "ConfigContainers");
+        }
+    }
+}
