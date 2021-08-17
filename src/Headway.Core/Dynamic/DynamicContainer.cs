df6a56fa822454e0a36d944d27364e6228103664
grantcolley <grantcolley@yahoo.com>
2021-08-17 16:43:27 +0100

    Update DynamicContainer.cs

--- src/Headway.Core/Dynamic/DynamicContainer.cs
+++ src/Headway.Core/Dynamic/DynamicContainer.cs
@@ -5,6 +5,13 @@ namespace Headway.Core.Dynamic
 {
     public class DynamicContainer
     {
+        public DynamicContainer()
+        {
+            DynamicFields = new List<DynamicField>();
+            DynamicContainers = new List<DynamicContainer>();
+        }
+
+        public int ContainerId { get; set; }
         public int Row { get; set; }
         public int Column {  get; set; }
         public string DynamicContainerTypeName { get; set; }
