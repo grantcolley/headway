93d6842450884b81a3e863ea1ead036a3e2fe3f4
grantcolley <grantcolley@yahoo.com>
2021-08-17 16:43:47 +0100

    Update DynamicModel.cs

--- src/Headway.Core/Dynamic/DynamicModel.cs
+++ src/Headway.Core/Dynamic/DynamicModel.cs
@@ -34,49 +34,93 @@ namespace Headway.Core.Dynamic
 
             typeHelper = DynamicTypeHelper.Get<T>();
 
-            BuildDynamicFields();
+            BuildDynamicComponents();
         }
 
         public T Model { get; private set; }
-        public List<DynamicField> DynamicFields { get; private set; }
         public Config Config { get; private set; }
+        public DynamicContainer RootContainer { get; set; }
+        public List<DynamicField> DynamicFields { get; private set; }
 
         public int Id { get { return Convert.ToInt32(typeHelper.GetValue(Model, idFieldName)); } }
 
         public string Title { get { return typeHelper.GetValue(Model, titleFieldName)?.ToString(); } }
 
-        private void BuildDynamicFields()
+        private void BuildDynamicComponents()
         {
             DynamicFields = new List<DynamicField>();
 
             var constantExpression = Expression.Constant(Model);
 
-            static DynamicField func(T model, ConstantExpression ce, PropertyInfo p, ConfigItem c)
+            DynamicFields = new List<DynamicField>((from p in typeHelper.SupportedProperties
+                                join c in Config.ConfigItems on p.Name equals c.PropertyName
+                                select CreateDynamicField(Model, constantExpression, p, c)).ToList());
+
+            RootContainer = CreateContainer(Config.Containers.Single(cc => cc.IsRootContainer));
+
+            var fieldGroups = from df in DynamicFields
+                                group df by df.ConfigContainerId into fieldGroup
+                                select fieldGroup;
+
+            MapDynamicContainerFields(RootContainer, fieldGroups);
+        }
+
+        private static DynamicContainer CreateContainer(ConfigContainer configContainer)
+        {
+            var dynamicContainer = new DynamicContainer
             {
-                var dynamicField = new DynamicField
+                ContainerId = configContainer.ConfigContainerId,
+                Row = configContainer.Row,
+                Column = configContainer.Column,
+                DynamicComponent = Type.GetType(configContainer.Container),
+                DynamicContainerTypeName = configContainer.Container
+            };
+
+            if(configContainer.ConfigContainers.Any())
+            {
+                foreach(var container in  configContainer.ConfigContainers)
                 {
-                    Model = model,
-                    Label = c.Label,
-                    Order = c.Order,
-                    Tag = c.Tag,
-                    PropertyInfo = p,
-                    PropertyName = p.Name,
-                    DynamicComponentTypeName = c.Component,
-                    DynamicComponent = Type.GetType(c.Component),
-                    MemberExpression = Expression.Property(ce, p.Name)
-                };
-
-                dynamicField.Parameters = new Dictionary<string, object> { { "Field", dynamicField } };
-
-                return dynamicField;
+                    dynamicContainer.DynamicContainers.Add(CreateContainer(container));
+                }
             }
 
-            var dynamicFields = from p in typeHelper.SupportedProperties
-                                join c in Config.ConfigItems on p.Name equals c.PropertyName
-                                orderby c.Order
-                                select func(Model, constantExpression, p, c);
+            return dynamicContainer;
+        }
 
-            DynamicFields.AddRange(dynamicFields);
+        private static DynamicField CreateDynamicField(T model, ConstantExpression expression, PropertyInfo propertyInfo, ConfigItem configItem)
+        {
+            var dynamicField = new DynamicField
+            {
+                Model = model,
+                Label = configItem.Label,
+                Order = configItem.Order,
+                ConfigContainerId = configItem.ConfigContainer.ConfigContainerId,
+                Tag = configItem.Tag,
+                PropertyInfo = propertyInfo,
+                PropertyName = propertyInfo.Name,
+                DynamicComponentTypeName = configItem.Component,
+                DynamicComponent = Type.GetType(configItem.Component),
+                MemberExpression = Expression.Property(expression, propertyInfo.Name)
+            };
+
+            dynamicField.Parameters = new Dictionary<string, object> { { "Field", dynamicField } };
+
+            return dynamicField;
+        }
+
+        private static void MapDynamicContainerFields(DynamicContainer container, IEnumerable<IGrouping<int,DynamicField>> fieldGroups)
+        {
+            var fieldGroup = fieldGroups.SingleOrDefault(fg => fg.Key.Equals(container.ContainerId));
+
+            if(fieldGroup != null)
+            {
+                container.DynamicFields.AddRange(fieldGroup.OrderBy(f => f.Order).ToList());
+            }
+
+            foreach(var c in container.DynamicContainers)
+            {
+                MapDynamicContainerFields(c, fieldGroups);
+            }
         }
     }
 }
