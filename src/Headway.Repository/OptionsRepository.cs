5be89fce7017089a53a69972f5178e024f9d7533
grantcolley <grantcolley@yahoo.com>
2021-08-19 19:30:21 +0100

    Update OptionsRepository.cs

--- src/Headway.Repository/OptionsRepository.cs
+++ src/Headway.Repository/OptionsRepository.cs
@@ -1,7 +1,10 @@
-﻿using Headway.Core.Interface;
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Interface;
 using Headway.Core.Model;
 using Headway.Core.Options;
 using Headway.Repository.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,24 +13,47 @@ namespace Headway.Repository
 {
     public class OptionsRepository : RepositoryBase, IOptionsRepository
     {
-        private readonly Dictionary<string, IOptionItems> localOptionItems = new();
+        private readonly Dictionary<string, Func<object, Task<IEnumerable<OptionItem>>>> optionItems = new();
 
         public OptionsRepository(ApplicationDbContext applicationDbContext)
             : base(applicationDbContext)
         {
-            localOptionItems.Add(typeof(ControllerOptionItems).Name, new ControllerOptionItems());
+            optionItems["ControllerOptionItems"] = new Func<object, Task<IEnumerable<OptionItem>>>(GetControllerOptionItemsAsync);
+            optionItems["ConfigOptionItems"] = new Func<object, Task<IEnumerable<OptionItem>>>(ConfigOptionItems);
         }
 
         public async Task<IEnumerable<OptionItem>> GetOptionItemsAsync(string optionsCode)
         {
-            if (localOptionItems.ContainsKey(optionsCode))
+            if (optionItems.ContainsKey(optionsCode))
             {
-                return await localOptionItems[optionsCode].GetOptionItemsAsync();
+                return await optionItems[optionsCode].Invoke(null);
             }
 
-            // Get option items from the database...
-
             throw new NotImplementedException();
+        }
+
+        private Task<IEnumerable<OptionItem>> GetControllerOptionItemsAsync(object arg = null)
+        {
+            var controllers = TypeAttributeHelper.GetEntryAssemblyTypeNamesByAttribute(typeof(DynamicApiControllerAttribute));
+
+            var optionItems = from c in controllers
+                              select new OptionItem
+                              {
+                                  Id = c.Name.Replace("Controller", ""),
+                                  Display = c.DisplayName.Replace("Controller", "")
+                              };
+
+            return Task.FromResult(optionItems);
+        }
+
+        private async Task<IEnumerable<OptionItem>> ConfigOptionItems(object arg = null)
+        {
+            var configs = await applicationDbContext.Configs
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+
+            return configs.Select(c => new OptionItem { Id = c.Name, Display = c.Title }).ToList();
         }
     }
 }
