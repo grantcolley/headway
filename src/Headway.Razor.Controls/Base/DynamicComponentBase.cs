56c0c0071a3646a21975b902a950ac95fa408a1c
grantcolley <grantcolley@yahoo.com>
2021-08-20 21:23:23 +0100

    Create DynamicComponentBase.cs

---
+++ src/Headway.Razor.Controls/Base/DynamicComponentBase.cs
@@ -0,0 +1,37 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Base
+{
+    public abstract class DynamicComponentBase : HeadwayComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        [Parameter]
+        public List<DynamicArg> ComponentArgs { get; set; }
+
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
+
+        public virtual void OnValueChanged(string value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+}
