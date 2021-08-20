28492c8efeedb58f7712af04f9505ae276d60e68
grantcolley <grantcolley@yahoo.com>
2021-08-20 19:22:37 +0100

    Create ModelFieldsOptionItems.cs

---
+++ src/Headway.Core/Options/ModelFieldsOptionItems.cs
@@ -0,0 +1,25 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Options
+{
+    public class ModelFieldsOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync()
+        {
+            //var models = TypeAttributeHelper.GetHeadwayTypesByAttribute(typeof(DynamicModelAttribute));
+
+            //var optionItems = from m in models
+            //                  select new OptionItem
+            //                  {
+            //                      Id = m.Namespace,
+            //                      Display = m.DisplayName
+            //                  };
+
+            //return Task.FromResult(optionItems);
+        }
+    }
+}
