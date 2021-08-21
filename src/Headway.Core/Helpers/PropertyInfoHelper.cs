30e9a82ec073dfb5d85c2340a06711205fe7e076
grantcolley <grantcolley@yahoo.com>
2021-08-21 16:38:44 +0100

    Create PropertyInfoHelper.cs

---
+++ src/Headway.Core/Helpers/PropertyInfoHelper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Headway.Core.Helpers
+{
+    public static class PropertyInfoHelper
+    {
+        public static IEnumerable<PropertyInfo> GetPropertyInfos(Type type)
+        {
+            var propertyInfoResults = new List<PropertyInfo>();
+
+            PropertyInfo[] propertyInfos = type.GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                if (UnsupportedProperty(propertyInfo))
+                {
+                    continue;
+                }
+
+                propertyInfoResults.Add(propertyInfo);
+            }
+
+            return propertyInfoResults;
+        }
+
+        private static bool UnsupportedProperty(PropertyInfo propertyInfo)
+        {
+            // Skip non-public properties and properties that are either 
+            // classes (but not strings), interfaces, lists, generic 
+            // lists or arrays.
+            var propertyType = propertyInfo.PropertyType;
+
+            if (propertyType != typeof(string)
+                && (propertyType.IsClass
+                    || propertyType.IsInterface
+                    || propertyType.IsArray
+                    || propertyType.GetInterfaces()
+                        .Any(
+                            i =>
+                                (i.GetTypeInfo().Name.Equals(typeof(IEnumerable).Name)
+                                 || (i.IsGenericType &&
+                                     i.GetGenericTypeDefinition().Name.Equals(typeof(IEnumerable<>).Name))))))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
