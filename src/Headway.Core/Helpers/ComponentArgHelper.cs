a8ca778b1ca78f71069d28214a748b7608ab1227
grantcolley <grantcolley@yahoo.com>
2021-08-21 11:59:09 +0100

    Create ComponentArgHelper.cs

---
+++ src/Headway.Core/Helpers/ComponentArgHelper.cs
@@ -0,0 +1,70 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Headway.Core.Helpers
+{
+    public static class ComponentArgHelper
+    {
+        public static List<Arg> GetArgs(IEnumerable<DynamicArg> dynamicArgs)
+        {
+            var args = new List<Arg>();
+
+            foreach (var dynamicArg in dynamicArgs)
+            {
+                var arg = new Arg { Name = dynamicArg.Name };
+
+                if (dynamicArg.Value is DynamicField field)
+                {
+                    arg.Value = field.PropertyInfo.GetValue(field.Model)?.ToString();
+                }
+                else
+                {
+                    arg.Value = dynamicArg.Value.ToString();
+                }
+
+                args.Add(arg);
+            }
+
+            return args;
+        }
+
+        public static void AddDynamicArgs(List<DynamicField> dynamicFields)
+        {
+            foreach (var dynamicField in dynamicFields)
+            {
+                var dynamicArgs = new List<DynamicArg>();
+
+                if (!string.IsNullOrWhiteSpace(dynamicField.ComponentArgs))
+                {
+                    var componentArgs = dynamicField.ComponentArgs.Split(';');
+
+                    foreach (var componentArg in componentArgs)
+                    {
+                        var nameValue = componentArg.Split(',');
+                        var name = nameValue[0].Split('=');
+                        var value = nameValue[1].Split('=');
+
+                        var dynamicArg = new DynamicArg { Name = name[1] };
+
+                        var field = dynamicFields.SingleOrDefault(f => f.PropertyName.Equals(value[1]));
+
+                        if (field != null)
+                        {
+                            dynamicArg.Value = field;
+                        }
+                        else
+                        {
+                            dynamicArg.Value = value[1];
+                        }
+
+                        dynamicArgs.Add(dynamicArg);
+                    }
+                }
+
+                dynamicField.Parameters.Add("ComponentArgs", dynamicArgs);
+            }
+        }
+    }
+}
