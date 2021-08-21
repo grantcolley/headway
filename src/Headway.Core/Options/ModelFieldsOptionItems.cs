6d72226245df77bc54fae132425816d679b813c3
grantcolley <grantcolley@yahoo.com>
2021-08-21 12:00:57 +0100

    Update ModelFieldsOptionItems.cs

--- src/Headway.Core/Options/ModelFieldsOptionItems.cs
+++ src/Headway.Core/Options/ModelFieldsOptionItems.cs
@@ -10,18 +10,20 @@ namespace Headway.Core.Options
 {
     public class ModelFieldsOptionItems : IOptionItems
     {
-        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync()
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<Arg> args)
         {
             var models = TypeAttributeHelper.GetHeadwayTypesByAttribute(typeof(DynamicModelAttribute));
 
-            var optionItems = from m in models
+            List<OptionItem> optionItems= new List<OptionItem>() { new OptionItem() };
+
+            optionItems.AddRange((from m in models
                               select new OptionItem
                               {
                                   Id = m.Namespace,
                                   Display = m.DisplayName
-                              };
+                              }).ToList());
 
-            return Task.FromResult(optionItems);
+            return Task.FromResult(optionItems.AsEnumerable());
         }
     }
 }
