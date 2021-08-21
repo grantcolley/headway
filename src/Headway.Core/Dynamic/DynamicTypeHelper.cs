8340c468443ad9f54f55b8c0b2c4fcff92f668a1
grantcolley <grantcolley@yahoo.com>
2021-08-21 16:38:48 +0100

    Update DynamicTypeHelper.cs

--- src/Headway.Core/Dynamic/DynamicTypeHelper.cs
+++ src/Headway.Core/Dynamic/DynamicTypeHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Headway.Core.Helpers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -105,7 +106,7 @@ namespace Headway.Core.Dynamic
                 return (DynamicTypeHelper<T>)cache[t];
             }
 
-            var propertyInfos = GetPropertyInfos<T>();
+            var propertyInfos = PropertyInfoHelper.GetPropertyInfos(t);
             return Get<T>(propertyInfos);
         }
 
@@ -208,49 +209,6 @@ namespace Headway.Core.Dynamic
         private static int GetNextCounterValue()
         {
             return Interlocked.Increment(ref counter);
-        }
-
-        private static IEnumerable<PropertyInfo> GetPropertyInfos<T>()
-        {
-            var propertyInfoResults = new List<PropertyInfo>();
-
-            PropertyInfo[] propertyInfos = typeof(T).GetProperties();
-
-            foreach (var propertyInfo in propertyInfos)
-            {
-                if (UnsupportedProperty(propertyInfo))
-                {
-                    continue;
-                }
-
-                propertyInfoResults.Add(propertyInfo);
-            }
-
-            return propertyInfoResults;
-        }
-
-        private static bool UnsupportedProperty(PropertyInfo propertyInfo)
-        {
-            // Skip non-public properties and properties that are either 
-            // classes (but not strings), interfaces, lists, generic 
-            // lists or arrays.
-            var propertyType = propertyInfo.PropertyType;
-
-            if (propertyType != typeof(string)
-                && (propertyType.IsClass
-                    || propertyType.IsInterface
-                    || propertyType.IsArray
-                    || propertyType.GetInterfaces()
-                        .Any(
-                            i =>
-                                (i.GetTypeInfo().Name.Equals(typeof(IEnumerable).Name)
-                                 || (i.IsGenericType &&
-                                     i.GetGenericTypeDefinition().Name.Equals(typeof(IEnumerable<>).Name))))))
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
