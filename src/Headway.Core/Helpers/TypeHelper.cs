4870aceae8058bbf13abc085aa056528d14462d9
grantcolley <grantcolley@yahoo.com>
2021-08-30 16:46:21 +0100

    Create TypeHelper.cs

---
+++ src/Headway.Core/Helpers/TypeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Headway.Core.Helpers
+{
+    public static class TypeHelper<T>
+    {
+        private static Func<T> creator =
+            Expression.Lambda<Func<T>>(Expression.New(typeof(T).GetConstructor(Type.EmptyTypes)))
+            .Compile();
+
+        public static T Create()
+        {
+            return creator();
+        }
+    }
+}
