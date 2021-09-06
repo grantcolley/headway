01712ca60e44b6110b0b92917a5227363531f741
grantcolley <grantcolley@yahoo.com>
2021-09-06 19:01:24 +0100

    Update Sql Migrations
    Update Sql Migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210906175034_Headway_ConfigItem_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ConfigItem_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfigName",
+                table: "ConfigItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfigName",
+                table: "ConfigItems");
+        }
+    }
+}
