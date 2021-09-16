5a6b4ec1647dde6e3a18a2efabe2ca890014212b
grantcolley <grantcolley@yahoo.com>
2021-09-16 20:19:59 +0100

    Update DynamicField.cs

--- src/Headway.Core/Dynamic/DynamicField.cs
+++ src/Headway.Core/Dynamic/DynamicField.cs
@@ -15,6 +15,7 @@ namespace Headway.Core.Dynamic
         public object Model { get; set; }
         public int Order { get; set; }
         public string Label { get; set; }
+        public string ConfigName { get; set; }
         public string PropertyName { get; set; }
         public string ComponentArgs { get; set; }
         public string DynamicComponentTypeName { get; set; }
