d07950ed4d028e789a24de9cfe35c7a8cca312d0
grantcolley <grantcolley@yahoo.com>
2021-09-17 20:49:36 +0100

    Add ListDetail.razor component
    Add ListDetail.razor component

---
+++ src/Headway.Razor.Controls/Components/ListDetail.razor.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public class ListDetailBase : DynamicComponentBase
+    {
+    }
+}
