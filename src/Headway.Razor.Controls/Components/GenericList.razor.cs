08a2ecffe784561f68d45f0b3ec9d892be0ae598
grantcolley <grantcolley@yahoo.com>
2021-09-17 20:49:15 +0100

    Add GenericList.razor component
    Add GenericList.razor component

---
+++ src/Headway.Razor.Controls/Components/GenericList.razor.cs
@@ -0,0 +1,25 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Model;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Components
+{
+    public class GenericListBase : ConfigComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        [Parameter]
+        public List<DynamicArg> ComponentArgs { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetConfig(Field.ConfigName).ConfigureAwait(false);
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
