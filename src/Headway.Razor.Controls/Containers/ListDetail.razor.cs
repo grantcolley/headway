2da6f3711d70e004b0ed540458db20a8faf6911f
grantcolley <grantcolley@yahoo.com>
2021-09-19 21:00:08 +0100

    Update ListDetail.razor.cs

--- src/Headway.Razor.Controls/Containers/ListDetail.razor.cs
+++ src/Headway.Razor.Controls/Containers/ListDetail.razor.cs
@@ -33,6 +33,12 @@ namespace Headway.Razor.Controls.Containers
         {
             list = (List<T>)Field.PropertyInfo.GetValue(Field.Model, null);
 
+            // https://softwareengineering.stackexchange.com/questions/287980/generic-sorting-of-lists
+            // get the dynamicModel for ConfigItem then get the propertyInfo for the Order field
+            // List<Order> SortedList = aList.OrderBy(o=>prop.GetValue(o)).ToList();
+
+            // Consider re-introducing the TypeHelper which will be faster than reflection on big lists. 
+
             await base.OnInitializedAsync().ConfigureAwait(false);
         }
     }
