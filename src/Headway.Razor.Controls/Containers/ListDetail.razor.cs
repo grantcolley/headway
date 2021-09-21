2112a009e3c3722b89b5714a2e09930e73b0da15
grantcolley <grantcolley@yahoo.com>
2021-09-21 13:56:01 +0100

    Update ListDetail.razor and ListDetail.razor
    Update ListDetail.razor and ListDetail.razor

--- src/Headway.Razor.Controls/Containers/ListDetail.razor.cs
+++ src/Headway.Razor.Controls/Containers/ListDetail.razor.cs
@@ -11,11 +11,8 @@ using System.Threading.Tasks;
 namespace Headway.Razor.Controls.Containers
 {
     [DynamicContainer]
-    public abstract class ListDetailBase<T> : GenericComponentBase<T>
+    public abstract class ListDetailBase<T> : GenericComponentBase<T> where T : class, new()
     {
-        [Inject]
-        public IDynamicService DynamicService { get; set; }
-
         [Parameter]
         public DynamicField Field { get; set; }
 
@@ -27,11 +24,13 @@ namespace Headway.Razor.Controls.Containers
 
         protected DynamicModel<T> dynamicModel;
 
-        protected List<T> list;
+        protected DynamicList<T> dynamicList;
 
         protected override async Task OnInitializedAsync()
         {
-            list = (List<T>)Field.PropertyInfo.GetValue(Field.Model, null);
+            var list = (List<T>)Field.PropertyInfo.GetValue(Field.Model, null);
+
+            dynamicList = await GetDynamicList(list, "ConfigItems").ConfigureAwait(false);
 
             // https://softwareengineering.stackexchange.com/questions/287980/generic-sorting-of-lists
             // get the dynamicModel for ConfigItem then get the propertyInfo for the Order field
@@ -40,6 +39,14 @@ namespace Headway.Razor.Controls.Containers
             // Consider re-introducing the TypeHelper which will be faster than reflection on big lists. 
 
             await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected void Add()
+        {
+        }
+
+        protected void Update(object id)
+        {
         }
     }
 }
