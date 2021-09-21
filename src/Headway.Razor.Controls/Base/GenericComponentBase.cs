d81ba4bea1c78e99a55a46f75d5e8f2ccb4ad959
grantcolley <grantcolley@yahoo.com>
2021-09-21 13:55:30 +0100

    Update GenericComponentBase.cs

--- src/Headway.Razor.Controls/Base/GenericComponentBase.cs
+++ src/Headway.Razor.Controls/Base/GenericComponentBase.cs
@@ -1,6 +1,22 @@
-﻿namespace Headway.Razor.Controls.Base
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Base
 {
-    public abstract class GenericComponentBase<T> : HeadwayComponentBase
+    public abstract class GenericComponentBase<T> : HeadwayComponentBase where T : class, new()
     {
+        [Inject]
+        public IDynamicService DynamicService { get; set; }
+
+        protected async Task<DynamicList<T>> GetDynamicList(IEnumerable<T> list, string config)
+        {
+            var result = await DynamicService.GetDynamicListAsync<T>(list, config)
+                .ConfigureAwait(false);
+
+            return GetResponse(result);
+        }
     }
 }
