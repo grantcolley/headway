4f1d6b2c7e1b7c269647ebe8e48bf4eb580a6058
grantcolley <grantcolley@yahoo.com>
2021-09-22 18:55:18 +0100

    Update Label.razor.cs

--- src/Headway.Razor.Controls/Components/Label.razor.cs
+++ src/Headway.Razor.Controls/Components/Label.razor.cs
@@ -6,5 +6,12 @@ namespace Headway.Razor.Controls.Components
     [DynamicComponent]
     public class LabelBase : DynamicComponentBase
     {
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
     }
 }
