00d0e00903e1175466a7eab54bd50d9b9b823aa8
grantcolley <grantcolley@yahoo.com>
2021-09-22 18:55:09 +0100

    Update Dropdown.razor.cs

--- src/Headway.Razor.Controls/Components/Dropdown.razor.cs
+++ src/Headway.Razor.Controls/Components/Dropdown.razor.cs
@@ -1,10 +1,11 @@
 ﻿using Headway.Core.Attributes;
-using Headway.Core.Helpers;
 using Headway.Core.Interface;
 using Headway.Core.Model;
 using Headway.Razor.Controls.Base;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Components
@@ -16,6 +17,27 @@ namespace Headway.Razor.Controls.Components
         public IOptionsService OptionsService { get; set; }
 
         protected IEnumerable<OptionItem> OptionItems;
+
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
+
+        public virtual void OnValueChanged(string value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
 
         protected override async Task OnParametersSetAsync()
         {
