60c56156de5e9aaca5f7252acfcb0b51cc3061d4
grantcolley <grantcolley@yahoo.com>
2021-09-22 18:55:13 +0100

    Update Text.razor.cs

--- src/Headway.Razor.Controls/Components/Text.razor.cs
+++ src/Headway.Razor.Controls/Components/Text.razor.cs
@@ -1,11 +1,33 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Razor.Controls.Base;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq.Expressions;
 
 namespace Headway.Razor.Controls.Components
 {
     [DynamicComponent]
     public class TextBase : DynamicComponentBase
     {
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+        }
+
+        public virtual void OnValueChanged(string value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
     }
 }
