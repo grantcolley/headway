511bb6548bc4ff3053bba62a5697540cb7a310ac
grantcolley <grantcolley@yahoo.com>
2021-09-22 18:55:58 +0100

    Create Integer.razor
    Create Integer.razor

---
+++ src/Headway.Razor.Controls/Components/Integer.razor.cs
@@ -0,0 +1,38 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public class IntegerBase : DynamicComponentBase
+    {
+        public Expression<Func<int>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<int>>(Field.MemberExpression);
+            }
+        }
+
+        public int PropertyValue
+        {
+            get
+            {
+                return (int)Field.PropertyInfo.GetValue(Field.Model);
+            }
+        }
+
+        public virtual void OnValueChanged(int value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+
+    public class IntegerNumber : InputNumber<int>
+    {
+    }
+}
