5c434d309ca78a7ebcd9dfcfd04f48dee0c85b7a
grantcolley <grantcolley@yahoo.com>
2021-09-22 18:55:01 +0100

    Create Checbox.razor
    Create Checbox.razor

---
+++ src/Headway.Razor.Controls/Components/Checkbox.razor.cs
@@ -0,0 +1,32 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using System;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public class CheckboxBase : DynamicComponentBase
+    {
+        public Expression<Func<bool>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<bool>>(Field.MemberExpression);
+            }
+        }
+
+        public bool PropertyValue
+        {
+            get
+            {
+                return (bool)Field.PropertyInfo.GetValue(Field.Model);
+            }
+        }
+
+        public virtual void OnValueChanged(bool value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+}
