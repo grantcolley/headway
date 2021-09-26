0d4436444f26adea4f9ff43297d1c617d004221c
grantcolley <grantcolley@yahoo.com>
2021-09-26 12:49:00 +0100

    Add SqlServer migrations
    Add SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20210926114441_Headway_ConfigContainer_1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ConfigContainer_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsTitle",
+                table: "ConfigItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsIdentity",
+                table: "ConfigItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ConfigContainers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ConfigContainers");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsTitle",
+                table: "ConfigItems",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsIdentity",
+                table: "ConfigItems",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
