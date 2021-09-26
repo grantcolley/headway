500bfa9970884e7b8ba825750082e7fd1c1f1d84
grantcolley <grantcolley@yahoo.com>
2021-09-26 12:24:21 +0100

    Create DropdownComplex.razor
    Create DropdownComplex.razor

---
+++ src/Headway.Razor.Controls/Components/DropdownComplex.razor.cs
@@ -0,0 +1,50 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Interface;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public class DropdownComplexBase<T> : DynamicComponentBase
+    {
+        [Inject]
+        public IOptionsService OptionsService { get; set; }
+
+        protected IEnumerable<T> OptionItems;
+
+        public Expression<Func<T>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<T>>(Field.MemberExpression);
+            }
+        }
+
+        public T PropertyValue
+        {
+            get
+            {
+                return (T)Field.PropertyInfo.GetValue(Field.Model);
+            }
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var result = await OptionsService.GetOptionItemsAsync<T>(ComponentArgs).ConfigureAwait(false);
+
+            OptionItems = GetResponse(result);
+
+            await base.OnParametersSetAsync();
+        }
+
+        public virtual void OnValueChanged(T value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+}
