3ad4a9d67ff0b7d616bb9facb82bee4224da32ac
grantcolley <grantcolley@yahoo.com>
2021-09-27 20:38:13 +0100

    Create DropdownComplex.eazor
    Create DropdownComplex.eazor

--- src/Headway.Razor.Controls/Components/DropdownComplex.razor.cs
+++ src/Headway.Razor.Controls/Components/DropdownComplex.razor.cs
@@ -1,9 +1,13 @@
 ﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Helpers;
 using Headway.Core.Interface;
 using Headway.Razor.Controls.Base;
+using Headway.Razor.Controls.Model;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -15,7 +19,7 @@ namespace Headway.Razor.Controls.Components
         [Inject]
         public IOptionsService OptionsService { get; set; }
 
-        protected IEnumerable<T> OptionItems;
+        protected IEnumerable<GenericItem<T>> OptionItems;
 
         public Expression<Func<T>> FieldExpression
         {
@@ -37,7 +41,14 @@ namespace Headway.Razor.Controls.Components
         {
             var result = await OptionsService.GetOptionItemsAsync<T>(ComponentArgs).ConfigureAwait(false);
 
-            OptionItems = GetResponse(result);
+            var optionItems = GetResponse(result);
+
+            if (optionItems.Any())
+            {
+                var displayName = ComponentArgs.Single(a => a.Name.Equals(Options.DISPLAY_FIELD)).Value.ToString();
+                var propertyInfo = PropertyInfoHelper.GetPropertyInfo(optionItems.First().GetType(), displayName);
+                OptionItems = optionItems.Select( oi => new GenericItem<T>(oi, propertyInfo));
+            }
 
             await base.OnParametersSetAsync().ConfigureAwait(false);
         }
