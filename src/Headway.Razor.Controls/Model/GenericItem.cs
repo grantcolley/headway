c38148c4036dffbb8f5e7aeb6c8c040f379f91f8
grantcolley <grantcolley@yahoo.com>
2021-09-27 20:38:18 +0100

    Create GenericItem.cs

---
+++ src/Headway.Razor.Controls/Model/GenericItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Headway.Razor.Controls.Model
+{
+    public class GenericItem<T>
+    {
+        public GenericItem(T item, PropertyInfo propertyInfo)
+        {
+            Item = item;
+
+            var name = propertyInfo.GetValue(item);
+
+            if(name != null)
+            {
+                Name = name.ToString();
+            }
+        }
+
+        public T Item {  get; private set; }
+        public string Name { get; private set; }
+    }
+}
