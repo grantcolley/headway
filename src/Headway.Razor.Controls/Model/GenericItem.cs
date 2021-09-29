6cddba003a59fa0f2302543aa037c4d5d73f035f
grantcolley <grantcolley@yahoo.com>
2021-09-29 21:00:22 +0100

    Update GenericItem.cs

--- src/Headway.Razor.Controls/Model/GenericItem.cs
+++ src/Headway.Razor.Controls/Model/GenericItem.cs
@@ -1,22 +1,43 @@
-﻿using System.Reflection;
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.Json;
 
 namespace Headway.Razor.Controls.Model
 {
     public class GenericItem<T>
     {
+        private List<Arg> args;
+
         public GenericItem(T item, PropertyInfo propertyInfo)
         {
             Item = item;
 
             var name = propertyInfo.GetValue(item);
 
-            if(name != null)
+            if (name != null)
             {
                 Name = name.ToString();
             }
+
+            args = new List<Arg>()
+            {
+                new Arg { Name = "Type", Value = $"{Item.GetType().FullName}, {Item.GetType().Assembly.GetName().Name}" },
+                new Arg { Name = "Value", Value = JsonSerializer.Serialize(Item)}
+            };
         }
 
-        public T Item {  get; private set; }
+        public T Item { get; private set; }
         public string Name { get; private set; }
+        public string Args
+        {
+            get { return JsonSerializer.Serialize(args); }
+            set
+            {
+                if (args.ToString() != value)
+                {
+                }
+            }
+        }
     }
 }
