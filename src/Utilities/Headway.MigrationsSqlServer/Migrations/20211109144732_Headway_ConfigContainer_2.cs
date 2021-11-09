54ec0ac0adf3a10b7d3fe57cd84880a1122c0088
grantcolley <grantcolley@yahoo.com>
2021-11-09 14:49:58 +0000

    Sql Server migrations
    Sql Server migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20211109144732_Headway_ConfigContainer_2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ConfigContainer_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "ConfigContainers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "ConfigContainers");
+        }
+    }
+}
