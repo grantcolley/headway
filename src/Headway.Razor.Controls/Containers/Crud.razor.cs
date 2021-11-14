6a5cca39aafaaca93382848b1efe6ba6ec8a5df0
grantcolley <grantcolley@yahoo.com>
2021-11-14 09:38:56 +0000

    Create Crud.razor and Crud.razor.cs
    Create Crud.razor and Crud.razor.cs

---
+++ src/Headway.Razor.Controls/Containers/Crud.razor.cs
@@ -0,0 +1,120 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Headway.Razor.Controls.Base;
+using Headway.Razor.Controls.Model;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Containers
+{
+    [DynamicContainer]
+    public abstract class CrudBase<T> : DynamicContainerBase<T> where T : class, new()
+    {
+        [Inject]
+        public IDynamicService DynamicService { get; set; }
+
+        [Parameter]
+        public string Config { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected Alert Alert { get; set; }
+        protected bool isSaveInProgress = false;
+        protected bool isDeleteInProgress = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IServiceResult<DynamicModel<T>> serviceResult;
+
+            if (Id.Equals(0))
+            {
+                serviceResult = await DynamicService
+                    .CreateDynamicModelInstanceAsync<T>(Config)
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                serviceResult = await DynamicService
+                    .GetDynamicModelAsync<T>(Id, Config)
+                    .ConfigureAwait(false);
+            }
+
+            DynamicModel = GetResponse(serviceResult);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected async Task Submit()
+        {
+            isSaveInProgress = true;
+
+            IServiceResult<DynamicModel<T>> serviceResult;
+            string message;
+
+            if (DynamicModel.Id.Equals(0))
+            {
+                serviceResult = await DynamicService
+                    .AddDynamicModelAsync<T>(DynamicModel)
+                    .ConfigureAwait(false);
+
+                message = "has been added.";
+            }
+            else
+            {
+                serviceResult = await DynamicService
+                    .UpdateDynamicModelAsync<T>(DynamicModel)
+                    .ConfigureAwait(false);
+
+                message = "has been updated.";
+            }
+
+            DynamicModel = GetResponse(serviceResult);
+
+            if (DynamicModel == null)
+            {
+                return;
+            }
+
+            Alert = new Alert
+            {
+                AlertType = Alerts.PRIMARY,
+                Title = DynamicModel.Title,
+                Message = message,
+                //RedirectText = dynamicModel.Config.NavigateText,
+                RedirectPage = DynamicModel.Config.NavigateTo
+            };
+
+            isSaveInProgress = false;
+        }
+
+        protected async Task Delete()
+        {
+            isDeleteInProgress = true;
+
+            var serviceResult = await DynamicService
+                .DeleteDynamicModelAsync(DynamicModel)
+                .ConfigureAwait(false);
+
+            var result = GetResponse(serviceResult);
+
+            if (result.Equals(0))
+            {
+                return;
+            }
+
+            Alert = new Alert
+            {
+                AlertType = Alerts.DANGER,
+                Title = DynamicModel.Title,
+                Message = $"has been deleted.",
+                //RedirectText = dynamicModel.Config.NavigateText,
+                RedirectPage = DynamicModel.Config.NavigateTo
+            };
+
+            isDeleteInProgress = false;
+        }
+    }
+}
