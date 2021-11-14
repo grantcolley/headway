9a177af107ce961f12b9ebd906e41d0e28a9f64e
grantcolley <grantcolley@yahoo.com>
2021-11-14 10:04:21 +0000

    Create TabPage component
    Create TabPage component

---
+++ src/Headway.Razor.Controls/Components/TabPage.razor.cs
@@ -0,0 +1,32 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class TabPageBase : DynamicComponentBase
+    {
+        [CascadingParameter]
+        protected TabControl Parent { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string TabButtonText { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Parent == null)
+            {
+                throw new ArgumentNullException(nameof(Parent), "TabPage must have a parent TabControl");
+            }
+
+            base.OnInitialized();
+
+            Parent.AddPage(this);
+        }
+    }
+}
