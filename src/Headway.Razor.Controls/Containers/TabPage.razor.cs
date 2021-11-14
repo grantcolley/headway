69537ab66ba84cdbb6e1a2d7451bcb3d2a89f690
grantcolley <grantcolley@yahoo.com>
2021-11-14 09:38:26 +0000

    Comment out Crud'able TabControl
    Comment out Crud'able TabControl

--- src/Headway.Razor.Controls/Containers/TabPage.razor.cs
+++ src/Headway.Razor.Controls/Containers/TabPage.razor.cs
@@ -1,32 +1,32 @@
-﻿using Headway.Core.Attributes;
-using Headway.Razor.Controls.Base;
-using Microsoft.AspNetCore.Components;
-using System;
+﻿//using Headway.Core.Attributes;
+//using Headway.Razor.Controls.Base;
+//using Microsoft.AspNetCore.Components;
+//using System;
 
-namespace Headway.Razor.Controls.Containers
-{
-    [DynamicContainer]
-    public abstract class TabPageBase<T> : DynamicContainerBase<T> where T : class, new()
-    {
-        [CascadingParameter]
-        protected TabControlBase<T> Parent { get; set; }
+//namespace Headway.Razor.Controls.Containers
+//{
+//    [DynamicContainer]
+//    public abstract class TabPageBase<T> : DynamicContainerBase<T> where T : class, new()
+//    {
+//        [CascadingParameter]
+//        protected TabControlBase<T> Parent { get; set; }
 
-        [Parameter]
-        public RenderFragment ChildContent { get; set; }
+//        [Parameter]
+//        public RenderFragment ChildContent { get; set; }
 
-        [Parameter]
-        public string TabButtonText { get; set; }
+//        [Parameter]
+//        public string TabButtonText { get; set; }
 
-        protected override void OnInitialized()
-        {
-            if (Parent == null)
-            {
-                throw new ArgumentNullException(nameof(Parent), "TabPage must have a parent TabControl");
-            }
+//        protected override void OnInitialized()
+//        {
+//            if (Parent == null)
+//            {
+//                throw new ArgumentNullException(nameof(Parent), "TabPage must have a parent TabControl");
+//            }
 
-            base.OnInitialized();
+//            base.OnInitialized();
 
-            Parent.AddPage((TabPage<T>)this);
-        }
-    }
-}
+//            Parent.AddPage((TabPage<T>)this);
+//        }
+//    }
+//}
