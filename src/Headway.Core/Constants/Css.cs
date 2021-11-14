fe982b755ad5a14e9a1abdd39343ceee6687fbd4
grantcolley <grantcolley@yahoo.com>
2021-11-14 09:13:51 +0000

    Create Css.cs

---
+++ src/Headway.Core/Constants/Css.cs
@@ -0,0 +1,8 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class Css
+    {
+        public const string BTN_PRIMARY = "btn-primary";
+        public const string BTN_SECONDARY = "btn-secondary";
+    }
+}
