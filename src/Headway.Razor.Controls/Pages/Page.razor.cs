49f5107bb616e26aa8e1dcc7b7cc5af5cc123f49
grantcolley <grantcolley@yahoo.com>
2021-11-25 20:22:45 +0000

    Add Page.razor.cs and Page.razor.cs
    Add Page.razor.cs and Page.razor.cs

---
+++ src/Headway.Razor.Controls/Pages/Page.razor.cs
@@ -0,0 +1,49 @@
+﻿using Headway.Core.Constants;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Pages
+{
+    public class PageBase : ConfigComponentBase
+    {
+        [Parameter]
+        public string Config { get; set; }
+
+        [Parameter]
+        public int? Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (config == null)
+            {
+                await GetConfig(Config).ConfigureAwait(false);
+            }
+            else if (!config.Name.Equals(Config))
+            {
+                config = null;
+
+                await GetConfig(Config).ConfigureAwait(false);
+            }
+
+            await base.OnParametersSetAsync().ConfigureAwait(false);
+        }
+
+        protected RenderFragment RenderView() => __builder =>
+        {
+            var type = Type.GetType(config.Model);
+            var component = Type.GetType(config.Container);
+            var genericType = component.MakeGenericType(new[] { type });
+            __builder.OpenComponent(1, genericType);
+            __builder.AddAttribute(2, Parameters.CONFIG, config.Name);
+
+            if (Id.HasValue)
+            {
+                __builder.AddAttribute(3, Parameters.ID, Id);
+            }
+
+            __builder.CloseComponent();
+        };
+    }
+}
