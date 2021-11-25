dbbbc8e065d2fd29b0737301d054822f2f3fe055
grantcolley <grantcolley@yahoo.com>
2021-11-25 20:32:43 +0000

    Comment out obsolete List and Model components prior to deleting
    Comment out obsolete List and Model components prior to deleting

--- src/Headway.Razor.Controls/Pages/Model.razor.cs
+++ src/Headway.Razor.Controls/Pages/Model.razor.cs
@@ -1,37 +1,37 @@
-﻿using Headway.Core.Attributes;
-using Headway.Core.Constants;
-using Headway.Razor.Controls.Base;
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Threading.Tasks;
+﻿//using Headway.Core.Attributes;
+//using Headway.Core.Constants;
+//using Headway.Razor.Controls.Base;
+//using Microsoft.AspNetCore.Components;
+//using System;
+//using System.Threading.Tasks;
 
-namespace Headway.Razor.Controls.Pages
-{
-    [DynamicPage]
-    public abstract class ModelBase : ConfigComponentBase
-    {
-        [Parameter]
-        public string Config { get; set; }
+//namespace Headway.Razor.Controls.Pages
+//{
+//    [DynamicPage]
+//    public abstract class ModelBase : ConfigComponentBase
+//    {
+//        [Parameter]
+//        public string Config { get; set; }
 
-        [Parameter]
-        public int Id { get; set; }
+//        [Parameter]
+//        public int Id { get; set; }
 
-        protected override async Task OnInitializedAsync()
-        {
-            await GetConfig(Config).ConfigureAwait(false);
+//        protected override async Task OnInitializedAsync()
+//        {
+//            await GetConfig(Config).ConfigureAwait(false);
 
-            await base.OnInitializedAsync().ConfigureAwait(false);
-        }
+//            await base.OnInitializedAsync().ConfigureAwait(false);
+//        }
 
-        protected RenderFragment RenderView() => __builder =>
-        {
-            var type = Type.GetType(config.Model);
-            var component = Type.GetType(config.Container);
-            var genericType = component.MakeGenericType(new[] { type });
-            __builder.OpenComponent(1, genericType);
-            __builder.AddAttribute(2, Parameters.CONFIG, config.Name);
-            __builder.AddAttribute(3, Parameters.ID, Id);
-            __builder.CloseComponent();
-        };
-    }
-}
+//        protected RenderFragment RenderView() => __builder =>
+//        {
+//            var type = Type.GetType(config.Model);
+//            var component = Type.GetType(config.Container);
+//            var genericType = component.MakeGenericType(new[] { type });
+//            __builder.OpenComponent(1, genericType);
+//            __builder.AddAttribute(2, Parameters.CONFIG, config.Name);
+//            __builder.AddAttribute(3, Parameters.ID, Id);
+//            __builder.CloseComponent();
+//        };
+//    }
+//}
