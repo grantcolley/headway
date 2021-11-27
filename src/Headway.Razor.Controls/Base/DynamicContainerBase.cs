df86e11daf2f91adf98fa145e89f7187957d627f
grantcolley <grantcolley@yahoo.com>
2021-11-27 19:45:32 +0000

    Update DynamicContainerBase.cs

--- src/Headway.Razor.Controls/Base/DynamicContainerBase.cs
+++ src/Headway.Razor.Controls/Base/DynamicContainerBase.cs
@@ -1,11 +1,24 @@
 ﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
 using Microsoft.AspNetCore.Components;
 
 namespace Headway.Razor.Controls.Base
 {
-    public abstract class DynamicContainerBase<T> : DynamicModelContainerBase<T> where T : class, new()
+    public abstract class DynamicContainerBase<T> : HeadwayComponentBase where T : class, new()
     {
+        [Inject]
+        protected IDynamicService DynamicService { get; set; }
+
         [Parameter]
         public DynamicContainer Container { get; set; }
+
+        [Parameter]
+        public DynamicModel<T> DynamicModel { get; set; }
+
+        [Parameter]
+        public string Config { get; set; }
+
+        [Parameter]
+        public int? Id { get; set; }
     }
 }
