8c9d8ed974d9f290b3459c45ef5587a4d0d1ecc8
grantcolley <grantcolley@yahoo.com>
2021-11-28 20:50:03 +0000

    Create GenericField.razor
    Create GenericField.razor

---
+++ src/Headway.Razor.Controls/Components/GenericField.razor.cs
@@ -0,0 +1,41 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Dynamic;
+using Headway.Core.Model;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class GenericFieldBase : ConfigComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+
+        [Parameter]
+        public List<DynamicArg> ComponentArgs { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetConfig(Field.ConfigName).ConfigureAwait(false);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected RenderFragment RenderView() => builder =>
+        {
+            var type = Type.GetType(config.Model);
+            var component = Type.GetType(config.Container);
+            var genericType = component.MakeGenericType(new[] { type });
+            builder.OpenComponent(1, genericType);
+            builder.AddAttribute(2, Parameters.FIELD, Field);
+            builder.AddAttribute(3, Parameters.COMPONENT_ARGS, ComponentArgs);
+            builder.AddAttribute(4, Parameters.CONFIG, config);
+            builder.CloseComponent();
+        };
+    }
+}
