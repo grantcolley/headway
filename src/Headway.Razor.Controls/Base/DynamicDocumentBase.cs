5f59e7c1933734ff2eb2ad04ec68077ed09e27c1
grantcolley <grantcolley@yahoo.com>
2021-12-05 10:25:24 +0000

    Replace DynamicContainerBase<T> with DynamicDocumentBase<T>
    Replace DynamicContainerBase<T> with DynamicDocumentBase<T>

---
+++ src/Headway.Razor.Controls/Base/DynamicDocumentBase.cs
@@ -0,0 +1,55 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Base
+{
+    public abstract class DynamicDocumentBase<T> : HeadwayComponentBase where T : class, new()
+    {
+        [Inject]
+        protected IDynamicService DynamicService { get; set; }
+
+        [Parameter]
+        public string Config { get; set; }
+
+        [Parameter]
+        public int? Id { get; set; }
+
+        protected DynamicModel<T> dynamicModel { get; set; }
+
+        protected DynamicList<T> dynamicList;
+
+        protected virtual async Task InitializeDynamicModelAsync()
+        {
+            IServiceResult<DynamicModel<T>> serviceResult;
+
+            if (!Id.HasValue
+                || Id.Value.Equals(0))
+            {
+                serviceResult = await DynamicService
+                    .CreateDynamicModelInstanceAsync<T>(Config)
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                serviceResult = await DynamicService
+                    .GetDynamicModelAsync<T>(Id.Value, Config)
+                    .ConfigureAwait(false);
+            }
+
+            dynamicModel = GetResponse(serviceResult);
+        }
+
+        protected virtual async Task InitializeDynamicListAsync()
+        {
+            var result =
+                await DynamicService.GetDynamicListAsync<T>(Config)
+                .ConfigureAwait(false);
+
+            dynamicList = GetResponse(result);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+    }
+}
