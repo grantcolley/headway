841b6fab3c1625c8a8b824d28ecfda1c54322fff
grantcolley <grantcolley@yahoo.com>
2021-12-11 10:55:03 +0000

    Create ContainerOptionItems.cs

---
+++ src/Headway.Core/Options/ContainerOptionItems.cs
@@ -0,0 +1,30 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Options
+{
+    public class ContainerOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<Arg> args)
+        {
+            var containers = TypeAttributeHelper.GetHeadwayTypesByAttribute(typeof(DynamicContainerAttribute));
+
+            List<OptionItem> optionItems = new() { new OptionItem() };
+
+            optionItems.AddRange((from c in containers
+                                  orderby c.Name
+                                  select new OptionItem
+                                  {
+                                      Id = c.Namespace,
+                                      Display = c.DisplayName
+                                  }).ToList());
+
+            return Task.FromResult(optionItems.AsEnumerable());
+        }
+    }
+}
