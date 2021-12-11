b4ec09bdedd05bdab75313634a7d994e3fff7c74
grantcolley <grantcolley@yahoo.com>
2021-12-11 19:30:15 +0000

    Create DynamicContainerBase.cs

---
+++ src/Headway.Razor.Controls/Base/DynamicContainerBase.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.Razor.Controls.Base
+{
+    public class DynamicContainerBase : ComponentBase
+    {
+        [Parameter]
+        public DynamicContainer Container { get; set; }
+    }
+}
