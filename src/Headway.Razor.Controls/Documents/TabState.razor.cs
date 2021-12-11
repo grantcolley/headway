b10361c3a899bd03e73b5461720bdcb65f8d74d5
grantcolley <grantcolley@yahoo.com>
2021-12-11 18:46:24 +0000

    Update TabState.razor.cs

--- src/Headway.Razor.Controls/Documents/TabState.razor.cs
+++ src/Headway.Razor.Controls/Documents/TabState.razor.cs
@@ -4,7 +4,6 @@ using Headway.Core.Dynamic;
 using Headway.Core.Interface;
 using Headway.Razor.Controls.Base;
 using Headway.Razor.Controls.Model;
-using Microsoft.AspNetCore.Components;
 using System.Linq;
 using System.Threading.Tasks;
 
