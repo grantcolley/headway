280ee3e244ea581f70864b2c9865a6172cb8a62f
grantcolley <grantcolley@yahoo.com>
2021-12-13 19:09:02 +0000

    Update ContainerLayoutBuilder.razor
    Update ContainerLayoutBuilder.razor

--- src/Headway.Razor.Controls/Custom/ContainerLayout/ContainerLayoutBuilder.razor.cs
+++ src/Headway.Razor.Controls/Custom/ContainerLayout/ContainerLayoutBuilder.razor.cs
@@ -1,8 +1,25 @@
-﻿using Headway.Razor.Controls.Base;
+﻿using Headway.Core.Model;
+using Headway.Razor.Controls.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Custom.ContainerLayout
 {
     public abstract class ContainerLayoutBuilderBase : DynamicComponentBase
     {
+        protected List<ConfigContainer> configContainers;
+        protected ConfigContainer activeContainer;
+
+        protected override async Task OnInitializedAsync()
+        {
+            configContainers = (List<ConfigContainer>)Field.PropertyInfo.GetValue(Field.Model, null);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected void EditAsync(ConfigContainer container)
+        {
+            activeContainer = container;
+        }
     }
 }
