63dab4be2470c3ffc1a5de7556c2b1bc486560ba
grantcolley <grantcolley@yahoo.com>
2021-12-19 20:13:18 +0000

    Update Args.cs

--- src/Headway.Core/Constants/Args.cs
+++ src/Headway.Core/Constants/Args.cs
@@ -8,5 +8,8 @@
         public const string SEARCH_PARAMETER = "SearchParameter";
         public const string TYPE = "Type";
         public const string VALUE = "Value";
+        public const string MODEL_LABEL = "ModelLabel";
+        public const string MODEL_LIST_PROPERTY = "ModelListProperty";
+
     }
 }
