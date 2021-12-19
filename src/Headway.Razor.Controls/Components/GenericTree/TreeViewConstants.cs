d22a73c47a1d54056ff90b85ad7001890786bbd8
grantcolley <grantcolley@yahoo.com>
2021-12-19 20:54:42 +0000

    Create TreeViewConstants.cs

---
+++ src/Headway.Razor.Controls/Components/GenericTree/TreeViewConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Headway.Razor.Controls.Components.GenericTree
+{
+    public static class TreeViewConstants
+    {
+        public const string TREENODE_PARAMETER_NODE = "Node";
+        public const string TREEVIEW_PARAMETER_TREE = "Tree";
+        public const string TREEVIEW_PARAMETER_NODE_LABEL = "NodeLabel";
+        public const string TREEVIEW_PARAMETER_NODES_PROPERTY = "NodesProperty";
+    }
+}
