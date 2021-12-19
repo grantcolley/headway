11216c13cf26cfffd2179f89f1d7d6c7b19e06e0
grantcolley <grantcolley@yahoo.com>
2021-12-19 20:55:25 +0000

    Create Node.cs

---
+++ src/Headway.Razor.Controls/Components/GenericTree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Razor.Controls.Components.GenericTree
+{
+    public class Node<T> where T : class, new()
+    {
+        public Node()
+        {
+            Nodes = new List<Node<T>>();
+        }
+
+        public T Model { get; set; }
+        public string Label { get; set; }
+        public Node<T> Source { get; set; }
+        public List<Node<T>> Nodes { get; set; }
+    }
+}
