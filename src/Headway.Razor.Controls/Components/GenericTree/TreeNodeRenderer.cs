e0b02dd1167196821217c5ec19501564c420006e
grantcolley <grantcolley@yahoo.com>
2021-12-22 21:36:56 +0000

    Update TreeNodeRenderer.cs

--- src/Headway.Razor.Controls/Components/GenericTree/TreeNodeRenderer.cs
+++ src/Headway.Razor.Controls/Components/GenericTree/TreeNodeRenderer.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
 using System;
-using System.Collections.Generic;
 
 namespace Headway.Razor.Controls.Components.GenericTree
 {
     public static class TreeNodeRenderer
     {
-        public static RenderFragment RenderTreeView<T>(List<T> tree, string nodeLabel, string nodesProperty) where T : class, new()
+        public static RenderFragment RenderTreeView<T>(DynamicField field, string nodeLabel, string nodesProperty) where T : class, new()
             => builder =>
         {
             var genericType = Type.GetType(typeof(TreeView<T>).AssemblyQualifiedName);
             builder.OpenComponent(1, genericType);
-            builder.AddAttribute(2, TreeViewConstants.TREEVIEW_PARAMETER_TREE, tree);
+            builder.AddAttribute(2, TreeViewConstants.TREEVIEW_PARAMETER_FIELD, field);
             builder.AddAttribute(3, TreeViewConstants.TREEVIEW_PARAMETER_NODE_LABEL, nodeLabel);
             builder.AddAttribute(4, TreeViewConstants.TREEVIEW_PARAMETER_NODES_PROPERTY, nodesProperty);
             builder.CloseComponent();
