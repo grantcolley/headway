be5eb7f5cf3e4ac004843c82fffd87a62ed03606
grantcolley <grantcolley@yahoo.com>
2021-12-26 20:32:59 +0000

    Sql Server migrations
    Sql Server migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20211226195503_Headway_ConfigItem_1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ConfigItem_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tooltip",
+                table: "ConfigItems",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tooltip",
+                table: "ConfigItems");
+        }
+    }
+}
