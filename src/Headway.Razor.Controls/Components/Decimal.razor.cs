3222ea2f3a818261a0a057296604dffad5a80fbd
grantcolley <grantcolley@yahoo.com>
2021-12-28 21:57:49 +0000

    Create Decimal.razor component

---
+++ src/Headway.Razor.Controls/Components/Decimal.razor.cs
@@ -0,0 +1,38 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class DecimalBase : DynamicComponentBase
+    {
+        public Expression<Func<decimal>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<decimal>>(Field.MemberExpression);
+            }
+        }
+
+        public decimal PropertyValue
+        {
+            get
+            {
+                return (decimal)Field.PropertyInfo.GetValue(Field.Model);
+            }
+        }
+
+        public virtual void OnValueChanged(decimal value)
+        {
+            Field.PropertyInfo.SetValue(Field.Model, value);
+        }
+    }
+
+    public class DecimalNumber : InputNumber<decimal>
+    {
+    }
+}
