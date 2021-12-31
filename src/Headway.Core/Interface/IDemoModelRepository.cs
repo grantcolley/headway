456e69445345d4815a3249c1dfae7ca73c257570
grantcolley <grantcolley@yahoo.com>
2021-12-31 21:20:05 +0000

    Create IDemoModelRepository.cs

---
+++ src/Headway.Core/Interface/IDemoModelRepository.cs
@@ -0,0 +1,15 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IDemoModelRepository : IRepository
+    {
+        Task<IEnumerable<DemoModel>> GetDemoModelsAsync();
+        Task<DemoModel> GetDemoModelAsync(int id);
+        Task<DemoModel> AddDemoModelAsync(DemoModel demoModel);
+        Task<DemoModel> UpdateDemoModelAsync(DemoModel demoModel);
+        Task<int> DeleteDemoModelAsync(int id);
+    }
+}
