b4fa741f6a7d2fe3f4986a2fb15fb9630991b1f5
grantcolley <grantcolley@yahoo.com>
2021-12-31 11:27:39 +0000

    SqlServer migrations
    SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20211231111749_Headway_Demo_1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Demo_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
