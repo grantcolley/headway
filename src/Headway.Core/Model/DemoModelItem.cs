786845e3a163152d8a7273cfdef7096f3a887812
grantcolley <grantcolley@yahoo.com>
2022-01-02 20:23:01 +0000

    Create DemoModelItem.cs

---
+++ src/Headway.Core/Model/DemoModelItem.cs
@@ -0,0 +1,15 @@
+﻿using Headway.Core.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Headway.Core.Model
+{
+    [DynamicModel]
+    public class DemoModelItem
+    {
+        public int DemoModelItemId { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(50, ErrorMessage = "Description must be between 1 and 50 characters")]
+        public string Description { get; set; }
+    }
+}
