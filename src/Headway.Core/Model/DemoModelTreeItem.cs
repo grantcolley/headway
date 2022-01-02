d80f4af29e938f204ad7c9850b42147e9782494d
grantcolley <grantcolley@yahoo.com>
2022-01-02 20:23:06 +0000

    Create DemoModelTreeItem.cs

---
+++ src/Headway.Core/Model/DemoModelTreeItem.cs
@@ -0,0 +1,18 @@
+﻿using Headway.Core.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Headway.Core.Model
+{
+    [DynamicModel]
+    public class DemoModelTreeItem
+    {
+        public int DemoModelTreeItemId { get; set; }
+
+        public List<DemoModelTreeItem> DemoModelTreeItems { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(50, ErrorMessage = "Description must be between 1 and 50 characters")]
+        public string Description { get; set; }
+    }
+}
