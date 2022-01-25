89f2559f4d718e7b02aa4f7844dc7f45cc99df42
grantcolley <grantcolley@yahoo.com>
2022-01-25 20:17:55 +0000

    Create GenericTreeHelperArgs.cs

---
+++ src/Headway.Core/Helpers/GenericTreeHelperArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Headway.Core.Helpers
+{
+    public class GenericTreeHelperArgs
+    {
+        public string ModelIdPropertyName { get; set; }
+        public string ItemsProperty { get; set; }
+        public string OrderByProperty { get; set; }
+        public string ItemCodeProperty { get; set; }
+        public string ParentItemCodeProperty { get; set; }
+    }
+}
