d7bc0848746decb8fe4f470d5a05cf4cd42959f9
grantcolley <grantcolley@yahoo.com>
2022-01-26 20:15:47 +0000

    Create GenericTreeArgs.cs

---
+++ src/Headway.Repository/Contstants/GenericTreeArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace Headway.Repository.Constants
+{
+    public static class GenericTreeArgs
+    {
+        // ConfigContainer
+        public const string CONFIG_ID = "ConfigId";
+        public const string CONFIG_CONTAINERS = "ConfigContainers";
+        public const string CONFIG_CONTAINER_CODE = "ContainerCode";
+        public const string CONFIG_CONTAINER_PARENT_CODE = "ParentContainerCode";
+        public const string CONFIG_CONTAINER_ORDER = "Order";
+
+        // DemoModel
+        public const string DEMO_MODEL_ID = "DemoModelId";
+        public const string DEMO_MODEL_TREE_ITEMS = "DemoModelTreeItems";
+        public const string DEMO_MODEL_ITEM_CODE = "ItemCode";
+        public const string DEMO_MODEL_PARENT_ITEM_CODE = "ParentItemCode";
+        public const string DEMO_MODEL_TREE_ITEM_ORDER = "Order";
+    }
+}
