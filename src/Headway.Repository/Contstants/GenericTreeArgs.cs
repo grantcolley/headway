afe5da7bd2db00d8d989d1ea76a7a0f5dea49929
grantcolley <grantcolley@yahoo.com>
2022-01-29 17:50:18 +0000

    Update GenericTreeArgs.cs

--- src/Headway.Repository/Contstants/GenericTreeArgs.cs
+++ src/Headway.Repository/Contstants/GenericTreeArgs.cs
@@ -5,15 +5,11 @@
         // ConfigContainer
         public const string CONFIG_ID = "ConfigId";
         public const string CONFIG_CONTAINERS = "ConfigContainers";
-        public const string CONFIG_CONTAINER_CODE = "ContainerCode";
-        public const string CONFIG_CONTAINER_PARENT_CODE = "ParentContainerCode";
         public const string CONFIG_CONTAINER_ORDER = "Order";
 
         // DemoModel
         public const string DEMO_MODEL_ID = "DemoModelId";
         public const string DEMO_MODEL_TREE_ITEMS = "DemoModelTreeItems";
-        public const string DEMO_MODEL_ITEM_CODE = "ItemCode";
-        public const string DEMO_MODEL_PARENT_ITEM_CODE = "ParentItemCode";
         public const string DEMO_MODEL_TREE_ITEM_ORDER = "Order";
     }
 }
