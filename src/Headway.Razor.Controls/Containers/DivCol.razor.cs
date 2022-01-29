32b6f5a4aa6357686ab8a27e1c3aa843a78322cd
grantcolley <grantcolley@yahoo.com>
2022-01-29 17:50:04 +0000

    Create DivCol.razor and DivCol.razor.cs
    Create DivCol.razor and DivCol.razor.cs

---
+++ src/Headway.Razor.Controls/Containers/DivCol.razor.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.Razor.Controls.Containers
+{
+    public abstract class DivColBase : ComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+    }
+}
