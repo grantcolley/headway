d2d73f76aa75267c8523c884c8225b007c1df513
grantcolley <grantcolley@yahoo.com>
2022-01-29 11:20:56 +0000

    Update Div.razor.cs

--- src/Headway.Razor.Controls/Containers/Div.razor.cs
+++ src/Headway.Razor.Controls/Containers/Div.razor.cs
@@ -1,11 +1,27 @@
 ﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
 using Headway.Core.Dynamic;
+using Headway.Core.Helpers;
 using Headway.Razor.Controls.Base;
+using System;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Containers
 {
     [DynamicContainer]
     public abstract class DivBase : DynamicContainerBase
     {
+        protected bool LayoutHorizontal { get; private set; }
+        protected override Task OnInitializedAsync()
+        {
+            var layoutHorizontal = ComponentArgHelper.GetArgValue(Container.DynamicArgs, Args.LAYOUT_HORIZONTAL);
+
+            if(!string.IsNullOrWhiteSpace(layoutHorizontal))
+            {
+                LayoutHorizontal = Convert.ToBoolean(layoutHorizontal);
+            }
+
+            return base.OnInitializedAsync();
+        }
     }
 }
