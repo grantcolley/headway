e30d92966e3220c2bf8fbafdb0a07781e51cb564
grantcolley <grantcolley@yahoo.com>
2022-01-30 14:06:34 +0000

    Create Checklist.razor, Checklist.razor.cs and Checklist.cs
    Create Checklist.razor, Checklist.razor.cs and Checklist.cs

---
+++ src/Headway.Core/Model/ChecklistItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class ChecklistItem
+    {
+        public bool IsChecked { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> SubItems { get; set; }
+    }
+}
---
+++ src/Headway.Razor.Controls/Components/CheckList.razor.cs
@@ -0,0 +1,21 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Model;
+using Headway.Razor.Controls.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class CheckListBase : DynamicComponentBase
+    {
+        protected List<ChecklistItem> checklist;
+
+        protected override Task OnParametersSetAsync()
+        {
+            checklist = (List<ChecklistItem>)Field.PropertyInfo.GetValue(Field.Model, null);
+
+            return base.OnParametersSetAsync();
+        }
+    }
+}
