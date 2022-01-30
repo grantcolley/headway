7ad59ac438116bb91ca4bd852891b4827ba29a54
grantcolley <grantcolley@yahoo.com>
2022-01-30 20:07:40 +0000

    User to display roles and permissions #40
    User to display roles and permissions #40

--- src/Headway.Core/Model/User.cs
+++ src/Headway.Core/Model/User.cs
@@ -1,6 +1,7 @@
 ﻿using Headway.Core.Attributes;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Headway.Core.Model
 {
@@ -16,6 +17,12 @@ namespace Headway.Core.Model
         public int UserId { get; set; }
         public List<Role> Roles { get; set; }
         public List<Permission> Permissions { get; set; }
+
+        [NotMapped]
+        public List<ChecklistItem> PermissionChecklist { get; set; }
+
+        [NotMapped]
+        public List<ChecklistItem> RoleChecklist { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
         [StringLength(50, ErrorMessage = "Name must be between 1 and 50 characters")]
