50f5683340aa903c5f987fb67a8c04c094115efc
grantcolley <grantcolley@yahoo.com>
2022-02-02 18:00:38 +0000

    Facilitate linked fields in DynamicComponentBase #43
    Facilitate linked fields in DynamicComponentBase #43

--- src/Headway.Razor.Controls/Base/DynamicComponentBase.cs
+++ src/Headway.Razor.Controls/Base/DynamicComponentBase.cs
@@ -1,7 +1,9 @@
-﻿using Headway.Core.Dynamic;
+﻿using Headway.Core.Constants;
+using Headway.Core.Dynamic;
 using Headway.Core.Model;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Headway.Razor.Controls.Base
 {
@@ -12,5 +14,31 @@ namespace Headway.Razor.Controls.Base
 
         [Parameter]
         public List<DynamicArg> ComponentArgs { get; set; }
+
+        protected void LinkFieldCheck()
+        {
+            if (Field.IsLinkedField)
+            {
+                SetLinkedValue();
+            }
+        }
+
+        protected void SetLinkedValue()
+        {
+            if (Field.IsLinkedField)
+            {
+                var value = Field.LinkValue;
+
+                var linkValueArg = ComponentArgs.FirstOrDefault(a => a.Name.Equals(Args.LINK_VALUE));
+
+                if(linkValueArg == null)
+                {
+                    linkValueArg = new DynamicArg { Name = Args.LINK_VALUE };
+                    ComponentArgs.Add(linkValueArg);
+                }
+
+                linkValueArg.Value = value;
+            }
+        }
     }
 }
