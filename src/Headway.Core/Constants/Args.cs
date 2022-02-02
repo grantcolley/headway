e1f90a88cbd91bd9dcd422961a4f96535dd7c868
grantcolley <grantcolley@yahoo.com>
2022-02-02 08:51:53 +0000

    Update Args.cs

--- src/Headway.Core/Constants/Args.cs
+++ src/Headway.Core/Constants/Args.cs
@@ -2,19 +2,21 @@
 {
     public static class Args
     {
-        public const string COMPONENT = "Component"; 
+        public const string CODE = "Code";
+        public const string CODE_PARENT = "ParentCode";
+        public const string COMPONENT = "Component";
+        public const string LABEL_PROPERTY = "LabelProperty";
+        public const string LAYOUT_HORIZONTAL = "LayoutHorizontal";
+        public const string LINK_SOURCE = "LinkSource";
+        public const string LINK_VALUE = "LinkValue";
         public const string LIST_CONFIG = "ListConfig";
+        public const string LIST_PROPERTY = "ListProperty";
         public const string MODEL = "Model";
         public const string SEARCH_PARAMETER = "SearchParameter";
-        public const string TYPE = "Type";
-        public const string VALUE = "Value";
-        public const string UNIQUE_PROPERTY = "UniqueProperty";
-        public const string PARENT_UNIQUE_PROPERTY = "ParentUniqueProperty";
-        public const string LABEL_PROPERTY = "LabelProperty";
-        public const string LIST_PROPERTY = "ListProperty";
         public const string TEXT_MULTILINE_ROWS = "Rows";
-        public const string LAYOUT_HORIZONTAL = "LayoutHorizontal";
-        public const string CODE = "Code";
-        public const string PARENT_CODE = "ParentCode";
+        public const string TYPE = "Type";
+        public const string UNIQUE_PROPERTY = "UniqueProperty";
+        public const string UNIQUE_PARENT_PROPERTY = "ParentUniqueProperty";
+        public const string VALUE = "Value";
     }
 }
