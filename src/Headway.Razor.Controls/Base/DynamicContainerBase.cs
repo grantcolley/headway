f845930027c0067b45d2f97268b0aed4fce1a0b4
grantcolley <grantcolley@yahoo.com>
2022-02-03 19:59:44 +0000

    Inject IStateNotificationMediator instance and register / deregister the container #43
    Inject IStateNotificationMediator instance and register / deregister the container #43

--- src/Headway.Razor.Controls/Base/DynamicContainerBase.cs
+++ src/Headway.Razor.Controls/Base/DynamicContainerBase.cs
@@ -1,11 +1,31 @@
 ﻿using Headway.Core.Dynamic;
+using Headway.Core.Mediators;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Base
 {
-    public class DynamicContainerBase : ComponentBase
+    public class DynamicContainerBase : ComponentBase, IDisposable
     {
+        [Inject]
+        public IStateNotificationMediator StateNotification { get; set; }
+
         [Parameter]
         public DynamicContainer Container { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            StateNotification.Register(Container.UniqueId, StateHasChanged);
+
+            await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            StateNotification.Deregister(Container.UniqueId);
+
+            GC.SuppressFinalize(this);
+        }
     }
 }
