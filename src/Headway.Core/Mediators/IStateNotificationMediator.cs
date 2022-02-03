27912e15307b4307dc349860b59c94518324a658
grantcolley <grantcolley@yahoo.com>
2022-02-03 19:58:34 +0000

    Create IStateNotificationMediator and StateNotificationMediator #43
    Create IStateNotificationMediator and StateNotificationMediator #43

---
+++ src/Headway.Core/Mediators/IStateNotificationMediator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Headway.Core.Mediators
+{
+    public interface IStateNotificationMediator
+    {
+        void Register(string target, Action action);
+        void Deregister(string target);
+        void NotifyStateHasChanged(string target);
+    }
+}
---
+++ src/Headway.Core/Mediators/StateNotificationMediator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Headway.Core.Mediators
+{
+    public class StateNotificationMediator : IStateNotificationMediator
+    {
+        private readonly Dictionary<string, Action> register;
+
+        public StateNotificationMediator()
+        {
+            register = new Dictionary<string, Action>();
+        }
+
+        public void Register(string target, Action action)
+        {
+            if(register.ContainsKey(target))
+            {
+                return;
+            }
+
+            register.Add(target, action);
+            Debug.WriteLine($"Registered {target}");
+        }
+
+        public void Deregister(string target)
+        {
+            if (register.ContainsKey(target))
+            {
+                register.Remove(target);
+                Debug.WriteLine($"Deregistered {target}");
+            }
+        }
+
+        public void NotifyStateHasChanged(string target)
+        {
+            if (register.ContainsKey(target))
+            {
+                register[target].Invoke();
+            }
+        }
+    }
+}
