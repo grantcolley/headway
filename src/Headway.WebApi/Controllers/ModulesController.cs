92058825f21fc2c1b3f745c081ea0286c51b2218
grantcolley <grantcolley@yahoo.com>
2022-02-08 20:15:26 +0000

    Update ModulesController.cs

--- src/Headway.WebApi/Controllers/ModulesController.cs
+++ src/Headway.WebApi/Controllers/ModulesController.cs
@@ -1,6 +1,7 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Constants;
 using Headway.Core.Interface;
+using Headway.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -20,8 +21,8 @@ namespace Headway.WebApi.Controllers
             this.moduleRepository = moduleRepository;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpGet("[action]")]
+        public async Task<IActionResult> ClaimModules()
         {
             var authorised = await IsAuthorisedAsync(Roles.USER)
                 .ConfigureAwait(false);
@@ -36,6 +37,96 @@ namespace Headway.WebApi.Controllers
             var modules = await moduleRepository.GetModulesAsync(claim).ConfigureAwait(false);
 
             return Ok(modules);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var authorised = await IsAuthorisedAsync(Roles.ADMIN)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var modules = await moduleRepository
+                .GetModulesAsync()
+                .ConfigureAwait(false);
+
+            return Ok(modules);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var authorised = await IsAuthorisedAsync(Roles.ADMIN)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var module = await moduleRepository
+                .GetModuleAsync(id)
+                .ConfigureAwait(false);
+
+            return Ok(module);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Module module)
+        {
+            var authorised = await IsAuthorisedAsync(Roles.ADMIN)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var savedModule = await moduleRepository
+                .AddModuleAsync(module)
+                .ConfigureAwait(false);
+
+            return Ok(savedModule);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Module module)
+        {
+            var authorised = await IsAuthorisedAsync(Roles.ADMIN)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var savedModule = await moduleRepository
+                .UpdateModuleAsync(module)
+                .ConfigureAwait(false);
+
+            return Ok(savedModule);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var authorised = await IsAuthorisedAsync(Roles.ADMIN)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            var result = await moduleRepository
+                .DeleteModuleAsync(id)
+                .ConfigureAwait(false);
+
+            return Ok(result);
         }
     }
 }
