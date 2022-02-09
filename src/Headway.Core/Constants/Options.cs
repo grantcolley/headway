5ac296747ce464a233c59aeb7ee9941b80d33b89
grantcolley <grantcolley@yahoo.com>
2022-02-09 07:53:54 +0000

    Update Options.cs

--- src/Headway.Core/Constants/Options.cs
+++ src/Headway.Core/Constants/Options.cs
@@ -2,11 +2,12 @@
 {
     public static class Options
     {
-        public const string CONFIG_OPTION_ITEMS = "ConfigOptionItems";
-        public const string CONTROLLER_OPTION_ITEMS = "ControllerOptionItems";
-        public const string CONTROLLER = "Controller";
         public const string CONFIG_CONTAINERS = "ConfigContainers";
+        public const string CONFIG_OPTION_ITEMS = "ConfigOptionItems";
+        public const string CONTROLLER = "Controller";
+        public const string CONTROLLER_OPTION_ITEMS = "ControllerOptionItems";
         public const string DISPLAY_FIELD = "DisplayField";
         public const string OPTIONS_CODE = "OptionsCode";
+        public const string PERMISSIONS_OPTION_ITEMS = "Permissions";
     }
 }
