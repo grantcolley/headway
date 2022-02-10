db6de0e98eb0731ff16432dea1581a5b41b8af19
grantcolley <grantcolley@yahoo.com>
2022-02-10 06:31:06 +0000

    Update Options.cs

--- src/Headway.Core/Constants/Options.cs
+++ src/Headway.Core/Constants/Options.cs
@@ -9,5 +9,7 @@
         public const string DISPLAY_FIELD = "DisplayField";
         public const string OPTIONS_CODE = "OptionsCode";
         public const string PERMISSIONS_OPTION_ITEMS = "Permissions";
+        public const string MODULES_OPTION_ITEMS = "Modules";
+        public const string CATEGORIES_OPTION_ITEMS = "Categories";
     }
 }
