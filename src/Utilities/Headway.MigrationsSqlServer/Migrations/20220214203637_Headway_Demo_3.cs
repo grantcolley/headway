66ccbf1d16c40bb0b6f17baf02682e7048031a49
grantcolley <grantcolley@yahoo.com>
2022-02-14 20:45:25 +0000

    Add SqlServer migrations
    Add SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220214203637_Headway_Demo_3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Demo_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dropdown",
+                table: "DemoModels",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dropdown",
+                table: "DemoModels");
+        }
+    }
+}
