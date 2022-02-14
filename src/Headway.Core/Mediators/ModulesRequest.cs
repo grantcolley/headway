6b868d2f7311352e90df50e79b583839ae44e1a2
grantcolley <grantcolley@yahoo.com>
2022-02-14 08:57:35 +0000

    Create mediator request and handler classes for modules #44
    Create mediator request and handler classes for modules #44

---
+++ src/Headway.Core/Mediators/ModulesRequest.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Model;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Headway.Core.Mediators
+{
+    public record ModulesRequest : IRequest<ModulesRequest.Response>
+    {
+        public record Response(IEnumerable<Module> Modules);
+    }
+}
