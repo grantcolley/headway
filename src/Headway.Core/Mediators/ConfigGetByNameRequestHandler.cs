d1613b3ee51938e7ce299f21ffe152ceab44e45f
grantcolley <grantcolley@yahoo.com>
2022-02-15 08:15:23 +0000

    Create ConfigGetByNameRequestHandler.cs

---
+++ src/Headway.Core/Mediators/ConfigGetByNameRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using Headway.Core.Interface;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Mediators
+{
+    public class ConfigGetByNameRequestHandler : IRequestHandler<ConfigGetByNameRequest, ConfigGetByNameRequest.Response>
+    {
+        private readonly IConfigurationService configurationService;
+
+        public ConfigGetByNameRequestHandler(IConfigurationService configurationService)
+        {
+            this.configurationService = configurationService;
+        }
+
+        public async Task<ConfigGetByNameRequest.Response> Handle(ConfigGetByNameRequest request, CancellationToken cancellationToken)
+        {
+            return new ConfigGetByNameRequest.Response(
+                await configurationService.GetConfigAsync(request.Name)
+                .ConfigureAwait(false));
+        }
+    }
+}
