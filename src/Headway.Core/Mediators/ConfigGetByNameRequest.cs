34fe1648fdef5613ce60e095ed5572c291f6934c
grantcolley <grantcolley@yahoo.com>
2022-02-15 08:15:19 +0000

    Create ConfigGetByNameRequest.cs

---
+++ src/Headway.Core/Mediators/ConfigGetByNameRequest.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using MediatR;
+
+namespace Headway.Core.Mediators
+{
+    public record ConfigGetByNameRequest(string Name) : IRequest<ConfigGetByNameRequest.Response>
+    {
+        public record Response(IResponse<Config> Config);
+    }
+}
