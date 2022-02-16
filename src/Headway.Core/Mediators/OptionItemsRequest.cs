d35fb18342adfac77fbf454b6e9646524492119c
grantcolley <grantcolley@yahoo.com>
2022-02-16 16:16:23 +0000

    Create OptionItemsRequest.cs

---
+++ src/Headway.Core/Mediators/OptionItemsRequest.cs
@@ -0,0 +1,12 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Headway.Core.Mediators
+{
+    public record OptionItemsRequest(List<DynamicArg> DynamicArgs) : IRequest<OptionItemsRequest.Response>
+    {
+        public record Response(IResponse<IEnumerable<OptionItem>> OptionItems);
+    }
+}
