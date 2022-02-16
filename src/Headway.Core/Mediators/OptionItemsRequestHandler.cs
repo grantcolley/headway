907f2e2fefe6afd845f441c091a17d9af718b833
grantcolley <grantcolley@yahoo.com>
2022-02-16 16:16:28 +0000

    Create OptionItemsRequestHandler.cs

---
+++ src/Headway.Core/Mediators/OptionItemsRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using Headway.Core.Interface;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Mediators
+{
+    public class OptionItemsRequestHandler : IRequestHandler<OptionItemsRequest, OptionItemsRequest.Response>
+    {
+        private readonly IOptionsApiRequest optionsApiRequest;
+
+        public OptionItemsRequestHandler(IOptionsApiRequest optionsApiRequest)
+        {
+            this.optionsApiRequest = optionsApiRequest;
+        }
+
+        public async Task<OptionItemsRequest.Response> Handle(OptionItemsRequest request, CancellationToken cancellationToken)
+        {
+            return new OptionItemsRequest.Response(await optionsApiRequest.GetOptionItemsAsync(request.DynamicArgs).ConfigureAwait(false));
+        }
+    }
+}
