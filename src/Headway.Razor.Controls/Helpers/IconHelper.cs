063a0ed9c8b188be50b6250d3a85a6bedef6b1d9
grantcolley <grantcolley@yahoo.com>
2022-02-20 08:40:24 +0000

    Create IconHelper #45

---
+++ src/Headway.Razor.Controls/Helpers/IconHelper.cs
@@ -0,0 +1,26 @@
+﻿using Headway.Core.Dynamic;
+using MudBlazor;
+
+namespace Headway.Razor.Controls.Helpers
+{
+    public static class IconHelper
+    {
+        private readonly static Outlined outlined = new();
+        private readonly static DynamicTypeHelper<Outlined> outlinedHelper;
+
+        static IconHelper()
+        {
+            outlinedHelper = DynamicTypeHelper.Get<Outlined>();
+        }
+
+        public static string GetOutlined(string name)
+        {
+            if(outlinedHelper == null)
+            {
+                return string.Empty;
+            }
+
+            return outlinedHelper.GetValue(outlined, name).ToString();
+        }
+    }
+}
