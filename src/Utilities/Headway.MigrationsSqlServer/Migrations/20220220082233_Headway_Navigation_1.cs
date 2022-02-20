701f6bde5eedc266b2687cbefd5b693ebd4494dc
grantcolley <grantcolley@yahoo.com>
2022-02-20 08:42:07 +0000

    SqlServer migrations
    SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220220082233_Headway_Navigation_1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Navigation_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageClass",
+                table: "MenuItems",
+                newName: "Icon");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "Modules",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "Categories",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "Icon",
+                table: "MenuItems",
+                newName: "ImageClass");
+        }
+    }
+}
