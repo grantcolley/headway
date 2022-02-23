3eaaf19fe6c8634acd80830f2be64427fccdc135
grantcolley <grantcolley@yahoo.com>
2022-02-23 09:17:04 +0000

    Create IShowDialogService and ShowDialogService #46
    Create IShowDialogService and ShowDialogService #46

---
+++ src/Headway.Razor.Controls/Services/IShowDialogService.cs
@@ -0,0 +1,10 @@
+﻿using MudBlazor;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Services
+{
+    public interface IShowDialogService
+    {
+        Task<DialogResult> DeleteAsync(string message);
+    }
+}
---
+++ src/Headway.Razor.Controls/Services/ShowDialogService.cs
@@ -0,0 +1,29 @@
+﻿using Headway.Razor.Controls.Dialogs;
+using MudBlazor;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Services
+{
+    public class ShowDialogService : IShowDialogService
+    {
+        private readonly IDialogService dialogService;
+        public ShowDialogService(IDialogService dialogService)
+        {
+            this.dialogService = dialogService;
+        }
+
+        public async Task<DialogResult> DeleteAsync(string message)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("ContentText", message);
+            parameters.Add("ButtonText", "Delete");
+            parameters.Add("Color", Color.Error);
+
+            var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+            var result = dialogService.Show<StandardDialog>("Delete", parameters, options);
+
+            return await result.Result.ConfigureAwait(false);
+        }
+    }
+}
