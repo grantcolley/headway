b4ce4a694e0abdb5d623b608c3200e977d2163b5
grantcolley <grantcolley@yahoo.com>
2022-02-24 19:23:12 +0000

    SqlServer migrations
    SqlServer migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220224184859_Headway_Config_1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Config_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NavigateToProperty",
+                table: "Configs",
+                newName: "NavigateProperty");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigateToConfig",
+                table: "Configs",
+                newName: "NavigatePage");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigateTo",
+                table: "Configs",
+                newName: "NavigateConfig");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NavigateResetBreadcrumb",
+                table: "Configs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NavigateResetBreadcrumb",
+                table: "Configs");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigateProperty",
+                table: "Configs",
+                newName: "NavigateToProperty");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigatePage",
+                table: "Configs",
+                newName: "NavigateToConfig");
+
+            migrationBuilder.RenameColumn(
+                name: "NavigateConfig",
+                table: "Configs",
+                newName: "NavigateTo");
+        }
+    }
+}
