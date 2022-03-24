ffee94e91c49b9c27b1c8d30529d9e3e3afd52d1
grantcolley <grantcolley@yahoo.com>
2022-03-24 20:18:54 +0000

    Toggle dark and light mode tooltip

---
+++ src/Headway.Core/Constants/Mode.cs
@@ -0,0 +1,8 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class Mode
+    {
+        public const string DARK = "Dark Mode";
+        public const string LIGHT = "Light Mode";
+    }
+}
