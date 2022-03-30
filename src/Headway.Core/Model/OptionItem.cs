b445fed63f69230462020bc032ee2883815b9179
grantcolley <grantcolley@yahoo.com>
2022-03-30 20:30:40 +0100

    Update OptionItem.cs

--- src/Headway.Core/Model/OptionItem.cs
+++ src/Headway.Core/Model/OptionItem.cs
@@ -4,5 +4,21 @@
     {
         public string Id { get; set; }
         public string Display { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var optionItem = obj as OptionItem;
+            return optionItem.Id == this.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Display;
+        }
     }
 }
