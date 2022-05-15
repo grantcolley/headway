4d47797887af006319288a873eea51902a662667
grantcolley <grantcolley@yahoo.com>
2022-05-15 19:12:34 +0100

    Headway.BlazorWebassemblyApp Program.cs to use top-level statements
    Headway.BlazorWebassemblyApp Program.cs to use top-level statements

--- src/Headway.BlazorWebassemblyApp/Program.cs
+++ src/Headway.BlazorWebassemblyApp/Program.cs
@@ -1,3 +1,4 @@
+using Headway.BlazorWebassemblyApp;
 using Headway.BlazorWebassemblyApp.Account;
 using Headway.Core.Cache;
 using Headway.Core.Interface;
@@ -6,6 +7,7 @@ using Headway.Razor.Controls.Services;
 using Headway.RequestApi.Api;
 using Headway.RequestApi.Requests;
 using MediatR;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,77 +15,68 @@ using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace Headway.BlazorWebassemblyApp
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOidcAuthentication(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    builder.Configuration.Bind("OidcConfiguration", options.ProviderOptions);
+    options.UserOptions.RoleClaim = "role";
+}).AddAccountClaimsPrincipalFactory<UserAccountFactory>();
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                builder.Configuration.Bind("OidcConfiguration", options.ProviderOptions);
-                options.UserOptions.RoleClaim = "role";
-            }).AddAccountClaimsPrincipalFactory<UserAccountFactory>();
+builder.Services.AddHttpClient("webapi", (sp, client) =>
+{
+    client.BaseAddress = new Uri("https://localhost:44320");
+}).AddHttpMessageHandler(sp =>
+{
+    var handler = sp.GetService<AuthorizationMessageHandler>()
+    .ConfigureHandler(
+        authorizedUrls: new[] { "https://localhost:44320" },
+        scopes: new[] { "webapi" });
+    return handler;
+});
 
-            builder.Services.AddHttpClient("webapi", (sp, client) =>
-            {
-                client.BaseAddress = new Uri("https://localhost:44320");
-            }).AddHttpMessageHandler(sp =>
-            {
-                var handler = sp.GetService<AuthorizationMessageHandler>()
-                .ConfigureHandler(
-                    authorizedUrls: new[] { "https://localhost:44320" },
-                    scopes: new[] { "webapi" });
-                return handler;
-            });
+builder.Services.AddSingleton<IAppCache, AppCache>();
+builder.Services.AddSingleton<IConfigCache, ConfigCache>();
+builder.Services.AddSingleton<IStateNotification, StateNotification>();
+builder.Services.AddTransient<IShowDialogService, ShowDialogService>();
+builder.Services.AddTransient<ModulesGetRequestHandler>();
+builder.Services.AddTransient<ConfigGetByNameRequestHandler>();
+builder.Services.AddTransient<OptionItemsRequestHandler>();
 
-            builder.Services.AddSingleton<IAppCache, AppCache>();
-            builder.Services.AddSingleton<IConfigCache, ConfigCache>();
-            builder.Services.AddSingleton<IStateNotification, StateNotification>();
-            builder.Services.AddTransient<IShowDialogService, ShowDialogService>();
-            builder.Services.AddTransient<ModulesGetRequestHandler>();
-            builder.Services.AddTransient<ConfigGetByNameRequestHandler>();
-            builder.Services.AddTransient<OptionItemsRequestHandler>();
+builder.Services.AddTransient<IModuleApiRequest, ModuleApiRequest>(sp =>
+{
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient("webapi");
+    return new ModuleApiRequest(httpClient);
+});
 
-            builder.Services.AddTransient<IModuleApiRequest, ModuleApiRequest>(sp =>
-            {
-                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient("webapi");
-                return new ModuleApiRequest(httpClient);
-            });
+builder.Services.AddTransient<IConfigurationApiRequest, ConfigurationApiRequest>(sp =>
+{
+    var configCache = sp.GetRequiredService<IConfigCache>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient("webapi");
+    return new ConfigurationApiRequest(httpClient, configCache);
+});
 
-            builder.Services.AddTransient<IConfigurationApiRequest, ConfigurationApiRequest>(sp =>
-            {
-                var configCache = sp.GetRequiredService<IConfigCache>();
-                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient("webapi");
-                return new ConfigurationApiRequest(httpClient, configCache);
-            });
+builder.Services.AddTransient<IDynamicApiRequest, DynamicApiRequest>(sp =>
+{
+    var configCache = sp.GetRequiredService<IConfigCache>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient("webapi");
+    return new DynamicApiRequest(httpClient, configCache);
+});
 
-            builder.Services.AddTransient<IDynamicApiRequest, DynamicApiRequest>(sp =>
-            {
-                var configCache = sp.GetRequiredService<IConfigCache>();
-                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient("webapi");
-                return new DynamicApiRequest(httpClient, configCache);
-            });
+builder.Services.AddTransient<IOptionsApiRequest, OptionsApiRequest>(sp =>
+{
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient("webapi");
+    return new OptionsApiRequest(httpClient);
+});
 
-            builder.Services.AddTransient<IOptionsApiRequest, OptionsApiRequest>(sp =>
-            {
-                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient("webapi");
-                return new OptionsApiRequest(httpClient);
-            });
+builder.Services.AddMediatR(typeof(ModuleApiRequest).Assembly);
+builder.Services.AddMudServices();
 
-            builder.Services.AddMediatR(typeof(ModuleApiRequest).Assembly);
-            builder.Services.AddMudServices();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
