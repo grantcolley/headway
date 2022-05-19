ab3362f71c6715b083de0a46c1be456c353572c2
grantcolley <grantcolley@yahoo.com>
2022-05-19 20:28:18 +0100

    Create IdentityProvider.cs

---
+++ src/Headway.Core/Constants/IdentityProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class IdentityProvider
+    {
+        public const string AUTH_0 = "Auth0";
+        public const string IDENTITY_SERVER_4 = "IdentityServer4";
+    }
+}
