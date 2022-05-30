ff1908751a6aa3b5d56c6a48f62805f03f5f1635
grantcolley <grantcolley@yahoo.com>
2022-05-30 20:40:25 +0100

    Create CustomerProduct.cs

---
+++ src/Headway.RemediatR/Model/CustomerProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace Headway.RemediatR.Model
+{
+    public class CustomerProduct
+    {
+        public int Id { get; set; }
+        public Customer? Customer { get; set; }
+        public Product? Product { get; set; }
+        public DateTime StartDate { get; set; }
+        public decimal Value { get; set; }
+    }
+}
