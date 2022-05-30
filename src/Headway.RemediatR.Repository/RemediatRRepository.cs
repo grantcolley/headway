0ee7851fda9c21ebd342ff2d377851015876365c
grantcolley <grantcolley@yahoo.com>
2022-05-30 21:32:03 +0100

    Create project Headway.RemediatR.Repository #52
    Create project Headway.RemediatR.Repository #52

---
+++ src/Headway.RemediatR.Repository/RemediatRRepository.cs
@@ -0,0 +1,43 @@
+﻿using Headway.RemediatR.Core.Interface;
+using Headway.RemediatR.Core.Model;
+
+namespace Headway.RemediatR.Repository
+{
+    public class RemediatRRepository : IRemediatRRepository
+    {
+        public Task<Product> AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAuthorisedAsync(string claim, string permission)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
