99f24ed18d50078ecf52871422267567778643e2
grantcolley <grantcolley@yahoo.com>
2022-05-31 08:25:57 +0100

    Update RemediatRRepository.cs

--- src/Headway.RemediatR.Repository/RemediatRRepository.cs
+++ src/Headway.RemediatR.Repository/RemediatRRepository.cs
@@ -2,6 +2,7 @@
 using Headway.RemediatR.Core.Model;
 using Headway.Repository;
 using Headway.Repository.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -16,29 +17,216 @@ namespace Headway.RemediatR.Repository
         {
         }
 
-        public Task<IEnumerable<Product>> GetProductsAsync()
+        public async Task<IEnumerable<Product>> GetProductsAsync()
         {
-            throw new NotImplementedException();
+            return await applicationDbContext.Products
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
         }
 
-        public Task<Product> GetProductAsync(int id)
+        public async Task<Product> GetProductAsync(int id)
         {
-            throw new NotImplementedException();
+            return await applicationDbContext.Products
+                .AsNoTracking()
+                .SingleAsync(p => p.ProductId.Equals(id))
+                .ConfigureAwait(false);
         }
 
-        public Task<Product> AddProductAsync(Product product)
+        public async Task<Product> AddProductAsync(Product product)
         {
-            throw new NotImplementedException();
+            await applicationDbContext.Products
+                .AddAsync(product)
+                .ConfigureAwait(false);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return product;
         }
 
-        public Task<Product> UpdateProductAsync(Product product)
+        public async Task<Product> UpdateProductAsync(Product product)
         {
-            throw new NotImplementedException();
+            applicationDbContext.Products.Update(product);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return product;
         }
 
-        public Task<int> DeleteProductAsync(int id)
+        public async Task<int> DeleteProductAsync(int id)
         {
-            throw new NotImplementedException();
+            var product = await applicationDbContext.Products
+                .SingleAsync(p => p.ProductId.Equals(id))
+                .ConfigureAwait(false);
+
+            applicationDbContext.Products.Remove(product);
+
+            return await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            return await applicationDbContext.Customers
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Customer> GetCustomerAsync(int id)
+        {
+            return await applicationDbContext.Customers
+                .AsNoTracking()
+                .SingleAsync(p => p.CustomerId.Equals(id))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Customer> AddCustomerAsync(Customer customer)
+        {
+            await applicationDbContext.Customers
+                .AddAsync(customer)
+                .ConfigureAwait(false);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return customer;
+        }
+
+        public async Task<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            applicationDbContext.Customers.Update(customer);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return customer;
+        }
+
+        public async Task<int> DeleteCustomerAsync(int id)
+        {
+            var customer = await applicationDbContext.Customers
+                .SingleAsync(p => p.CustomerId.Equals(id))
+                .ConfigureAwait(false);
+
+            applicationDbContext.Customers.Remove(customer);
+
+            return await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Program>> GetProgramsAsync()
+        {
+            return await applicationDbContext.Programs
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Program> GetProgramAsync(int id)
+        {
+            return await applicationDbContext.Programs
+                .AsNoTracking()
+                .SingleAsync(p => p.ProgramId.Equals(id))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Program> AddProgramAsync(Program program)
+        {
+            await applicationDbContext.Programs
+                .AddAsync(program)
+                .ConfigureAwait(false);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return program;
+        }
+
+        public async Task<Program> UpdateProgramAsync(Program program)
+        {
+            applicationDbContext.Programs.Update(program);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return program;
+        }
+
+        public async Task<int> DeleteProgramAsync(int id)
+        {
+            var program = await applicationDbContext.Programs
+                .SingleAsync(p => p.ProgramId.Equals(id))
+                .ConfigureAwait(false);
+
+            applicationDbContext.Programs.Remove(program);
+
+            return await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Redress>> GetRedressesAsync()
+        {
+            return await applicationDbContext.Redresses
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Redress> GetRedressAsync(int id)
+        {
+            return await applicationDbContext.Redresses
+                .AsNoTracking()
+                .SingleAsync(p => p.RedressId.Equals(id))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Redress> AddRedressAsync(Redress redress)
+        {
+            await applicationDbContext.Redresses
+                .AddAsync(redress)
+                .ConfigureAwait(false);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return redress;
+        }
+
+        public async Task<Redress> UpdateRedressAsync(Redress redress)
+        {
+            applicationDbContext.Redresses.Update(redress);
+
+            await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return redress;
+        }
+
+        public async Task<int> DeleteRedressAsync(int id)
+        {
+            var redress = await applicationDbContext.Redresses
+                .SingleAsync(p => p.RedressId.Equals(id))
+                .ConfigureAwait(false);
+
+            applicationDbContext.Redresses.Remove(redress);
+
+            return await applicationDbContext
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
         }
     }
 }
