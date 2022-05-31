702410b27e95e78da90342a96607c19a13e4899f
grantcolley <grantcolley@yahoo.com>
2022-05-31 08:25:55 +0100

    Update IRemediatRRepository.cs

--- src/Headway.RemediatR.Core/Interface/IRemediatRRepository.cs
+++ src/Headway.RemediatR.Core/Interface/IRemediatRRepository.cs
@@ -12,5 +12,20 @@ namespace Headway.RemediatR.Core.Interface
         Task<Product> AddProductAsync(Product product);
         Task<Product> UpdateProductAsync(Product product);
         Task<int> DeleteProductAsync(int id);
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+        Task<Customer> GetCustomerAsync(int id);
+        Task<Customer> AddCustomerAsync(Customer customer);
+        Task<Customer> UpdateCustomerAsync(Customer customer);
+        Task<int> DeleteCustomerAsync(int id);
+        Task<IEnumerable<Program>> GetProgramsAsync();
+        Task<Program> GetProgramAsync(int id);
+        Task<Program> AddCustomerAsync(Program program);
+        Task<Program> UpdateProgramAsync(Program program);
+        Task<int> DeleteProgramAsync(int id);
+        Task<IEnumerable<Redress>> GetRedressesAsync();
+        Task<Redress> GetRedressAsync(int id);
+        Task<Redress> AddRedressAsync(Redress redress);
+        Task<Redress> UpdateRedressAsync(Redress redress);
+        Task<int> DeleteRedressAsync(int id);
     }
 }
