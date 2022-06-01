293a4b6590f0dc7778cd6ffc1142dab9042e8772
grantcolley <grantcolley@yahoo.com>
2022-06-01 23:21:46 +0100

    Create RefundCalculation.cs

---
+++ src/Headway.RemediatR.Core/Model/RefundCalculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Headway.RemediatR.Core.Model
+{
+    public class RefundCalculation
+    {
+        public decimal? BasicRefundAmount { get; set; }
+        public decimal? CompensatoryAmount { get; set; }
+        public decimal? TotalRefundAmount { get; set; }
+        public DateTime? SubmittedDate { get; set; }
+        public string? SubmittedBy { get; set; }
+    }
+}
