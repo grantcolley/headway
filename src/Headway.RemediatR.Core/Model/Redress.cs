f69d5738877e6ab161867fc45eb149d5fc7dcb14
grantcolley <grantcolley@yahoo.com>
2022-06-01 23:21:44 +0100

    Update Redress.cs

--- src/Headway.RemediatR.Core/Model/Redress.cs
+++ src/Headway.RemediatR.Core/Model/Redress.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Headway.RemediatR.Core.Model
 {
@@ -13,5 +14,19 @@ namespace Headway.RemediatR.Core.Model
         public Customer? Customer { get; set; }
         public Program? Program { get; set; }
         public List<Product> Products { get; set; }
+        public RefundCalculation? RefundCalculation { get; set; }
+        public RefundCalculation? RefundVerification { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? RedressCaseOwner { get; set; }
+        public string? RefundReviewedBy { get; set; }
+        public DateTime? RefundReviewDate { get; set; }
+        public string? RefundReviewComment { get; set; }
+        public string? RedressReviewedBy { get; set; }
+        public DateTime? RedressReviewDate { get; set; }
+        public string? RedressReviewComment { get; set; }
+        public DateTime? CommunicationDispatchDate { get; set; }
+        public bool ResponseRequired { get; set; }
+        public bool ResponseRecieved { get; set; }
+        public string? ResponseComment { get; set; }
     }
 }
