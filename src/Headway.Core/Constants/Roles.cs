7ce5c2c46658d25ccc6fd74c0831d946ddc2a176
grantcolley <grantcolley@yahoo.com>
2022-06-05 09:31:04 +0100

    Update Roles.cs

--- src/Headway.Core/Constants/Roles.cs
+++ src/Headway.Core/Constants/Roles.cs
@@ -10,5 +10,7 @@
         public const string REMEDIATR_ADMIN = "remediatradmin";
         public const string REMEDIATR_CUSTOMER_READ = "remediatrcustomerread";
         public const string REMEDIATR_CUSTOMER_WRITE = "remediatrcustomerwrite";
+        public const string REMEDIATR_REDRESS_READ = "remediatrredressread";
+        public const string REMEDIATR_REDRESS_WRITE = "remediatrredresswrite";
     }
 }
