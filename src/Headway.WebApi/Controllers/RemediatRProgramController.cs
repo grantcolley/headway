262d3fe9039a562bf78ffbcf53464de8c5391123
grantcolley <grantcolley@yahoo.com>
2022-06-08 07:50:41 +0100

    Update RemediatRProgramController.cs

--- src/Headway.WebApi/Controllers/RemediatRProgramController.cs
+++ src/Headway.WebApi/Controllers/RemediatRProgramController.cs
@@ -1,4 +1,5 @@
 ﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
 using Headway.RemediatR.Core.Constants;
 using Headway.RemediatR.Core.Interface;
 using Microsoft.AspNetCore.Mvc;
@@ -23,7 +24,7 @@ namespace Headway.WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var authorised = await IsAuthorisedAsync(RemediatRRoles.REMEDIATR_ADMIN)
+            var authorised = await IsAuthorisedAsync(Permissions.ADMIN)
                 .ConfigureAwait(false);
 
             if (!authorised)
@@ -41,7 +42,7 @@ namespace Headway.WebApi.Controllers
         [HttpGet("{programId}")]
         public async Task<IActionResult> Get(int programId)
         {
-            var authorised = await IsAuthorisedAsync(RemediatRRoles.REMEDIATR_ADMIN)
+            var authorised = await IsAuthorisedAsync(Permissions.ADMIN)
                 .ConfigureAwait(false);
 
             if (!authorised)
@@ -59,7 +60,7 @@ namespace Headway.WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] RemediatR.Core.Model.Program program)
         {
-            var authorised = await IsAuthorisedAsync(RemediatRRoles.REMEDIATR_ADMIN)
+            var authorised = await IsAuthorisedAsync(Permissions.ADMIN)
                 .ConfigureAwait(false);
 
             if (!authorised)
@@ -77,7 +78,7 @@ namespace Headway.WebApi.Controllers
         [HttpPut]
         public async Task<IActionResult> Put([FromBody] RemediatR.Core.Model.Program program)
         {
-            var authorised = await IsAuthorisedAsync(RemediatRRoles.REMEDIATR_ADMIN)
+            var authorised = await IsAuthorisedAsync(Permissions.ADMIN)
                 .ConfigureAwait(false);
 
             if (!authorised)
@@ -95,7 +96,7 @@ namespace Headway.WebApi.Controllers
         [HttpDelete("{programId}")]
         public async Task<IActionResult> Delete(int programId)
         {
-            var authorised = await IsAuthorisedAsync(RemediatRRoles.REMEDIATR_ADMIN)
+            var authorised = await IsAuthorisedAsync(Permissions.ADMIN)
                 .ConfigureAwait(false);
 
             if (!authorised)
