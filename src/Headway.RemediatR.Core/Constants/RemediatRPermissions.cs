a98693f5df0429fc80a0e3736e4facedd7be9cea
grantcolley <grantcolley@yahoo.com>
2022-06-08 07:50:25 +0100

    Create RemediatRPermissions.cs

---
+++ src/Headway.RemediatR.Core/Constants/RemediatRPermissions.cs
@@ -0,0 +1,19 @@
+﻿namespace Headway.RemediatR.Core.Constants
+{
+    public static class RemediatRPermissions
+    {
+        // RemediatR
+        public const string CUSTOMER_READ = "Customer Read";
+        public const string CUSTOMER_WRITE = "Customer Write";
+        public const string REDRESS_READ = "Redress Read";
+        public const string REDRESS_WRITE = "Redress Write";
+        public const string REDRESS_TRANSITION = "Redress Transition";
+        public const string COMMUNICATION_DISPATCH = "Communication Dispatch Transition";
+        public const string AWAITING_REPONSE_TRANSITION = "Awaiting Response Transition";
+        public const string REDRESS_REVIEW_TRANSITION = "Redress Review Transition";
+        public const string REDRESS_COMPLETE = "Redress Complete";
+        public const string REFUND_CACULATION = "Refund Calculation Complete";
+        public const string REFUND_VERIFICATION_COMPLETE = "Refund Varification Complete";
+        public const string REFUND_REVIEW_TRANSITION = "Refund Review Transition";
+    }
+}
