74b5daefea245ded73116493a278887960afe13d
grantcolley <grantcolley@yahoo.com>
2022-06-08 20:48:25 +0100

    Create HeadwayAuthorisation.cs

---
+++ src/Headway.Core/Constants/HeadwayAuthorisation.cs
@@ -0,0 +1,10 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class HeadwayAuthorisation
+    {
+        // Default
+        public const string ADMIN = "Admin";
+        public const string USER = "User";
+        public const string DEVELOPER = "Developer";
+    }
+}
