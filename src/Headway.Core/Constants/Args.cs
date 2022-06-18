8ce68a73a5d3d84854db771d931e7653db429706
grantcolley <grantcolley@yahoo.com>
2022-06-18 20:27:01 +0100

    Update Args.cs

--- src/Headway.Core/Constants/Args.cs
+++ src/Headway.Core/Constants/Args.cs
@@ -12,7 +12,7 @@
         public const string LIST_CONFIG = "ListConfig";
         public const string LIST_PROPERTY = "ListProperty";
         public const string MODEL = "Model";
-        public const string SEARCH_PARAMETER = "SearchParameter";
+        public const string PROPAGATE_FIELDS = "PropagateFields";
         public const string TEXT_MULTILINE_ROWS = "Rows";
         public const string TYPE = "Type";
         public const string UNIQUE_PROPERTY = "UniqueProperty";
