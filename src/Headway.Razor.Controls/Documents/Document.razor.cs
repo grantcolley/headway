6c674514f9efac72748b50041d695892f4f35a98
grantcolley <grantcolley@yahoo.com>
2022-06-30 10:00:52 +0100

    Update Document.razor.cs

--- src/Headway.Razor.Controls/Documents/Document.razor.cs
+++ src/Headway.Razor.Controls/Documents/Document.razor.cs
@@ -1,9 +1,5 @@
 ﻿using Headway.Core.Attributes;
-using Headway.Core.Constants;
-using Headway.Core.Dynamic;
-using Headway.Core.Interface;
 using Headway.Razor.Controls.Base;
-using Headway.Razor.Controls.Model;
 using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Documents
@@ -16,62 +12,6 @@ namespace Headway.Razor.Controls.Documents
             await InitializeDynamicModelAsync().ConfigureAwait(false);
 
             await base.OnInitializedAsync().ConfigureAwait(false);
-        }
-
-        protected async Task Submit()
-        {
-            isSaveInProgress = true;
-
-            IResponse<DynamicModel<T>> response;
-
-            if (dynamicModel.Id.Equals(0))
-            {
-                response = await DynamicService
-                    .AddDynamicModelAsync<T>(dynamicModel)
-                    .ConfigureAwait(false);
-            }
-            else
-            {
-                response = await DynamicService
-                    .UpdateDynamicModelAsync<T>(dynamicModel)
-                    .ConfigureAwait(false);
-            }
-
-            SetCurrentModelContext(response);
-
-            isSaveInProgress = false;
-        }
-
-        protected async Task Delete()
-        {
-            var deleteResult = await CanDeleteDialog($"Do you really want to delete {dynamicModel.Title}").ConfigureAwait(false);
-
-            if (deleteResult.Cancelled)
-            {
-                return;
-            }
-
-            isDeleteInProgress = true;
-
-            var response = await DynamicService
-                .DeleteDynamicModelAsync(dynamicModel)
-                .ConfigureAwait(false);
-
-            var result = GetResponse(response);
-
-            if (result.Equals(0))
-            {
-                return;
-            }
-
-            Alert = new Alert
-            {
-                AlertType = Alerts.INFO,
-                Title = dynamicModel.Title,
-                Message = $"has been deleted."
-            };
-
-            isDeleteInProgress = false;
         }
     }
 }
