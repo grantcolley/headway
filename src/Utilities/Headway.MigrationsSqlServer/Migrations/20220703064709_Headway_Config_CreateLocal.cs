f0d688849192d3ed07211e86fd6a0d37294ef90c
grantcolley <grantcolley@yahoo.com>
2022-07-05 20:25:40 +0100

    SqlServer Migration Headway_Config_CreateLocal

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220703064709_Headway_Config_CreateLocal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Config_CreateLocal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CreateLocal",
+                table: "Configs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateLocal",
+                table: "Configs");
+        }
+    }
+}
