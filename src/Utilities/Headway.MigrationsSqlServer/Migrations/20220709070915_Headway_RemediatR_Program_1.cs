2b0e26ff7c0515470f72fb2bee997f51a2059730
grantcolley <grantcolley@yahoo.com>
2022-07-09 08:52:14 +0100

    SqlServer Migrations Program Description
    SqlServer Migrations Program Description

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220709070915_Headway_RemediatR_Program_1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_RemediatR_Program_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Programs",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Programs");
+        }
+    }
+}
