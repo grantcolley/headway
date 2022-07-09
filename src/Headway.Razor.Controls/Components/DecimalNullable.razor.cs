1ea150017690f1e50d1cdda998bd887912144f49
grantcolley <grantcolley@yahoo.com>
2022-07-09 22:07:13 +0100

    Create DecimalNullable.razor component
    Create DecimalNullable.razor component

---
+++ src/Headway.Razor.Controls/Components/DecimalNullable.razor.cs
@@ -0,0 +1,32 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq.Expressions;
+
+namespace Headway.Razor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class DecimalNullableBase : DynamicComponentBase
+    {
+        public Expression<Func<decimal?>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<decimal?>>(Field.MemberExpression);
+            }
+        }
+
+        public decimal? PropertyValue
+        {
+            get
+            {
+                return (decimal?)Field.PropertyInfo.GetValue(Field.Model);
+            }
+            set
+            {
+                Field.PropertyInfo.SetValue(Field.Model, value);
+            }
+        }
+    }
+}
