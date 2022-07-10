2a23466eca194585079d9edf4925bdffe035f57c
grantcolley <grantcolley@yahoo.com>
2022-07-10 07:51:49 +0100

    Update Decimal.razor
    Update Decimal.razor

--- src/Headway.Razor.Controls/Components/Decimal.razor.cs
+++ src/Headway.Razor.Controls/Components/Decimal.razor.cs
@@ -1,14 +1,59 @@
 ﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Helpers;
 using Headway.Razor.Controls.Base;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Components
 {
     [DynamicComponent]
     public abstract class DecimalBase : DynamicComponentBase
     {
+        protected string format = string.Empty;
+        protected int? maxLength = null;
+        protected decimal? min = null;
+        protected decimal? max = null;
+
+        protected override Task OnInitializedAsync()
+        {
+            var formatArg = ComponentArgHelper.GetArg(ComponentArgs, Args.FORMAT);
+
+            if (formatArg != null)
+            {
+                format = formatArg.Value;
+            }
+
+            var maxLengthArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MAX_LENGTH);
+
+            if (maxLengthArg != null)
+            {
+                maxLength = int.Parse(maxLengthArg.Value);
+            }
+
+            var minArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MIN);
+
+            if (minArg != null)
+            {
+                min = decimal.Parse(minArg.Value);
+            }
+
+            var maxArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MAX);
+
+            if (maxArg != null)
+            {
+                max = decimal.Parse(maxArg.Value);
+            }
+
+            return base.OnInitializedAsync();
+        }
+
+        public int MaxLength { get { return maxLength.HasValue ? maxLength.Value : int.MaxValue; } }
+        public decimal Max { get { return max ?? decimal.MaxValue; } }
+        public decimal Min { get { return min ?? decimal.MinValue; } }
+
         public Expression<Func<decimal>> FieldExpression
         {
             get
