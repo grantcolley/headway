8fc8e0bb512ec5e88fe3579b576fc8a6bc1fe482
grantcolley <grantcolley@yahoo.com>
2022-07-11 09:32:41 +0100

    Add Min and Max to Integer.razor

--- src/Headway.Razor.Controls/Components/Integer.razor.cs
+++ src/Headway.Razor.Controls/Components/Integer.razor.cs
@@ -1,14 +1,51 @@
 ﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Helpers;
 using Headway.Razor.Controls.Base;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Components
 {
     [DynamicComponent]
     public abstract class IntegerBase : DynamicComponentBase
     {
+        protected int? maxLength = null;
+        protected int? min = null;
+        protected int? max = null;
+
+        protected override Task OnInitializedAsync()
+        {
+            var maxLengthArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MAX_LENGTH);
+
+            if (maxLengthArg != null)
+            {
+                maxLength = int.Parse(maxLengthArg.Value);
+            }
+
+            var minArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MIN);
+
+            if (minArg != null)
+            {
+                min = int.Parse(minArg.Value);
+            }
+
+            var maxArg = ComponentArgHelper.GetArg(ComponentArgs, Args.MAX);
+
+            if (maxArg != null)
+            {
+                max = int.Parse(maxArg.Value);
+            }
+
+            return base.OnInitializedAsync();
+        }
+
+        public int MaxLength { get { return maxLength.HasValue ? maxLength.Value : int.MaxValue; } }
+        public int Max { get { return max ?? int.MaxValue; } }
+        public int Min { get { return min ?? int.MinValue; } }
+
         public Expression<Func<int>> FieldExpression
         {
             get
