ce0068368d6687efd4bb28db7c7e7f8d1487ba1a
grantcolley <grantcolley@yahoo.com>
2022-07-12 19:25:05 +0100

    Add FluentValidators for RemediatR #52
    Add FluentValidators for RemediatR #52

---
+++ src/Headway.RemediatR.Core/Validation/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Headway.RemediatR.Core.Model;
+
+namespace Headway.RemediatR.Core.Validation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotNull().WithMessage("Name is required")
+                .Length(1, 50).WithMessage("Name cannot exceed 50 characters");
+
+            RuleFor(v => v.StartDate)
+                .NotNull().WithMessage("Start Date is required");
+        }
+    }
+}
---
+++ src/Headway.RemediatR.Core/Validation/ProgramValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Headway.RemediatR.Core.Model;
+
+namespace Headway.RemediatR.Core.Validation
+{
+    public class ProgramValidator : AbstractValidator<Program>
+    {
+        public ProgramValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(1, 50).WithMessage("Name cannot exceed 50 characters");
+
+            RuleFor(v => v.Description)
+                .NotEmpty().WithMessage("Description is required")
+                .Length(1, 150).WithMessage("Description cannot exceed 150 characters");
+        }
+    }
+}
