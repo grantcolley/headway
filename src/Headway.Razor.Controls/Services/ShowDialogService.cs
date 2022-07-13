9309b5ca3a734a4801735ce9202d41a7853cbb03
grantcolley <grantcolley@yahoo.com>
2022-07-13 19:39:08 +0100

    Update ShowDialogService.cs

--- src/Headway.Razor.Controls/Services/ShowDialogService.cs
+++ src/Headway.Razor.Controls/Services/ShowDialogService.cs
@@ -7,9 +7,24 @@ namespace Headway.Razor.Controls.Services
     public class ShowDialogService : IShowDialogService
     {
         private readonly IDialogService dialogService;
+
         public ShowDialogService(IDialogService dialogService)
         {
             this.dialogService = dialogService;
+        }
+
+        public async Task<DialogResult> ValidationErrorsAsync(string message)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("ContentText", message);
+            parameters.Add("ButtonText", "Ok");
+            parameters.Add("Color", Color.Error);
+
+            var options = new DialogOptions() { MaxWidth = MaxWidth.ExtraSmall };
+
+            var result = dialogService.Show<ScrollableDialog>("Validation Errors", parameters, options);
+
+            return await result.Result.ConfigureAwait(false);
         }
 
         public async Task<DialogResult> DeleteAsync(string message)
