28d07b07ed62a4da78f0baffa732f1361da580eb
grantcolley <grantcolley@yahoo.com>
2022-07-19 08:10:09 +0100

    Create RedressValidator.cs

---
+++ src/Headway.RemediatR.Core/Validation/RedressValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Headway.RemediatR.Core.Model;
+
+namespace Headway.RemediatR.Core.Validation
+{
+    public class RedressValidator : AbstractValidator<Redress>
+    {
+        public RedressValidator()
+        {
+            RuleFor(v => v.Customer)
+                .NotNull().WithMessage("Customer is required");
+
+            RuleFor(v => v.Program)
+                .NotNull().WithMessage("Program is required");
+
+            RuleFor(v => v.RedressProduct)
+                .NotNull().WithMessage("Product is required");
+
+            //RuleFor(v => v.RefundCalculation).SetValidator(new RefundCalculationValidator());
+            //RuleFor(v => v.RefundVerification).SetValidator(new RefundCalculationValidator());
+        }
+    }
+}
