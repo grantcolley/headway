5be978742ca9230dd654f428b670f678802a14d6
grantcolley <grantcolley@yahoo.com>
2022-07-20 20:03:51 +0100

    Delete RedressProduct.cs

--- src/Headway.RemediatR.Core/Model/RedressProduct.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Headway.RemediatR.Core.Model
-{
-    public class RedressProduct
-    {
-        public int RedressProductId { get; set; }
-        public Redress Redress { get; set; }
-        public Product Product { get; set; }
-    }
-}
