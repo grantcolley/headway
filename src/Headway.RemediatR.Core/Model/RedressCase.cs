40093ad09b8e0e66816b3331b3ca65041610d4af
grantcolley <grantcolley@yahoo.com>
2022-07-21 07:15:15 +0100

    Create RedressCase.cs

---
+++ src/Headway.RemediatR.Core/Model/RedressCase.cs
@@ -0,0 +1,10 @@
+﻿namespace Headway.RemediatR.Core.Model
+{
+    public class RedressCase
+    {
+        public int RedressId { get; set; }
+        public string CustomerName { get; set; }
+        public string ProgramName { get; set; }
+        public string Status { get; set; }
+    }
+}
