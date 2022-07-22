744ba3de4515d1c152cbee1fe4e77fcec14d8a71
grantcolley <grantcolley@yahoo.com>
2022-07-22 17:09:11 +0100

    Create ConfigSearchItem.cs

---
+++ src/Headway.Core/Model/ConfigSearchItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Headway.Core.Model
+{
+    public class ConfigSearchItem
+    {
+        public int ConfigSearchItemId { get; set; }
+        public int Order { get; set; }
+
+        [Required(ErrorMessage = "Label is required")]
+        [StringLength(50, ErrorMessage = "Label must be between 1 and 50 characters")]
+        public string Label { get; set; }
+
+        [StringLength(50, ErrorMessage = "Tooltip cannot exceed 50 characters")]
+        public string Tooltip { get; set; }
+
+        [StringLength(150, ErrorMessage = "Component cannot exceed 150 characters")]
+        public string Component { get; set; }
+    }
+}
