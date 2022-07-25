37864944699db430548ab28b7d9d6c3c5dc863e7
grantcolley <grantcolley@yahoo.com>
2022-07-25 13:17:42 +0100

    Update Css.cs

--- src/Headway.Core/Constants/Css.cs
+++ src/Headway.Core/Constants/Css.cs
@@ -2,7 +2,12 @@
 {
     public static class Css
     {
+        public const string HEADER_BUTTON = "HeaderButton";
+        public const string ROW_BUTTON = "RowButton";
+
         public const string BTN_PRIMARY = "btn-primary";
         public const string BTN_SECONDARY = "btn-secondary";
+        public const string BTN_IMAGE_PLUS = "fas fa-plus";
+        public const string BTN_IMAGE_EDIT = "fas fa-edit";
     }
 }
