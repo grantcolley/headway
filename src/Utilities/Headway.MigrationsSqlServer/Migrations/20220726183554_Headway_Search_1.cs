c22f3b649322122a3672812dd0fb553298b7f46f
grantcolley <grantcolley@yahoo.com>
2022-07-26 19:46:26 +0100

    SqlServer Migrations for searching
    SqlServer Migrations for searching

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220726183554_Headway_Search_1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_Search_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SearchComponent",
+                table: "Configs",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchComponent",
+                table: "Configs");
+        }
+    }
+}
