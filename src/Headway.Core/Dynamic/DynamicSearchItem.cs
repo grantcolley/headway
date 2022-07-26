f62aaff17525d8018dbd00dc1480f1291bac7ca5
grantcolley <grantcolley@yahoo.com>
2022-07-26 19:48:21 +0100

    Create DynamicSearchItem.cs

---
+++ src/Headway.Core/Dynamic/DynamicSearchItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Headway.Core.Dynamic
+{
+    public class DynamicSearchItem
+    {
+        public DynamicSearchItem()
+        {
+            Parameters = new Dictionary<string, object>();
+        }
+
+        public int Order { get; set; }
+        public string Label { get; set; }
+        public string Tooltip { get; set; }
+        public Type SearchComponent { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+    }
+}
