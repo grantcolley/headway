b4232fbf0a5020d59ee370aa8550c24413a3fb6e
grantcolley <grantcolley@yahoo.com>
2022-07-27 20:05:08 +0100

    Create SearchComponentBase.cs

---
+++ src/Headway.Razor.Controls/Base/SearchComponentBase.cs
@@ -0,0 +1,43 @@
+﻿using Headway.Core.Constants;
+using Headway.Core.Dynamic;
+using Headway.Core.Model;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Headway.Razor.Controls.Base
+{
+    public class SearchComponentBase : HeadwayComponentBase
+    {
+        [Parameter]
+        public DynamicSearchItem SearchItem { get; set; }
+
+        [Parameter]
+        public List<Arg> ComponentArgs { get; set; }
+
+        protected void LinkFieldCheck()
+        {
+            if (SearchItem.IsLinkedSearchItem)
+            {
+                SetLinkedValue();
+            }
+        }
+
+        protected void SetLinkedValue()
+        {
+            if (SearchItem.IsLinkedSearchItem)
+            {
+                var value = SearchItem.LinkValue;
+
+                var linkValueArg = ComponentArgs.DynamicArgOrDefault(Args.LINK_VALUE);
+
+                if (linkValueArg == null)
+                {
+                    linkValueArg = new Arg { Name = Args.LINK_VALUE };
+                    ComponentArgs.Add(linkValueArg);
+                }
+
+                linkValueArg.Value = value;
+            }
+        }
+    }
+}
