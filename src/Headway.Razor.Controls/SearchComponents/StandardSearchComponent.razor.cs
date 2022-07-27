ccb788019b8b4ed951fd6ecee312fe2b7f724059
grantcolley <grantcolley@yahoo.com>
2022-07-27 20:05:31 +0100

    Create StandardSearchComponent.razor.cs

---
+++ src/Headway.Razor.Controls/SearchComponents/StandardSearchComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Headway.Razor.Controls.SearchComponents
+{
+    public class StandardSearchComponentBase : HeadwayComponentBase
+    {
+        [Parameter]
+        public List<DynamicSearchItem> SearchItems { get; set; }
+    }
+}
