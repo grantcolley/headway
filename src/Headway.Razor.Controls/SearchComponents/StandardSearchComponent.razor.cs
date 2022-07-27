917e1416d75bde33a48a5a6d32c2c7bb56d00a7a
grantcolley <grantcolley@yahoo.com>
2022-07-27 20:38:27 +0100

    Update StandardSearchComponent.razor.cs

--- src/Headway.Razor.Controls/SearchComponents/StandardSearchComponent.razor.cs
+++ src/Headway.Razor.Controls/SearchComponents/StandardSearchComponent.razor.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Headway.Razor.Controls.SearchComponents
 {
-    public class StandardSearchComponentBase : HeadwayComponentBase
+    public class StandardSearchComponentBase : SearchComponentBase
     {
         [Parameter]
         public List<DynamicSearchItem> SearchItems { get; set; }
