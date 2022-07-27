b87008a87117f95275cb9f094274d15dda5f60ab
grantcolley <grantcolley@yahoo.com>
2022-07-27 20:55:17 +0100

    Create SearchOptionItemsRequest.cs

---
+++ src/Headway.RequestApi/Requests/SearchOptionItemsRequest.cs
@@ -0,0 +1,12 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Headway.RequestApi.Requests
+{
+    public record SearchOptionItemsRequest(List<Arg> Args) : IRequest<SearchOptionItemsRequest.Response>
+    {
+        public record Response(IResponse<IEnumerable<OptionItem>> OptionItems);
+    }
+}
