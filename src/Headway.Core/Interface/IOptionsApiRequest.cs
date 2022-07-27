5c97cfaadc9e224dc9567ba43d4624db0c84ab24
grantcolley <grantcolley@yahoo.com>
2022-07-27 20:55:06 +0100

    Update IOptionsApiRequest.cs

--- src/Headway.Core/Interface/IOptionsApiRequest.cs
+++ src/Headway.Core/Interface/IOptionsApiRequest.cs
@@ -6,6 +6,7 @@ namespace Headway.Core.Interface
 {
     public interface IOptionsApiRequest
     {
+        Task<IResponse<IEnumerable<OptionItem>>> GetOptionItemsAsync(List<Arg> args);
         Task<IResponse<IEnumerable<OptionItem>>> GetOptionItemsAsync(List<DynamicArg> dynamicArgs);
         Task<IResponse<IEnumerable<T>>> GetOptionItemsAsync<T>(List<DynamicArg> dynamicArgs);
     }
