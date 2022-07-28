8eade76c2db215a4e665eb26f9b77f7ebe3c31e9
grantcolley <grantcolley@yahoo.com>
2022-07-28 08:37:34 +0100

    Create EnumNamesOptionItems.cs

---
+++ src/Headway.Core/Options/EnumNamesOptionItems.cs
@@ -0,0 +1,29 @@
+﻿using Headway.Core.Constants;
+using Headway.Core.Extensions;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Options
+{
+    public class EnumNamesOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<Arg> args)
+        {
+            var typeName = args.ArgValue(Args.TYPE);
+
+            var type = Type.GetType(typeName);
+
+            string[] enumIems = Enum.GetNames(type);
+
+            var optionItems = new List<OptionItem>(enumIems.Select(i => new OptionItem { Id = i, Display = i }));
+
+            optionItems.Insert(0, new OptionItem());
+
+            return Task.FromResult(optionItems.AsEnumerable());
+        }
+    }
+}
