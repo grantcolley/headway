79e44e97739d7d6b04d545a3e8cf8dcc72f37976
grantcolley <grantcolley@yahoo.com>
2022-07-30 13:29:50 +0100

    Update SearchComponentBase.cs

--- src/Headway.Razor.Controls/Base/SearchComponentBase.cs
+++ src/Headway.Razor.Controls/Base/SearchComponentBase.cs
@@ -1,6 +1,9 @@
-﻿using Headway.Core.Notifications;
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Notifications;
+using Headway.Razor.Controls.Model;
 using Microsoft.AspNetCore.Components;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.Base
@@ -10,8 +13,14 @@ namespace Headway.Razor.Controls.Base
         [Inject]
         public IStateNotification StateNotification { get; set; }
 
+        [CascadingParameter]
+        public SearchCallback SearchAction { get; set; }
+
         [Parameter]
         public string SearchComponentUniqueId { get; set; }
+
+        [Parameter]
+        public List<DynamicSearchItem> SearchItems { get; set; }
 
         public void Dispose()
         {
@@ -25,6 +34,11 @@ namespace Headway.Razor.Controls.Base
             StateNotification.Register(SearchComponentUniqueId, StateHasChanged);
 
             await base.OnInitializedAsync().ConfigureAwait(false);
+        }
+
+        protected async Task Search()
+        {
+            await SearchAction?.Seach.Invoke();
         }
     }
 }
