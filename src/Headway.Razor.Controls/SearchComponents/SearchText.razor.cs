68c7ce1b233585d0d62e4aa651e18483937714b5
grantcolley <grantcolley@yahoo.com>
2022-08-01 06:40:24 +0100

    Create SearchText.razor component

---
+++ src/Headway.Razor.Controls/SearchComponents/SearchText.razor.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Attributes;
+using Headway.Razor.Controls.Base;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.Razor.Controls.SearchComponents
+{
+    [DynamicComponent]
+    public abstract class SearchTextBase : SearchItemComponentBase
+    {
+    }
+}
