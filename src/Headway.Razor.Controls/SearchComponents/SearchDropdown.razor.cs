7c722cccc71793cd0f2b80e33c2d085d7fe337a5
grantcolley <grantcolley@yahoo.com>
2022-08-01 20:47:51 +0100

    Update SearchDropdown.razor.cs

--- src/Headway.Razor.Controls/SearchComponents/SearchDropdown.razor.cs
+++ src/Headway.Razor.Controls/SearchComponents/SearchDropdown.razor.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Headway.Razor.Controls.SearchComponents
 {
-    [DynamicComponent]
+    [DynamicSearchComponent]
     public abstract class SearchDropdownBase : SearchItemComponentBase
     {
         [Inject]
