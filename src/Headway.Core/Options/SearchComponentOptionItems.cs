54de0b0a365c71fdbe3afe67765d60c9d5c5c029
grantcolley <grantcolley@yahoo.com>
2022-08-01 20:47:59 +0100

    Create SearchComponentOptionItems.cs

---
+++ src/Headway.Core/Options/SearchComponentOptionItems.cs
@@ -0,0 +1,30 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Helpers;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Options
+{
+    public class SearchComponentOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<Arg> args)
+        {
+            var components = TypeAttributeHelper.GetHeadwayTypesByAttribute(typeof(DynamicSearchComponentAttribute));
+
+            List<OptionItem> optionItems = new() { new OptionItem() };
+
+            optionItems.AddRange((from c in components
+                              orderby c.Name
+                              select new OptionItem
+                              {
+                                  Id = c.Namespace,
+                                  Display = c.DisplayName
+                              }).ToList());
+
+            return Task.FromResult(optionItems.AsEnumerable());
+        }
+    }
+}
