1c188aed2277cc6637a0159b25b334bdd40a4064
grantcolley <grantcolley@yahoo.com>
2022-08-02 07:51:31 +0100

    Create SearchCriteria.cs

---
+++ src/Headway.Core/Model/SearchCriteria.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class SearchCriteria
+    {
+        public SearchCriteria(List<SearchCriterion> criteria)
+        {
+            Clauses = new List<SearchCriterion>(criteria);
+        }
+
+        public List<SearchCriterion> Clauses { get; }
+    }
+}
