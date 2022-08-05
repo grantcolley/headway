6db9149e47dcb5d95048fd3728be4e28805b8dd9
grantcolley <grantcolley@yahoo.com>
2022-08-05 08:44:21 +0100

    Create SearchItemCallback.cs

---
+++ src/Headway.Razor.Controls/Callbacks/SearchItemCallback.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Headway.Razor.Controls.Callbacks
+{
+    public class SearchItemCallback
+    {
+        public Func<Task> OnKeyDown { get; set; }
+    }
+}
