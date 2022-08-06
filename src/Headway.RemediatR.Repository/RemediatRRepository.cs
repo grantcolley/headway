34042ec6eeeaca6a75c34813b7228d5756c2a44a
grantcolley <grantcolley@yahoo.com>
2022-08-06 21:18:09 +0100

    Update RemediatRRepository.cs

--- src/Headway.RemediatR.Repository/RemediatRRepository.cs
+++ src/Headway.RemediatR.Repository/RemediatRRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Headway.Core.Model;
+using Headway.RemediatR.Core.Enums;
 using Headway.RemediatR.Core.Interface;
 using Headway.RemediatR.Core.Model;
 using Headway.Repository;
 using Headway.Repository.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -282,6 +285,75 @@ namespace Headway.RemediatR.Repository
                     ProductName = r.ProductName,
                     ProgramName = r.ProgramName
                 })
+                .ToList();
+        }
+
+        public async Task<IEnumerable<RedressCase>> GetRedressesAsync(SearchCriteria searchCriteria)
+        {
+            var productTypeClause = searchCriteria.Clauses.First(c => c.ParameterName.Equals("ProductType"));
+            if (!string.IsNullOrWhiteSpace(productTypeClause.Value))
+            {
+                productTypeClause.Value = ((int)Enum.Parse<ProductType>(productTypeClause.Value)).ToString();
+            }
+
+            var rateTypeClause = searchCriteria.Clauses.First(c => c.ParameterName.Equals("RateType"));
+            if (!string.IsNullOrWhiteSpace(rateTypeClause.Value))
+            {
+                rateTypeClause.Value = ((int)Enum.Parse<RateType>(rateTypeClause.Value)).ToString();
+            }
+
+            var repaymentTypeClause = searchCriteria.Clauses.First(c => c.ParameterName.Equals("RepaymentType"));
+            if (!string.IsNullOrWhiteSpace(repaymentTypeClause.Value))
+            {
+                repaymentTypeClause.Value = ((int)Enum.Parse<RepaymentType>(repaymentTypeClause.Value)).ToString();
+            }
+
+            var productParameters = new List<SqlParameter>();
+            var productRawSql = @"SELECT * FROM Products ";
+            bool firstClause = true;
+
+            for (int i = 0; i < searchCriteria.Clauses.Count; i++)
+            {
+                if (string.IsNullOrWhiteSpace(searchCriteria.Clauses[i].Value)
+                    || searchCriteria.Clauses[i].Value.ToLowerInvariant().Equals("unknown"))
+                {
+                    continue;
+                }
+
+                if (firstClause)
+                {
+                    productRawSql += $" WHERE {searchCriteria.Clauses[i].ParameterName} = @{searchCriteria.Clauses[i].ParameterName}";
+                    firstClause = false;
+                }
+                else
+                {
+                    productRawSql += $" AND {searchCriteria.Clauses[i].ParameterName} = @{searchCriteria.Clauses[i].ParameterName}";
+                }
+
+                productParameters.Add(new SqlParameter($"@{searchCriteria.Clauses[i].ParameterName}", int.Parse(searchCriteria.Clauses[i].Value)));
+            }
+
+            var products = await applicationDbContext.Products
+                .FromSqlRaw(productRawSql, productParameters.ToArray())
+                .AsNoTracking()
+                .ToArrayAsync()
+                .ConfigureAwait(false);
+
+            var redressCases = await applicationDbContext.Redresses
+                    .Include(r => r.Customer)
+                    .Include(r => r.Program)
+                    .Include(r => r.Product)
+                    .AsNoTracking()
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+
+            return redressCases.Select(r => new RedressCase
+            {
+                RedressId = r.RedressId,
+                CustomerName = r.CustomerName,
+                ProductName = r.ProductName,
+                ProgramName = r.ProgramName
+            })
                 .ToList();
         }
 
