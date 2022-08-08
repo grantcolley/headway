113064844479503b1d45ed89158858217fc196a7
grantcolley <grantcolley@yahoo.com>
2022-08-08 10:40:18 +0100

    Create RemediatRSearchSource.cs

---
+++ src/Headway.RemediatR.Core/Constants/RemediatRSearchSource.cs
@@ -0,0 +1,8 @@
+﻿namespace Headway.RemediatR.Repository.Constants
+{
+    public static class RemediatRSearchSource
+    {
+        public const string REDRESSCASES = "RedressCases";
+        public const string NEWREDRESSCASE = "NewRedressCase";
+    }
+}
