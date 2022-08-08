3ecff3310dadd1616c7b07ade48fea99075eaf3f
grantcolley <grantcolley@yahoo.com>
2022-08-08 07:47:55 +0100

    Update RedressCase.cs

--- src/Headway.RemediatR.Core/Model/RedressCase.cs
+++ src/Headway.RemediatR.Core/Model/RedressCase.cs
@@ -5,7 +5,7 @@ namespace Headway.RemediatR.Core.Model
     [DynamicModel]
     public class RedressCase
     {
-        public int RedressId { get; set; }
+        public int? RedressId { get; set; }
         public string? CustomerName { get; set; }
         public string? ProgramName { get; set; }
         public string? ProductName { get; set; }
