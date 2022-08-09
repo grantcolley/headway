6452ae3148ae63de4f2d719b7834fbedc9f0f34a
grantcolley <grantcolley@yahoo.com>
2022-08-09 20:08:41 +0100

    Update SearchCriterion.cs

--- src/Headway.Core/Model/SearchCriterion.cs
+++ src/Headway.Core/Model/SearchCriterion.cs
@@ -2,6 +2,7 @@
 {
     public class SearchCriterion
     {
+        public string Name { get; set; }
         public string ParameterName { get; set; }
         public string Value { get; set; }
     }
