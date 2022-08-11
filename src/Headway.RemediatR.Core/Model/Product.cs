eb4d425bd23fc4a8e4def9c78c8d8bb24ce8f66f
grantcolley <grantcolley@yahoo.com>
2022-08-11 08:49:52 +0100

    Update Product.cs

--- src/Headway.RemediatR.Core/Model/Product.cs
+++ src/Headway.RemediatR.Core/Model/Product.cs
@@ -10,6 +10,7 @@ namespace Headway.RemediatR.Core.Model
     public class Product
     {
         public int ProductId { get; set; }
+        public int CustomerId { get; set; }
         public ProductType ProductType { get; set; }
         public RateType RateType { get; set; }
         public RepaymentType RepaymentType { get; set; }
