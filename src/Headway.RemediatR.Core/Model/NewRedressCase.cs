bb8b6997c5643676d63d864bc15cd9ce6d8a1b9c
grantcolley <grantcolley@yahoo.com>
2022-08-24 20:36:43 +0100

    Create NewRedressCase.cs

---
+++ src/Headway.RemediatR.Core/Model/NewRedressCase.cs
@@ -0,0 +1,17 @@
+﻿using Headway.Core.Attributes;
+
+namespace Headway.RemediatR.Core.Model
+{
+    [DynamicModel]
+    public class NewRedressCase
+    {
+        public int? RedressId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? ProgramName { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductType { get; set; }
+        public string? RateType { get; set; }
+        public string? RepaymentType { get; set; }
+        public string? Status { get; set; }
+    }
+}
