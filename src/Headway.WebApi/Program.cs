06270d4cbf80209ed60487598b0acb321c97ffd5
grantcolley <grantcolley@yahoo.com>
2022-08-26 18:22:35 +0100

    Update Program.cs

--- src/Headway.WebApi/Program.cs
+++ src/Headway.WebApi/Program.cs
@@ -14,7 +14,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
