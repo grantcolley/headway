d398c8faa100d445583091779693ee4a9596bca1
grantcolley <grantcolley@yahoo.com>
2022-08-28 21:13:11 +0100

    Update Controllers.cs

--- src/Headway.Core/Constants/Controllers.cs
+++ src/Headway.Core/Constants/Controllers.cs
@@ -3,6 +3,7 @@
     public static class Controllers
     {
         public const string CONFIGURATION = "Configuration";
+        public const string CREATE_ACTION = "Create";
         public const string MODULES_CLAIMMODULES = "Modules/ClaimModules";
         public const string OPTIONS = "Options";
         public const string OPTIONS_COMPLEXOPTIONS = "Options/ComplexOptions";
