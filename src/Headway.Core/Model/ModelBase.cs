d3c304f7bbcc14e6c1065789cafdc35174797eba
grantcolley <grantcolley@yahoo.com>
2022-09-04 10:50:53 +0100

    Create ModelBase.cs

---
+++ src/Headway.Core/Model/ModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Headway.Core.Model
+{
+    public abstract class ModelBase
+    {
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
