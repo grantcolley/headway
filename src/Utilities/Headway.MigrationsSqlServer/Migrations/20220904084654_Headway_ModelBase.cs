8ad6711f972729d6a525b883134d2da2a99e85fe
grantcolley <grantcolley@yahoo.com>
2022-09-04 10:52:02 +0100

    Migrations for ModelBase

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220904084654_Headway_ModelBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ModelBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Configs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Configs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Configs",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Configs");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Configs");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Configs");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Configs");
+        }
+    }
+}
