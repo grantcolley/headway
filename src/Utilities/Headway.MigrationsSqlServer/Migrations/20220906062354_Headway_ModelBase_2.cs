536ed6b95707ad778ed7689d0aed5563f946d89f
grantcolley <grantcolley@yahoo.com>
2022-09-06 07:31:49 +0100

    SQL Migrations

---
+++ src/Utilities/Headway.MigrationsSqlServer/Migrations/20220906062354_Headway_ModelBase_2.cs
@@ -0,0 +1,536 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    public partial class Headway_ModelBase_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Roles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Roles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Permissions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Permissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Permissions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Permissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Modules",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Modules",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "MenuItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "MenuItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "MenuItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "MenuItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DemoModelTreeItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "DemoModelTreeItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "DemoModelTreeItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "DemoModelTreeItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DemoModels",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "DemoModels",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "DemoModels",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "DemoModels",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DemoModelItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "DemoModelItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "DemoModelItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "DemoModelItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DemoModelComplexProperties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "DemoModelComplexProperties",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "DemoModelComplexProperties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "DemoModelComplexProperties",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ConfigSearchItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ConfigSearchItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ConfigSearchItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ConfigSearchItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ConfigItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ConfigItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ConfigItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ConfigItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ConfigContainers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ConfigContainers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ConfigContainers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ConfigContainers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DemoModelTreeItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "DemoModelTreeItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "DemoModelTreeItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "DemoModelTreeItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DemoModels");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "DemoModels");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "DemoModels");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "DemoModels");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DemoModelItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "DemoModelItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "DemoModelItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "DemoModelItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DemoModelComplexProperties");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "DemoModelComplexProperties");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "DemoModelComplexProperties");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "DemoModelComplexProperties");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ConfigSearchItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ConfigSearchItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ConfigSearchItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ConfigSearchItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ConfigItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ConfigItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ConfigItems");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ConfigItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ConfigContainers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ConfigContainers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ConfigContainers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ConfigContainers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Categories");
+        }
+    }
+}
