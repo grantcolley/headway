0c6f5d3e29432432002effb70692a5232f238de1
grantcolley <grantcolley@yahoo.com>
2022-09-07 16:58:18 +0100

    Create Log.cs

---
+++ src/Headway.Core/Model/Log.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Enums;
+
+namespace Headway.Core.Model
+{
+    public class Log
+    {
+        public LogLevel Level { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
