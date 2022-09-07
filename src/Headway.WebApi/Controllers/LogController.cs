dba37c7893d1ed8ac8391aa3bd04ddae49843d0a
grantcolley <grantcolley@yahoo.com>
2022-09-07 17:02:15 +0100

    Create LogController.cs

---
+++ src/Headway.WebApi/Controllers/LogController.cs
@@ -0,0 +1,39 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    [DynamicApiController]
+    public class LogController : ApiControllerBase<LogController>
+    {
+        private readonly ILogRepository logRepository;
+
+        public LogController(
+            ILogRepository logRepository,
+            ILogger<LogController> logger)
+            : base(logRepository, logger)
+        {
+            this.logRepository = logRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Core.Model.Log log)
+        {
+            var authorised = await IsAuthorisedAsync(HeadwayAuthorisation.USER)
+                .ConfigureAwait(false);
+
+            if (!authorised)
+            {
+                return Unauthorized();
+            }
+
+            logRepository.LogAsync(log);
+
+            return Ok();
+        }
+    }
+}
