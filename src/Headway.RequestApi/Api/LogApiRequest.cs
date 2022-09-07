c290985833d6d94cc9c59d71c599757f38646323
grantcolley <grantcolley@yahoo.com>
2022-09-07 21:01:47 +0100

    Create LogApiRequest.cs

---
+++ src/Headway.RequestApi/Api/LogApiRequest.cs
@@ -0,0 +1,28 @@
+﻿using Headway.Core.Constants;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Headway.RequestApi.Api
+{
+    public class LogApiRequest : ApiRequestBase, ILogApiRequest
+    {
+        public LogApiRequest(HttpClient httpClient)
+            : base(httpClient, false, null)
+        {
+        }
+
+        public LogApiRequest(HttpClient httpClient, TokenProvider tokenProvider)
+            : base(httpClient, true, tokenProvider)
+        {
+        }
+
+        public async Task LogAsync(Log log)
+        {
+            using var httpResponseMessage = await httpClient.PostAsJsonAsync(Controllers.LOG, log)
+                .ConfigureAwait(false);
+        }
+    }
+}
