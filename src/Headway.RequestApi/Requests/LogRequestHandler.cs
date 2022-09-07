e18b422502dc340558b427da2cc373f1c1399e14
grantcolley <grantcolley@yahoo.com>
2022-09-07 21:02:00 +0100

    Create LogRequestHandler.cs

---
+++ src/Headway.RequestApi/Requests/LogRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using Headway.Core.Interface;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Headway.RequestApi.Requests
+{
+    public class LogRequestHandler : IRequestHandler<LogRequest, Unit>
+    {
+        private readonly ILogApiRequest logApiRequest;
+
+        public LogRequestHandler(ILogApiRequest logApiRequest)
+        {
+            this.logApiRequest = logApiRequest;
+        }
+
+        public async Task<Unit> Handle(LogRequest request, CancellationToken cancellationToken)
+        {
+            await logApiRequest.LogAsync(request.Log).ConfigureAwait(false);
+            return Unit.Value;
+        }
+    }
+}
