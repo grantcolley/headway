b43fe8a544cbf38d063ac1b7ba3769c5494b8cfa
grantcolley <grantcolley@yahoo.com>
2022-09-07 16:58:23 +0100

    Create LogRepository.cs

---
+++ src/Headway.Repository/Repositories/LogRepository.cs
@@ -0,0 +1,44 @@
+﻿using Headway.Core.Interface;
+using Headway.Core.Model;
+using Headway.Repository.Data;
+using Microsoft.Extensions.Logging;
+
+namespace Headway.Repository.Repositories
+{
+    public class LogRepository : RepositoryBase<LogRepository>, ILogRepository
+    {
+        public LogRepository(ApplicationDbContext applicationDbContext, ILogger<LogRepository> logger)
+            : base(applicationDbContext, logger)
+        {
+        }
+
+        public void LogAsync(Log log)
+        {
+            if(log == null)
+            {
+                return;
+            }
+
+            switch(log.Level)
+            {
+                case Core.Enums.LogLevel.Information:
+                    logger.LogInformation(log.Message);
+                    break;
+                case Core.Enums.LogLevel.Warning:
+                    logger.LogWarning(log.Message);
+                    break;
+                case Core.Enums.LogLevel.Error:
+                    if(log.Exception != null)
+                    {
+                        logger.LogError(log.Exception, log.Message);
+                    }
+                    else
+                    {
+                        logger.LogError(log.Message);
+                    }
+
+                    break;
+            }
+        }
+    }
+}
