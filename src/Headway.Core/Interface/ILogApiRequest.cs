2f8338f34dbd127a03de02386c18266ea268ae16
grantcolley <grantcolley@yahoo.com>
2022-09-07 21:01:40 +0100

    Create ILogApiRequest.cs

---
+++ src/Headway.Core/Interface/ILogApiRequest.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Model;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface ILogApiRequest
+    {
+        Task LogAsync(Log log);
+    }
+}
