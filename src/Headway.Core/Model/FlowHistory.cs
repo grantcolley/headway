018f7b0655602391bd8b85243226be7b08674eb5
grantcolley <grantcolley@yahoo.com>
2022-12-15 20:42:16 +0000

    Create FlowHistory.cs

---
+++ src/Headway.Core/Model/FlowHistory.cs
@@ -0,0 +1,22 @@
+﻿using Headway.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Headway.Core.Model
+{
+    public abstract class FlowHistory : ModelBase
+    {
+        public StateStatus StateStatus { get; set; }
+
+        [StringLength(50)]
+        public string StateCode { get; set; }
+
+        [StringLength(50)]
+        public string Owner { get; set; }
+
+        [StringLength(50)]
+        public string Ascendant { get; set; }
+
+        [StringLength(50)]
+        public string Descendant { get; set; }
+    }
+}
