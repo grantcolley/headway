23df313aa74d530b397187911815121b013c41e0
grantcolley <grantcolley@yahoo.com>
2022-12-16 17:49:14 +0000

    Create StateAction.cs

---
+++ src/Headway.Core/Model/StateAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Headway.Core.Enums;
+
+namespace Headway.Core.Model
+{
+    public class StateAction
+    {
+        public StateActionType StateActionType { get; set; }
+
+        public int Order { get; set; }
+
+        public Func<State, object, Task> ActionAsync { get; set; }
+    }
+}
