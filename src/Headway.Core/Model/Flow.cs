9b9982129f834d3c15f9d54b28f6ab33f853ecc8
grantcolley <grantcolley@yahoo.com>
2022-12-17 13:24:36 +0000

    Update Flow.cs

--- src/Headway.Core/Model/Flow.cs
+++ src/Headway.Core/Model/Flow.cs
@@ -1,5 +1,6 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Enums;
+using Headway.Core.Extensions;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,6 +12,8 @@ namespace Headway.Core.Model
     [DynamicModel]
     public class Flow : ModelBase
     {
+        private bool configured;
+
         public Flow()
         {
             States = new List<State>();
@@ -18,10 +21,9 @@ namespace Headway.Core.Model
 
         public int FlowId { get; set; }
         public int ConfigId { get; set; }
+        public FlowStatus FlowStatus { get; set; }
         public List<State> States { get; set; }
-
-        [NotMapped]
-        public Config Config { get; set; }
+        public List<FlowHistory> History { get; set; }
 
         [Required]
         [StringLength(50)]
@@ -36,16 +38,55 @@ namespace Headway.Core.Model
         public string Permissions { get; set; }
 
         [NotMapped]
+        public Config Config { get; set; }
+
+        [NotMapped]
         [JsonIgnore]
         public State ActiveState { get; private set; }
 
+        [NotMapped]
+        [JsonIgnore]
+        public State RootState { get; private set; }
+
         public void SetActiveState(string activeStateCode = "", StateStatus activeStateStatus = StateStatus.NotStarted)
         {
-            if(States.Any())
+            if (!configured)
+            {
+                Configure();
+            }
+
+            if (string.IsNullOrWhiteSpace(activeStateCode))
+            {
+                ActiveState = RootState;
+            }
+            else
             {
                 ActiveState = States.FirstOrDefault(s => s.Code.Equals(activeStateCode));
-                ActiveState.StateStatus = activeStateStatus;
             }
+
+            ActiveState.StateStatus = activeStateStatus;
+        }
+
+        private void Configure()
+        {
+            var states = States.ToDictionary(s => s.Code, s => s);
+
+            var rootStatePoition = States.Min(s => s.Position);
+            RootState = States.First(s => s.Position.Equals(rootStatePoition));
+
+            foreach (var state in states)
+            {
+                if(!string.IsNullOrWhiteSpace(state.Value.ParentStateCode))
+                {
+                    state.Value.ParentState = states[state.Value.ParentStateCode];
+                }
+
+                state.Value.SubStates.AddRange(states.GetStates(state.Value.SubStateCodesList));
+                state.Value.Transitions.AddRange(states.GetStates(state.Value.TransitionStateCodesList));
+                state.Value.Dependencies.AddRange(states.GetStates(state.Value.DependencyStateCodesList));
+            }
+
+            configured = true;
         }
     }
 }
