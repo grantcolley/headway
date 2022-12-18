c254c5f7a17369613cd3cd35c59608ccb8562da2
grantcolley <grantcolley@yahoo.com>
2022-12-18 20:48:46 +0000

    Update RemediatRFlowTests.cs

--- tests/Headway.Core.Tests/RemediatRFlowTests.cs
+++ tests/Headway.Core.Tests/RemediatRFlowTests.cs
@@ -6,29 +6,62 @@ namespace Headway.Core.Tests
     public class RemediatRFlowTests
     {
         [TestMethod]
-        public void RemediatR_Flow_Configure()
+        public void Flow_RootState()
+        {
+            // Arrange
+            var flow = RemediatRFlow.FlowCreate();
+
+            // Act
+            var rootState = flow.RootState;
+
+            // Assert
+            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REDRESS_CREATE")), rootState);
+        }
+
+        [TestMethod]
+        public void Flow_ReplayHistory_NoHistory()
         {
             // Arrange
             var flow = RemediatRFlow.FlowCreate();
             var rootState = flow.RootState;
 
             // Act
-            flow.SetActiveState();
+            flow.ReplayHistory();
 
-            // Assert root and active states
+            // Assert active state
             Assert.AreEqual(flow.RootState, rootState);
             Assert.AreEqual(flow.ActiveState, rootState);
 
             // Assert sample of parent and substates
-            Assert.AreEqual(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")), flow.States.First(s => s.Code.Equals("REFUND_CALCULATION")).ParentState);
-            Assert.AreEqual(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")), flow.States.First(s => s.Code.Equals("REFUND_VERIFICATION")).ParentState);
-            Assert.IsTrue(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")).SubStates.Contains(flow.States.First(s => s.Code.Equals("REFUND_CALCULATION"))));
-            Assert.IsTrue(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")).SubStates.Contains(flow.States.First(s => s.Code.Equals("REFUND_VERIFICATION"))));
+            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")), flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")).ParentState);
+            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")), flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION")).ParentState);
+            Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).SubStates.Contains(flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION"))));
+            Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).SubStates.Contains(flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION"))));
 
             // Assert sample of transition states
-            Assert.IsTrue(flow.States.First(s => s.Code.Equals("REDRESS_CREATE")).Transitions.Contains(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT"))));
-            Assert.IsTrue(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")).Transitions.Contains(flow.States.First(s => s.Code.Equals("REDRESS_CREATE"))));
-            Assert.IsTrue(flow.States.First(s => s.Code.Equals("REFUND_ASSESSMENT")).Transitions.Contains(flow.States.First(s => s.Code.Equals("REFUND_REVIEW"))));
+            Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REDRESS_CREATE")).Transitions.Contains(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT"))));
+            Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).Transitions.Contains(flow.States.First(s => s.StateCode.Equals("REDRESS_CREATE"))));
+            Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).Transitions.Contains(flow.States.First(s => s.StateCode.Equals("REFUND_REVIEW"))));
+        }
+
+        public void State_Complete_Transition_ParentState()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+
+        }
+
+        public void State_Complete_Last_SubState()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+
         }
     }
 }
