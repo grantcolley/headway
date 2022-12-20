969d491ee4adf53da9d5974e572f2fb56c947ee9
grantcolley <grantcolley@yahoo.com>
2022-12-20 22:14:46 +0000

    Create FlowTestData.cs

---
+++ tests/Headway.Core.Tests/Helpers/FlowTestData.cs
@@ -0,0 +1,19 @@
+﻿using Headway.Core.Model;
+
+namespace Headway.Core.Tests.Helpers
+{
+    public class FlowTestData
+    {
+        public static Flow CreateFlow(int numberOfStates)
+        {
+            var flow = new Flow();
+
+            for(int i = 1; i < numberOfStates + 1; i++)
+            {
+                flow.States.Add(new State { Position = i * 10, StateCode = $"State {i}", Flow = flow });
+            }
+
+            return flow;
+        }
+    }
+}
