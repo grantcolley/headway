43710e56ac55fc0ca9aac313ce60c58ede4a90ee
grantcolley <grantcolley@yahoo.com>
2022-12-21 20:27:41 +0000

    Create FlowTestHelper.cs

---
+++ tests/Headway.Core.Tests/Helpers/FlowTestHelper.cs
@@ -0,0 +1,20 @@
+﻿using Headway.Core.Enums;
+using Headway.Core.Model;
+
+namespace Headway.Core.Tests.Helpers
+{
+    public class FlowTestHelper
+    {
+        public static Flow CreateFlow(int numberOfStates)
+        {
+            var flow = new Flow();
+
+            for(int i = 1; i < numberOfStates + 1; i++)
+            {
+                flow.States.Add(new State { Position = i * 10, StateCode = $"State {i}", Flow = flow });
+            }
+
+            return flow;
+        }
+    }
+}
