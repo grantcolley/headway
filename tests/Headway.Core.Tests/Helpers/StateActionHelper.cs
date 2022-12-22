9016abe870884cebdfcf7e9f23fe676b629c3f1a
grantcolley <grantcolley@yahoo.com>
2022-12-22 19:50:07 +0000

    Create StateActionHelper.cs

---
+++ tests/Headway.Core.Tests/Helpers/StateActionHelper.cs
@@ -0,0 +1,30 @@
+﻿using Headway.Core.Enums;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+
+namespace Headway.Core.Tests.Helpers
+{
+    public class StateActionHelper : ISetupStateActions
+    {
+        public void SetupActions(State state)
+        {
+            static Task action(State state, StateActionType stateActionType, int order)
+            {
+                if (state.Context == null)
+                {
+                    state.Context = $"{order} {stateActionType} {state.StateCode}";
+                }
+                else
+                {
+                    state.Context += $"; {order} {stateActionType} {state.StateCode}";
+                }
+
+                return Task.CompletedTask;
+            }
+
+            state.Context = null;
+            state.StateActions.Add(new StateAction { Order = 4, StateActionType = StateActionType.Initialize, ActionAsync = action });
+            state.StateActions.Add(new StateAction { Order = 3, StateActionType = StateActionType.Initialize, ActionAsync = action });
+        }
+    }
+}
