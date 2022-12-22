fa366ae4bc97fa855864667bbca3078b10916ef1
grantcolley <grantcolley@yahoo.com>
2022-12-22 09:46:07 +0000

    Create ISetupFlowActions.cs

---
+++ src/Headway.Core/Interface/ISetupFlowActions.cs
@@ -0,0 +1,10 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+
+namespace Headway.Core.Interface
+{
+    public interface ISetupFlowActions
+    {
+       void SetupActions(Dictionary<string, State>  stateDictionary);
+    }
+}
