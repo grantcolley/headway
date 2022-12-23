005576651579ec1df4c86086f1f9a48f170933ce
grantcolley <grantcolley@yahoo.com>
2022-12-23 23:05:36 +0000

    Update FlowStateActionHelper.cs

--- tests/Headway.Core.Tests/Helpers/FlowStateActionHelper.cs
+++ tests/Headway.Core.Tests/Helpers/FlowStateActionHelper.cs
@@ -5,30 +5,30 @@ using Headway.Core.Model;
 
 namespace Headway.Core.Tests.Helpers
 {
-    public class FlowStateActionHelper : ISetupFlowActions
+    public class FlowStateActionHelper : IConfigureFlowActions
     {
-        public void SetupActions(Dictionary<string, State> stateDictionary)
+        public void ConfigureActions(Dictionary<string, State> stateDictionary)
         {
-            static Task action(State state, StateActionType stateActionType, int order)
-            {
-                if (state.Context == null)
-                {
-                    state.Context = $"{order} {stateActionType} {state.StateCode}";
-                }
-                else
-                {
-                    state.Context += $"; {order} {stateActionType} {state.StateCode}";
-                }
-
-                return Task.CompletedTask;
-            }
-
             var states = stateDictionary.Values.ToList();
             var state = states.FirstState();
 
             state.Context = null;
-            state.StateActions.Add(new StateAction { Order = 2, StateActionType = StateActionType.Initialize, ActionAsync = action });
-            state.StateActions.Add(new StateAction { Order = 1, StateActionType = StateActionType.Initialize, ActionAsync = action });
+            state.StateActions.Add(new StateAction { Order = 2, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
+            state.StateActions.Add(new StateAction { Order = 1, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
+        }
+
+        private Task StateAction(State state, StateActionType stateActionType, int order)
+        {
+            if (state.Context == null)
+            {
+                state.Context = $"{order} {stateActionType} {state.StateCode}";
+            }
+            else
+            {
+                state.Context += $"; {order} {stateActionType} {state.StateCode}";
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
