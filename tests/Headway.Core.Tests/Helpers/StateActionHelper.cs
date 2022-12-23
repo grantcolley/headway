a4e969e437cffcaf4576b3975f5e62757cdb60fc
grantcolley <grantcolley@yahoo.com>
2022-12-23 23:05:45 +0000

    Update StateActionHelper.cs

--- tests/Headway.Core.Tests/Helpers/StateActionHelper.cs
+++ tests/Headway.Core.Tests/Helpers/StateActionHelper.cs
@@ -4,27 +4,27 @@ using Headway.Core.Model;
 
 namespace Headway.Core.Tests.Helpers
 {
-    public class StateActionHelper : ISetupStateActions
+    public class StateActionHelper : IConfigureStateActions
     {
-        public void SetupActions(State state)
+        public void ConfigureActions(State state)
         {
-            static Task action(State state, StateActionType stateActionType, int order)
-            {
-                if (state.Context == null)
-                {
-                    state.Context = $"{order} {stateActionType} {state.StateCode}";
-                }
-                else
-                {
-                    state.Context += $"; {order} {stateActionType} {state.StateCode}";
-                }
+            state.Context = null;
+            state.StateActions.Add(new StateAction { Order = 4, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
+            state.StateActions.Add(new StateAction { Order = 3, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
+        }
 
-                return Task.CompletedTask;
+        private Task StateAction(State state, StateActionType stateActionType, int order)
+        {
+            if (state.Context == null)
+            {
+                state.Context = $"{order} {stateActionType} {state.StateCode}";
+            }
+            else
+            {
+                state.Context += $"; {order} {stateActionType} {state.StateCode}";
             }
 
-            state.Context = null;
-            state.StateActions.Add(new StateAction { Order = 4, StateActionType = StateActionType.Initialize, ActionAsync = action });
-            state.StateActions.Add(new StateAction { Order = 3, StateActionType = StateActionType.Initialize, ActionAsync = action });
+            return Task.CompletedTask;
         }
     }
 }
