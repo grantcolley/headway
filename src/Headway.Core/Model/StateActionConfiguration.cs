b5b685f50d4bb2d2580b2a2c60c9e1576f76d147
grantcolley <grantcolley@yahoo.com>
2022-12-23 23:05:10 +0000

    Add action configuration cache classes

---
+++ src/Headway.Core/Model/StateActionConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Interface;
+using System.Reflection;
+
+namespace Headway.Core.Model
+{
+    public class StateActionConfiguration
+    {
+        public IConfigureStateActions Instance { get; set; }
+        public MethodInfo MethodInfo { get; set; }
+    }
+}
