a857d03cb91549d04ddb38dc830d32f984dabb5f
grantcolley <grantcolley@yahoo.com>
2022-12-23 23:14:27 +0000

    Update FlowTest.cs

--- tests/Headway.Core.Tests/FlowTest.cs
+++ tests/Headway.Core.Tests/FlowTest.cs
@@ -88,7 +88,7 @@ namespace Headway.Core.Tests
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.ActiveState.StateStatus, StateStatus.InProgress);
+            Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
         }
 
         [TestMethod]
@@ -103,10 +103,10 @@ namespace Headway.Core.Tests
             await flow.States.Find(s => s.StateCode.Equals("REFUND_ASSESSMENT")).InitialiseAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).StateStatus, StateStatus.InProgress);
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")).StateStatus, StateStatus.InProgress);
+            Assert.AreEqual(StateStatus.InProgress, flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).StateStatus);
+            Assert.AreEqual(StateStatus.InProgress, flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")).StateStatus);
+            Assert.AreEqual(StateStatus.NotStarted, flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION")).StateStatus);
             Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")), flow.ActiveState);
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION")).StateStatus, StateStatus.NotStarted);
         }
 
         [TestMethod]
@@ -132,7 +132,7 @@ namespace Headway.Core.Tests
             catch (InvalidOperationException ex)
             {
                 // Assert
-                Assert.AreEqual(ex.Message, $"Can't initialize {flow.States[1].StateStatus} because it's already {StateStatus.InProgress}.");
+                Assert.AreEqual($"Can't initialize {flow.States[1].StateStatus} because it's already {StateStatus.InProgress}.", ex.Message);
 
                 throw;
             }
@@ -152,9 +152,9 @@ namespace Headway.Core.Tests
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.ActiveState, flow.States.FirstState());
-            Assert.AreEqual(flow.ActiveState.StateStatus, StateStatus.InProgress);
-            Assert.AreEqual(flow.ActiveState.Context, $"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}");
+            Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
+            Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
+            Assert.AreEqual($"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Context);
         }
 
         [TestMethod]
@@ -171,9 +171,9 @@ namespace Headway.Core.Tests
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.ActiveState, flow.States.FirstState());
-            Assert.AreEqual(flow.ActiveState.StateStatus, StateStatus.InProgress);
-            Assert.AreEqual(flow.ActiveState.Context, $"3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}");
+            Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
+            Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
+            Assert.AreEqual($"3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Context);
         }
 
         [TestMethod]
@@ -194,9 +194,9 @@ namespace Headway.Core.Tests
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.ActiveState, flow.States.FirstState());
-            Assert.AreEqual(flow.ActiveState.StateStatus, StateStatus.InProgress);
-            Assert.AreEqual(flow.ActiveState.Context, activeStateContext);
+            Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
+            Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
+            Assert.AreEqual(activeStateContext, flow.ActiveState.Context);
         }
 
         [TestMethod]
@@ -213,8 +213,8 @@ namespace Headway.Core.Tests
             await flow.ActiveState.CompleteAsync().ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.States[0].StateStatus, StateStatus.Completed);
-            Assert.AreEqual(flow.States[1].StateStatus, StateStatus.InProgress);
+            Assert.AreEqual(StateStatus.Completed, flow.States[0].StateStatus);
+            Assert.AreEqual(StateStatus.InProgress, flow.States[1].StateStatus);
             Assert.AreEqual(flow.States[1], flow.ActiveState);
         }
 
@@ -232,8 +232,8 @@ namespace Headway.Core.Tests
             await flow.ActiveState.CompleteAsync(flow.States[1].StateCode).ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.States[0].StateStatus, StateStatus.Completed);
-            Assert.AreEqual(flow.States[1].StateStatus, StateStatus.InProgress);
+            Assert.AreEqual(StateStatus.Completed, flow.States[0].StateStatus);
+            Assert.AreEqual(StateStatus.InProgress, flow.States[1].StateStatus);
             Assert.AreEqual(flow.States[1], flow.ActiveState);
         }
 
@@ -249,11 +249,11 @@ namespace Headway.Core.Tests
             await flow.ActiveState.CompleteAsync("REFUND_ASSESSMENT").ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual(flow.States.FirstState().StateStatus, StateStatus.Completed);
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).StateStatus, StateStatus.InProgress);
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")).StateStatus, StateStatus.InProgress);
+            Assert.AreEqual(StateStatus.Completed, flow.States.FirstState().StateStatus);
+            Assert.AreEqual(StateStatus.InProgress, flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT")).StateStatus);
+            Assert.AreEqual(StateStatus.InProgress, flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")).StateStatus);
+            Assert.AreEqual(StateStatus.NotStarted, flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION")).StateStatus);
             Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_CALCULATION")), flow.ActiveState);
-            Assert.AreEqual(flow.States.First(s => s.StateCode.Equals("REFUND_VERIFICATION")).StateStatus, StateStatus.NotStarted);
         }
     }
 }
