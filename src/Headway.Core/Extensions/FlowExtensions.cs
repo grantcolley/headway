2678a825316cb282cef6814cf93bf5a5dabc1bdf
grantcolley <grantcolley@yahoo.com>
2022-12-25 13:31:09 +0000

    Update FlowExtensions.cs

--- src/Headway.Core/Extensions/FlowExtensions.cs
+++ src/Headway.Core/Extensions/FlowExtensions.cs
@@ -25,6 +25,7 @@ namespace Headway.Core.Extensions
             {
                 state.StateStatus = default;
                 state.Owner = default;
+
                 state.Flow = flow;
 
                 if (state.Context != null)
