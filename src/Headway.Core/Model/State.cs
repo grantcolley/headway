36da9d28757a2b4a9b4ab4764774790601c2b676
grantcolley <grantcolley@yahoo.com>
2022-12-27 13:19:12 +0000

    Update State.cs

--- src/Headway.Core/Model/State.cs
+++ src/Headway.Core/Model/State.cs
@@ -1,12 +1,10 @@
 ﻿using Headway.Core.Attributes;
 using Headway.Core.Enums;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace Headway.Core.Model
 {
