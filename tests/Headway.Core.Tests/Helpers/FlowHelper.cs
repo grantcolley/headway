c30e2ba562a42dc7f0503cc622f096018dd7efdd
grantcolley <grantcolley@yahoo.com>
2022-12-28 19:27:20 +0000

    Update FlowHelper.cs

--- tests/Headway.Core.Tests/Helpers/FlowHelper.cs
+++ tests/Headway.Core.Tests/Helpers/FlowHelper.cs
@@ -24,20 +24,20 @@ namespace Headway.Core.Tests.Helpers
             var states = flow.StateDictionary.Values.ToList();
             var state = states.FirstState();
 
-            state.Context = null;
+            state.Comment = default;
             state.StateActions.Add(new StateAction { Order = 2, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
             state.StateActions.Add(new StateAction { Order = 1, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
         }
 
         private Task StateAction(State state, StateActionType stateActionType, int order)
         {
-            if (state.Context == null)
+            if (state.Comment == null)
             {
-                state.Context = $"{order} {stateActionType} {state.StateCode}";
+                state.Comment = $"{order} {stateActionType} {state.StateCode}";
             }
             else
             {
-                state.Context += $"; {order} {stateActionType} {state.StateCode}";
+                state.Comment += $"; {order} {stateActionType} {state.StateCode}";
             }
 
             return Task.CompletedTask;
