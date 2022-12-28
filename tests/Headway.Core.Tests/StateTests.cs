f5359982d4a16d8bd29817bb951ae5ffe09859bc
grantcolley <grantcolley@yahoo.com>
2022-12-28 19:27:46 +0000

    Update StateTests.cs

--- tests/Headway.Core.Tests/StateTests.cs
+++ tests/Headway.Core.Tests/StateTests.cs
@@ -107,7 +107,7 @@ namespace Headway.Core.Tests
             // Assert
             Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
             Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
-            Assert.AreEqual($"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Context);
+            Assert.AreEqual($"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Comment);
         }
 
         [TestMethod]
@@ -151,7 +151,7 @@ namespace Headway.Core.Tests
             // Assert
             Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
             Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
-            Assert.AreEqual($"3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Context);
+            Assert.AreEqual($"3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}", flow.ActiveState.Comment);
         }
 
         [TestMethod]
@@ -166,7 +166,7 @@ namespace Headway.Core.Tests
 
             flow.ActiveState.ConfigureStateClass = "Headway.Core.Tests.Helpers.StateHelper, Headway.Core.Tests";
 
-            var activeStateContext = $"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}; 3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}";
+            var activeStateComment = $"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}; 3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}";
 
             // Act
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
@@ -174,7 +174,7 @@ namespace Headway.Core.Tests
             // Assert
             Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
             Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
-            Assert.AreEqual(activeStateContext, flow.ActiveState.Context);
+            Assert.AreEqual(activeStateComment, flow.ActiveState.Comment);
         }
 
         [TestMethod]
@@ -189,7 +189,7 @@ namespace Headway.Core.Tests
 
             flow.Bootstrap();
 
-            var activeStateContext = $"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}; 3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}";
+            var activeStateComment = $"1 Initialize {flow.ActiveState.StateCode}; 2 Initialize {flow.ActiveState.StateCode}; 3 Initialize {flow.ActiveState.StateCode}; 4 Initialize {flow.ActiveState.StateCode}";
 
             // Act
             await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
@@ -197,7 +197,7 @@ namespace Headway.Core.Tests
             // Assert
             Assert.AreEqual(flow.States.FirstState(), flow.ActiveState);
             Assert.AreEqual(StateStatus.InProgress, flow.ActiveState.StateStatus);
-            Assert.AreEqual(activeStateContext, flow.ActiveState.Context);
+            Assert.AreEqual(activeStateComment, flow.ActiveState.Comment);
         }
 
         [TestMethod]
@@ -392,7 +392,7 @@ namespace Headway.Core.Tests
             await flow.ActiveState.CompleteAsync(flow.States[1].StateCode).ConfigureAwait(false);
 
             // Assert
-            Assert.AreEqual($"1 Complete {flow.States[0].StateCode}; 2 Complete {flow.States[0].StateCode}", flow.States[0].Context);
+            Assert.AreEqual($"1 Complete {flow.States[0].StateCode}; 2 Complete {flow.States[0].StateCode}", flow.States[0].Comment);
             Assert.AreEqual(StateStatus.Completed, flow.States[0].StateStatus);
             Assert.AreEqual(StateStatus.InProgress, flow.States[1].StateStatus);
             Assert.AreEqual(flow.States[1], flow.ActiveState);
@@ -449,8 +449,134 @@ namespace Headway.Core.Tests
             Assert.AreEqual(StateStatus.NotStarted, flow.States[2].StateStatus);
             Assert.AreEqual(StateStatus.InProgress, flow.States[3].StateStatus);
             Assert.AreEqual(flow.States[3], flow.ActiveState);
-            Assert.AreEqual($"Route {flow.States[1].StateCode} to {flow.ActiveState.StateCode}", flow.States[1].Context);
-            Assert.AreEqual($"Routed from {flow.States[1].StateCode}", flow.ActiveState.Context);
+            Assert.AreEqual($"Route {flow.States[1].StateCode} to {flow.ActiveState.StateCode}", flow.States[1].Comment);
+            Assert.AreEqual($"Routed from {flow.States[1].StateCode}", flow.ActiveState.Comment);
+        }
+
+        [TestMethod]
+        public async Task History_Record_Transitions()
+        {
+            // Arrange
+            var flow = RemediatRFlow.CreateRemediatRFlow();
+
+            flow.Bootstrap();
+
+            // Act
+            await flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
+            await flow.ActiveState.CompleteAsync().ConfigureAwait(false);
+            await flow.ActiveState.CompleteAsync().ConfigureAwait(false);
+            await flow.ActiveState.CompleteAsync().ConfigureAwait(false);
+
+            //Assert
+            Assert.AreEqual(9, flow.History.Count);
+
+            Assert.AreEqual("Initialize", flow.History[0].Event);
+            Assert.AreEqual(flow.States[0].StateCode, flow.History[0].StateCode);
+            Assert.AreEqual(StateStatus.InProgress, flow.History[0].StateStatus);
+
+            Assert.AreEqual(StateStatus.Completed, flow.States[0].StateStatus);
+            Assert.AreEqual("Completed", flow.History[1].Event);
+            Assert.AreEqual(flow.States[0].StateCode, flow.History[1].StateCode);
+            Assert.AreEqual(flow.States[0].StateStatus, flow.History[1].StateStatus);
+
+            Assert.AreEqual("Initialize", flow.History[2].Event);
+            Assert.AreEqual(flow.States[1].StateCode, flow.History[2].StateCode);
+            Assert.AreEqual(StateStatus.InProgress, flow.History[2].StateStatus);
+
+            Assert.AreEqual("Initialize", flow.History[3].Event);
+            Assert.AreEqual(flow.States[2].StateCode, flow.History[3].StateCode);
+            Assert.AreEqual(StateStatus.InProgress, flow.History[3].StateStatus);
+
+            Assert.AreEqual(StateStatus.Completed, flow.States[2].StateStatus);
+            Assert.AreEqual("Completed", flow.History[4].Event);
+            Assert.AreEqual(flow.States[2].StateCode, flow.History[4].StateCode);
+            Assert.AreEqual(flow.States[2].StateStatus, flow.History[4].StateStatus);
+
+            Assert.AreEqual("Initialize", flow.History[5].Event);
+            Assert.AreEqual(flow.States[3].StateCode, flow.History[5].StateCode);
+            Assert.AreEqual(StateStatus.InProgress, flow.History[5].StateStatus);
+
+            Assert.AreEqual(StateStatus.Completed, flow.States[3].StateStatus);
+            Assert.AreEqual("Completed", flow.History[6].Event);
+            Assert.AreEqual(flow.States[3].StateCode, flow.History[6].StateCode);
+            Assert.AreEqual(flow.States[3].StateStatus, flow.History[6].StateStatus);
+
+            Assert.AreEqual(StateStatus.Completed, flow.States[1].StateStatus);
+            Assert.AreEqual("Completed", flow.History[7].Event);
+            Assert.AreEqual(flow.States[1].StateCode, flow.History[7].StateCode);
+            Assert.AreEqual(flow.States[1].StateStatus, flow.History[7].StateStatus);
+
+            Assert.AreEqual(StateStatus.InProgress, flow.States[4].StateStatus);
+            Assert.AreEqual("Initialize", flow.History[8].Event);
+            Assert.AreEqual(flow.States[4].StateCode, flow.History[8].StateCode);
+            Assert.AreEqual(flow.States[4].StateStatus, flow.History[8].StateStatus);
+
+            Assert.AreEqual(flow.ActiveState, flow.States[4]);
+        }
+
+        [TestMethod]
+        public async Task Reset_No_Transition()
+        {
+            // Arrange
+            var flow = RemediatRFlow.CreateRemediatRFlow();
+
+            flow.Bootstrap();
+
+            flow.ActiveState.StateStatus = StateStatus.InProgress;
+            flow.ActiveState.Comment = "Hello World!";
+            flow.ActiveState.Owner = "state owner";
+
+            // Act
+            await flow.ActiveState.ResetAsync().ConfigureAwait(false);
+
+            //Assert
+            Assert.AreEqual(StateStatus.NotStarted, flow.ActiveState.StateStatus);
+            Assert.IsNull(flow.ActiveState.Comment);
+            Assert.IsNull(flow.ActiveState.Owner);
+        }
+
+        [TestMethod]
+        public async Task Reset_No_Transition_State_Has_Actions_Configure_At_State_Reset()
+        {
+            // Arrange
+            var flow = RemediatRFlow.CreateRemediatRFlow();
+
+            flow.Bootstrap();
+
+            flow.ActiveState.ConfigureStateClass = "Headway.Core.Tests.Helpers.StateHelper, Headway.Core.Tests";
+
+            // Act
+            await flow.ActiveState.ResetAsync().ConfigureAwait(false);
+
+            // Assert
+            Assert.AreEqual(StateStatus.NotStarted, flow.ActiveState.StateStatus);
+            Assert.IsNull(flow.ActiveState.Owner);
+            Assert.AreEqual($"5 Reset {flow.ActiveState.StateCode}; 6 Reset {flow.ActiveState.StateCode}", flow.ActiveState.Comment);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(StateException))]
+        public async Task Reset_To_Invalid_StateCode()
+        {
+            // Arrange
+            var flow = RemediatRFlow.CreateRemediatRFlow();
+
+            flow.Bootstrap();
+
+            var resetStateCode = "ABC";
+
+            try
+            {
+                // Act
+                await flow.ActiveState.ResetAsync(resetStateCode).ConfigureAwait(false);
+            }
+            catch (StateException ex)
+            {
+                // Assert
+                Assert.AreEqual($"Can't reset {flow.ActiveState.StateCode} because it doesn't support resetting back to {resetStateCode}.", ex.Message);
+
+                throw;
+            }
         }
 
         [TestMethod]
