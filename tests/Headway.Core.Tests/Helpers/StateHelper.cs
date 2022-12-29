d293f8705a3074e09e5923781b1fbde6660955b9
grantcolley <grantcolley@yahoo.com>
2022-12-29 08:00:38 +0000

    Update StateHelper.cs

--- tests/Headway.Core.Tests/Helpers/StateHelper.cs
+++ tests/Headway.Core.Tests/Helpers/StateHelper.cs
@@ -9,8 +9,8 @@ namespace Headway.Core.Tests.Helpers
         public void Configure(State state)
         {
             state.Comment = default;
-            state.StateActions.Add(new StateAction { Order = 2, StateActionType = StateActionType.Complete, ActionAsync = StateAction });
-            state.StateActions.Add(new StateAction { Order = 1, StateActionType = StateActionType.Complete, ActionAsync = StateAction });
+            state.StateActions.Add(new StateAction { Order = 2, StateActionType = StateActionType.Completed, ActionAsync = StateAction });
+            state.StateActions.Add(new StateAction { Order = 1, StateActionType = StateActionType.Completed, ActionAsync = StateAction });
             state.StateActions.Add(new StateAction { Order = 4, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
             state.StateActions.Add(new StateAction { Order = 3, StateActionType = StateActionType.Initialize, ActionAsync = StateAction });
             state.StateActions.Add(new StateAction { Order = 6, StateActionType = StateActionType.Reset, ActionAsync = StateAction });
