7edbfdc600ae8648b5d9a2a0615ba0c2405c37b0
grantcolley <grantcolley@yahoo.com>
2022-12-31 18:59:18 +0000

    Update Flow.cs

--- src/Headway.Core/Model/Flow.cs
+++ src/Headway.Core/Model/Flow.cs
@@ -1,5 +1,4 @@
 ﻿using Headway.Core.Attributes;
-using Headway.Core.Enums;
 using Headway.Core.Extensions;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -19,52 +18,107 @@ namespace Headway.Core.Model
             History = new List<FlowHistory>();
         }
 
+        /// <summary>
+        /// The flow identity number.
+        /// </summary>
         public int FlowId { get; set; }
+
+        /// <summary>
+        /// Config Id for the <see cref="Config"/> associated with the flow.
+        /// </summary>
         public int ConfigId { get; set; }
-        public FlowStatus FlowStatus { get; set; }
+
+        /// <summary>
+        /// A flag indicating wether to configure all the 
+        /// flow states during the flow bootstrap routine.
+        /// </summary>
         public bool ConfigureStatesDuringBootstrap { get; set; }
+
+        /// <summary>
+        /// The states associated with the flow.
+        /// </summary>
         public List<State> States { get; set; }
+
+        /// <summary>
+        /// A record of states transitioning through the flow.  
+        /// </summary>
         public List<FlowHistory> History { get; set; }
 
+        /// <summary>
+        /// The name of the flow.
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The class dynamically loaded during the flow bootstrap 
+        /// routine for configuring state actions to be invoked at 
+        /// runtime. This class can contain actions to be applied 
+        /// to all states or individual ones. 
+        /// The pattern expected is '{type full name}, {assembly name}'.
+        /// </summary>
         [Required]
         [StringLength(150)]
-        public string Model { get; set; }
+        public string ActionConfigurationClass { get; set; }
 
-        [Required]
-        [StringLength(150)]
-        public string ConfigureFlowClass { get; set; }
-
+        /// <summary>
+        /// Comma seperated list of permissions to give 
+        /// read / write access to all states in the flow.
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string Permissions { get; set; }
 
+        /// <summary>
+        /// The instance of the config associated with the flow.
+        /// </summary>
         [NotMapped]
         public Config Config { get; set; }
 
+        /// <summary>
+        /// A flag indicating whether the flow bootstrap 
+        /// routine has been completed.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
         public bool Bootstrapped { get; set; }
 
+        /// <summary>
+        /// A flag indicating whether actions have been 
+        /// configured during the flow bootstrap routine.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
-        public bool Configured { get; set; }
+        public bool ActionsConfigured { get; set; }
 
+        /// <summary>
+        /// The active state in the flow. This is set 
+        /// during the bootstrap routine and tracked 
+        /// during the lifetime of the flow instance.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
         public State ActiveState { get; set; }
 
+        /// <summary>
+        /// A dictionary of states where the key is the StateCode.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
         public Dictionary<string, State> StateDictionary { get; set; }
 
+        /// <summary>
+        /// The context associated with the flow.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
         public object Context { get; set; }
 
+        /// <summary>
+        /// The root state is the state with the minimum 
+        /// state position value.
+        /// </summary>
         [NotMapped]
         [JsonIgnore]
         public State RootState
