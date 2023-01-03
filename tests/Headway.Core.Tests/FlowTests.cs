eb6929ef2fd5bae7f2894119f1b549276105cb9a
grantcolley <grantcolley@yahoo.com>
2023-01-03 20:05:01 +0000

    Update FlowTests.cs

--- tests/Headway.Core.Tests/FlowTests.cs
+++ tests/Headway.Core.Tests/FlowTests.cs
@@ -1,4 +1,5 @@
-﻿using Headway.Core.Exceptions;
+﻿using Headway.Core.Enums;
+using Headway.Core.Exceptions;
 using Headway.Core.Extensions;
 using Headway.Core.Tests.Helpers;
 using Headway.SeedData.RemediatR;
@@ -92,6 +93,116 @@ namespace Headway.Core.Tests
             Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_REVIEW")).Regressions.Contains(flow.States.First(s => s.StateCode.Equals("REDRESS_CREATE"))));
             Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_REVIEW")).Regressions.Contains(flow.States.First(s => s.StateCode.Equals("REFUND_ASSESSMENT"))));
             Assert.IsTrue(flow.States.First(s => s.StateCode.Equals("REFUND_REVIEW")).Transitions.Contains(flow.States.First(s => s.StateCode.Equals("REDRESS_REVIEW"))));
+        }
+
+        [TestMethod]
+        public void Flow_NotStarted()
+        {
+            // Arrange
+            var flow = FlowHelper.CreateFlow(2);
+
+            // Act
+            flow.Bootstrap();
+
+            //Assert
+            Assert.AreEqual(FlowStatus.NotStarted, flow.FlowStatus);
+        }
+
+        [TestMethod]
+        public async Task Flow_InProgress()
+        {
+            // Arrange
+            var flow = FlowHelper.CreateFlow(2);
+            
+            flow.Bootstrap();
+
+            // Act
+            await flow.ActiveState.InitialiseAsync();
+
+            //Assert
+            Assert.AreEqual(FlowStatus.InProgress, flow.FlowStatus);
+        }
+
+        [TestMethod]
+        public async Task Flow_Completed()
+        {
+            // Arrange
+            var flow = FlowHelper.CreateFlow(2);
+
+            flow.States[0].TransitionStateCodes = $"{flow.States[1].StateCode}";
+
+            flow.Bootstrap();
+
+            await flow.ActiveState.InitialiseAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            // Act
+            await flow.ActiveState.CompleteAsync();
+
+            //Assert
+            Assert.AreEqual(FlowStatus.Completed, flow.FlowStatus);
+            Assert.AreEqual(flow.FinalState, flow.ActiveState);
+        }
+
+        [TestMethod]
+        public async Task Flow_Completed_Regress_To_Prior_State_Flow_InProgress()
+        {
+            // Arrange
+            var flow = FlowHelper.CreateFlow(3);
+
+            flow.States[0].TransitionStateCodes = $"{flow.States[1].StateCode}";
+            flow.States[1].TransitionStateCodes = $"{flow.States[2].StateCode}";
+            flow.States[2].RegressionStateCodes = $"{flow.States[1].StateCode}";
+
+            flow.Bootstrap();
+
+            await flow.ActiveState.InitialiseAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            Assert.AreEqual(FlowStatus.Completed, flow.FlowStatus);
+
+            // Act
+            await flow.FinalState.ResetAsync(flow.States[1].StateCode); 
+
+            //Assert
+            Assert.AreEqual(FlowStatus.InProgress, flow.FlowStatus);
+            Assert.AreEqual(flow.ActiveState, flow.States[1]);
+        }
+
+        [TestMethod]
+        public async Task Flow_Completed_Regress_To_Root_State_Flow_NotStarted()
+        {
+            // Arrange
+            var flow = FlowHelper.CreateFlow(3);
+
+            flow.States[0].TransitionStateCodes = $"{flow.States[1].StateCode}";
+            flow.States[1].TransitionStateCodes = $"{flow.States[2].StateCode}";
+            flow.States[2].RegressionStateCodes = $"{flow.States[0].StateCode}";
+
+            flow.Bootstrap();
+
+            await flow.ActiveState.InitialiseAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            await flow.ActiveState.CompleteAsync();
+
+            Assert.AreEqual(FlowStatus.Completed, flow.FlowStatus);
+
+            // Act
+            await flow.FinalState.ResetAsync(flow.States[0].StateCode);
+
+            //Assert
+            Assert.AreEqual(FlowStatus.NotStarted, flow.FlowStatus);
+            Assert.AreEqual(flow.RootState, flow.ActiveState);
         }
     }
 }
