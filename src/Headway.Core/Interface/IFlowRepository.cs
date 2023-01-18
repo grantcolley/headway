588c46ebe498d8c9662aa3862911bd7b2c47ba15
grantcolley <grantcolley@yahoo.com>
2023-01-18 18:54:49 +0000

    Create IFlowRepository.cs

---
+++ src/Headway.Core/Interface/IFlowRepository.cs
@@ -0,0 +1,15 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IFlowRepository : IRepository
+    {
+        Task<IEnumerable<Flow>> GetFlowsAsync();
+        Task<Flow> GetFlowAsync(int id);
+        Task<Flow> AddFlowAsync(Flow flow);
+        Task<Flow> UpdateFlowAsync(Flow flow);
+        Task<int> DeleteFlowAsync(int id);
+    }
+}
