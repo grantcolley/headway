fbb4cbd9a3df38d97e489df94f1d52521a355fc9
grantcolley <grantcolley@yahoo.com>
2023-01-21 19:09:57 +0000

    Create RedressFlowConfiguration.cs

---
+++ src/Modules/RemediatR.Core/Flow/RedressFlowConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Interface;
+
+namespace RemediatR.Core.Flow
+{
+    [FlowConfiguration]
+    public class RedressFlowConfiguration : IConfigureFlow
+    {
+        public void Configure(Headway.Core.Model.Flow flow)
+        {
+            // configure flow level state actions here...
+        }
+    }
+}
