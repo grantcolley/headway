c21970d1aaa9e5f8f9e5a274f08df209b0301bb1
grantcolley <grantcolley@yahoo.com>
2023-01-21 19:10:01 +0000

    Create RedressStateConfiguration.cs

---
+++ src/Modules/RemediatR.Core/Flow/RedressStateConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Interface;
+using Headway.Core.Model;
+
+namespace RemediatR.Core.Flow
+{
+    [StateConfiguration]
+    public class RedressStateConfiguration : IConfigureState
+    {
+        public void Configure(State state)
+        {
+            // configure state level actions here...
+        }
+    }
+}
