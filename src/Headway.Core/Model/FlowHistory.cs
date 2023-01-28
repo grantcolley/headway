a93c2cc1a10483a33aca9a33310924b3b50bb144
grantcolley <grantcolley@yahoo.com>
2023-01-28 14:46:21 +0000

    Update FlowHistory.cs

--- src/Headway.Core/Model/FlowHistory.cs
+++ src/Headway.Core/Model/FlowHistory.cs
@@ -7,17 +7,26 @@ namespace Headway.Core.Model
     public class FlowHistory : ModelBase
     {
         public int FlowHistoryId { get; set; }
+
+        [Required]
         public StateStatus StateStatus { get; set; }
 
         [JsonIgnore]
         public Flow Flow { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        public string FlowCode { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string StateCode { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string Event { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string Owner { get; set; }
 
