b9efb592a8e7b005d3d474e6f23a5b10a73efe59
grantcolley <grantcolley@yahoo.com>
2023-01-28 14:47:24 +0000

    Create IFlowContext.cs

---
+++ src/Headway.Core/Interface/IFlowContext.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Model;
+using System.Collections.Generic;
+
+namespace Headway.Core.Interface
+{
+    public interface IFlowContext
+    {
+        Flow Flow { get; set; }
+        List<FlowHistory> FlowHistory { get; set; }
+    }
+}
