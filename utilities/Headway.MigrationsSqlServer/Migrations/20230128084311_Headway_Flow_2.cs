3c38c17b4a1597e6e498b483593931f221f12212
grantcolley <grantcolley@yahoo.com>
2023-01-28 14:47:09 +0000

    SqlServer migrations Flow_2

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230128084311_Headway_Flow_2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class HeadwayFlow2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlowCode",
+                table: "Flows",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlowCode",
+                table: "Flows");
+        }
+    }
+}
