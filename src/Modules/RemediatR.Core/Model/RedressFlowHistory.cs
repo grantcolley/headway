6c30eab97cbbcd8f6ed0bc89968b966a11c2ec91
grantcolley <grantcolley@yahoo.com>
2023-01-30 19:05:54 +0000

    Create RedressFlowHistory.cs

---
+++ src/Modules/RemediatR.Core/Model/RedressFlowHistory.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Model;
+
+namespace RemediatR.Core.Model
+{
+    public class RedressFlowHistory : FlowHistory
+    {
+        public int RedressFlowHistoryId { get; set; }
+        public int RedressId { get; set; }
+        public Redress Redress { get; set; }
+    }
+}
