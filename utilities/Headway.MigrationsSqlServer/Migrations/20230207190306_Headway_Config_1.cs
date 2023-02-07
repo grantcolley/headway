0202f4ee34986b79ee274dd45e758e7ca47823af
grantcolley <grantcolley@yahoo.com>
2023-02-07 19:04:43 +0000

    Updated migrations

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230207190306_Headway_Config_1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class HeadwayConfig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlowArgs",
+                table: "ConfigContainers",
+                type: "nvarchar(350)",
+                maxLength: 350,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlowArgs",
+                table: "ConfigContainers");
+        }
+    }
+}
