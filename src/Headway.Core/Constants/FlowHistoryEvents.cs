2aede96569b725aaa36a7f6cb1411c7b9a65e02c
grantcolley <grantcolley@yahoo.com>
2023-04-12 22:06:43 +0100

    Create FlowHistoryEvents.cs

---
+++ src/Headway.Core/Constants/FlowHistoryEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class FlowHistoryEvents
+    {
+        public const string INITIALIZE = "Initialize";
+        public const string START = "Start";
+        public const string COMPLETE = "Complete";
+        public const string RESET = "Reset";
+    }
+}
