57d770fbcf9b1f1e4f9c446ecb50d5457398eebb
grantcolley <grantcolley@yahoo.com>
2023-04-13 21:33:04 +0100

    Update StateExtensions.cs

--- src/Headway.Core/Extensions/StateExtensions.cs
+++ src/Headway.Core/Extensions/StateExtensions.cs
@@ -9,11 +9,32 @@ using System.Threading.Tasks;
 
 namespace Headway.Core.Extensions
 {
+    /// <summary>
+    /// <see cref="StateExtensions"/> contains extension methods for a <see cref="State"/>. 
+    /// </summary>
     public static class StateExtensions
     {
         private static readonly IDictionary<string, StateConfiguration> stateConfigurationCache = new Dictionary<string, StateConfiguration>();
         private static object stateConfigurationCacheLock = new object();
 
+        /// <summary>
+        /// Initialization routine for a <see cref="State"/>.
+        /// 
+        /// Sequence:   Execute any initialisation state actions
+        ///             Set the status to <see cref="StateStatus.Initialized"/>
+        ///             Record the initialization event in the <see cref="FlowHistory"/>
+        ///             Set the state as the <see cref="Flow.ActiveState"/>
+        ///             If the first state in the <see cref="Flow"/> set it to <see cref="FlowStatus.InProgress"/>
+        ///             If <see cref="State.IsOwnerRestricted"/> is false or an owner has been assigned execute the <see cref="StartAsync"/> routine. 
+        ///             
+        /// <see cref="StateException"/> thrown when:
+        ///             - the state is already <see cref="StateStatus.Initialized"/> or <see cref="StateStatus.InProgress"/>. 
+        ///             - if <see cref="State.IsOwnerRestricted"/> is true and an owner has not been assigned (only checked after executing start actions).
+        ///             - if the <see cref="StateType"/> is <see cref="StateType.Parent"/> but it doesn't have any associated sub states. 
+        /// </summary>
+        /// <param name="state">The state to initialize.</param>
+        /// <returns>A Task.</returns>
+        /// <exception cref="StateException"></exception>
         public static async Task InitialiseAsync(this State state)
         {
             if(state.StateStatus.Equals(StateStatus.Initialized)
@@ -53,6 +74,23 @@ namespace Headway.Core.Extensions
             }
         }
 
+        /// <summary>
+        /// Start routine for a <see cref="State"/>.
+        /// 
+        /// Sequence:   Execute any start state actions
+        ///             Set the status to <see cref="StateStatus.InProgress"/>
+        ///             Record the start event in the <see cref="FlowHistory"/>
+        ///             If the <see cref="State"/> is <see cref="StateType.Parent"/> then run the initialization routine for it's first sub state. 
+        ///             Else if the <see cref="State"/> is <see cref="StateType.Auto"/> then either auto complete or auto regress, as determined by the <see cref="State.AutoActionResult"/>. 
+        ///             
+        /// <see cref="StateException"/> thrown when:
+        ///             - the state is not the <see cref="Flow.ActiveState"/> of if the state status.
+        ///             - the state is not <see cref="StateStatus.Initialized"/>. 
+        ///             - if <see cref="State.IsOwnerRestricted"/> is true and an owner has not been assigned (only checked after executing start actions).
+        /// </summary>
+        /// <param name="state">The state to start.</param>
+        /// <returns>A Task.</returns>
+        /// <exception cref="StateException"></exception>
         public static async Task StartAsync(this State state)
         {
             if (!state.Flow.ActiveState.Equals(state))
@@ -67,13 +105,13 @@ namespace Headway.Core.Extensions
                 throw new StateException(state, $"Can't start {state.StateCode} because it's {state.StateStatus}.");
             }
 
-            if(state.IsOwnerRestricted
+            await state.ExecuteActionsAsync(StateActionType.Start).ConfigureAwait(false);
+
+            if (state.IsOwnerRestricted
                 && string.IsNullOrWhiteSpace(state.Owner))
             {
                 throw new StateException(state, $"Can't start owner restricted state {state.StateCode} without an owner.");
             }
-
-            await state.ExecuteActionsAsync(StateActionType.Start).ConfigureAwait(false);
 
             state.StateStatus = StateStatus.InProgress;
 
@@ -98,6 +136,27 @@ namespace Headway.Core.Extensions
             }
         }
 
+        /// <summary>
+        /// Completion routine for a <see cref="State"/>.
+        /// 
+        /// Sequence:   If <see cref="transitionStateCode"/> is not null or empty assign it to <see cref="State.TransitionStateCode"/>.
+        ///             Execute any complete state actions
+        ///             If <see cref="State.TransitionStateCode"/> is still unassigned, assign it to the first <see cref="State"/> in <see cref="State.Transitions"/>.
+        ///             Set the status to <see cref="StateStatus.Completed"/>
+        ///             Record the completed event in the <see cref="FlowHistory"/>
+        ///             If it has a state to transition to then run the initialization routine for the transition state.
+        ///             Else if the state has a <see cref="State.ParentState"/> then run the completion routine for the <see cref="State.ParentState"/>.
+        ///             If the <see cref="State"/> is the <see cref="Flow.FinalState"/> then set the <see cref="Flow.FlowStatus"/> to <see cref="FlowStatus.Completed"/>.
+        ///             
+        /// <see cref="StateException"/> thrown when:
+        ///             - the state is not the <see cref="Flow.ActiveState"/> of if the state status is not <see cref="StateStatus.InProgress"/>. 
+        ///             - if it tries to transition to a state that is not in its configured list of transition states.
+        ///             - if <see cref="State.IsOwnerRestricted"/> is true and an owner has not been assigned (only checked after executing complete actions). 
+        /// </summary>
+        /// <param name="state">The state to complete.</param>
+        /// <param name="transitionStateCode">Optional state code of the state to transition to once the state has been completed.</param>
+        /// <returns>A Task.</returns>
+        /// <exception cref="StateException"></exception>
         public static async Task CompleteAsync(this State state, string transitionStateCode = "")
         {
             if (state.StateStatus.Equals(StateStatus.Completed)
@@ -128,6 +187,12 @@ namespace Headway.Core.Extensions
             }
 
             await state.ExecuteActionsAsync(StateActionType.Complete).ConfigureAwait(false);
+
+            if (state.IsOwnerRestricted
+                && string.IsNullOrWhiteSpace(state.Owner))
+            {
+                throw new StateException(state, $"Can't complete owner restricted state {state.StateCode} without an owner.");
+            }
 
             State transitionState = null;
 
@@ -167,6 +232,28 @@ namespace Headway.Core.Extensions
             }
         }
 
+        /// <summary>
+        /// Reset routine for a <see cref="State"/>.
+        /// 
+        /// Sequence:   If <see cref="regressStateCode"/> is not null or empty assign it to <see cref="State.RegressionStateCode"/>.
+        ///             Execute any reset state actions.
+        ///             Set the <see cref="State.StateStatus"/> to <see cref="StateStatus.Uninitialized"/>.
+        ///             Record the reset event in the <see cref="FlowHistory"/>.
+        ///             Set the <see cref="State.Owner"/> and <see cref="State.Comment"/> to null.
+        ///             If <see cref="State.RegressionStateCode"/> is assigned then traverse backwards in the <see cref="Flow.History"/> 
+        ///                 calling the reset routine on all prior states in the <see cref="Flow.History"/>. When it reaches the target 
+        ///                 regression state, call the initialization routine on the target regression state. Finally, ensure the 
+        ///                 <see cref="Flow.FlowStatus"/> is set to <see cref="FlowStatus.InProgress"/>.
+        ///             
+        /// <see cref="StateException"/> thrown when:
+        ///             - the state is not the <see cref="regressStateCode"/> is null and the state status is <see cref="StateStatus.Uninitialized"/>. 
+        ///             - if it tries to regress to a state that is not in its configured list of regression states or in the <see cref="Flow.History"/>.
+        ///             - if the <see cref="State.Position"/> of the regression state is greater than that of the <see cref="State"/>. 
+        /// </summary>
+        /// <param name="state">The state to reset.</param>
+        /// <param name="regressStateCode">Optional state code of the state to regress to.</param>
+        /// <returns>A Task.</returns>
+        /// <exception cref="StateException"></exception>
         public static async Task ResetAsync(this State state, string regressStateCode = "")
         {
             if(string.IsNullOrWhiteSpace(regressStateCode)
