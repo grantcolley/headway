9768f20de4b855fdec3391990714b71afbf5f58f
grantcolley <grantcolley@yahoo.com>
2023-04-15 20:55:54 +0100

    Sql Server Migrations add

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230415194644_Headway_Flow_1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class HeadwayFlow1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOwnerRestricted",
+                table: "States",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOwnerRestricted",
+                table: "States");
+        }
+    }
+}
