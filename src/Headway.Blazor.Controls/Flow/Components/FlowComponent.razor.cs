b4b532d93c656b39becfd3ec88301e1f5fcc5893
grantcolley <grantcolley@yahoo.com>
2023-04-20 21:57:10 +0100

    Update FlowComponent.razor.cs

--- src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
+++ src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
@@ -1,8 +1,36 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Headway.Core.Notifications;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Headway.Blazor.Controls.Flow.Components
 {
     public class FlowComponentBase : ComponentBase
     {
+        protected bool OwnerAssigned { get; set; }
+        protected string Comment { get; set; }
+        protected string ActionText { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync().ConfigureAwait(false);
+
+            ActionText = FlowConstants.FLOW_ACTION_PROCEED;
+        }
+
+        protected virtual void OnActionChanged(IEnumerable<string> values)
+        {
+            if (values != null 
+                && values.Count().Equals(1))
+            {
+                ActionText = values.Single();
+            }
+
+            //await InvokeAsync(() =>
+            //{
+            //    StateHasChanged();
+            //});
+        }
     }
 }
