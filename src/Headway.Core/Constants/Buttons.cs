f9eb586a084cce34e8ff58fa6faf074cc29e0b3a
grantcolley <grantcolley@yahoo.com>
2023-04-21 17:42:37 +0100

    Create Buttons.cs

---
+++ src/Headway.Core/Constants/Buttons.cs
@@ -0,0 +1,17 @@
+﻿namespace Headway.Core.Constants
+{
+    public static class Buttons
+    {
+        public const string HEADER_BTN_IMAGE = "HeaderButtonImage";
+        public const string ROW_BTN_IMAGE = "RowButtonImage";
+
+        public const string HEADER_BTN_TOOLTIP = "HeaderButtonTooltip";
+        public const string ROW_BTN_TOOLTIP = "RowButtonTooltip";
+
+        public const string BTN_ICON_ADD = "Add";
+        public const string BTN_ICON_EDIT_NOTE = "EditNote";
+
+        public const string BTN_TOOLTIP_CREATE = "Create";
+        public const string BTN_TOOLTIP_EDIT = "Edit";
+    }
+}
