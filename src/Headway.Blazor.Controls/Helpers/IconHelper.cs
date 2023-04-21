973dc299d9a7413093f4d481b4d68aacd67a603b
grantcolley <grantcolley@yahoo.com>
2023-04-21 17:40:41 +0100

    Update IconHelper.cs

--- src/Headway.Blazor.Controls/Helpers/IconHelper.cs
+++ src/Headway.Blazor.Controls/Helpers/IconHelper.cs
@@ -13,5 +13,17 @@
 
             return fieldInfo.GetValue(null).ToString();
         }
+
+        public static string GetFilled(string name)
+        {
+            var fieldInfo = typeof(MudBlazor.Icons.Filled).GetField(name);
+
+            if (fieldInfo == null)
+            {
+                return string.Empty;
+            }
+
+            return fieldInfo.GetValue(null).ToString();
+        }
     }
 }
