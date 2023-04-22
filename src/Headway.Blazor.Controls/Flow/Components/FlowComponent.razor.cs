e097eaffebdb7a8e5d7cecd93cf44ce4e4ccfa0f
grantcolley <grantcolley@yahoo.com>
2023-04-22 21:46:46 +0100

    Update FlowComponent.razor.cs

--- src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
+++ src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
@@ -1,22 +1,51 @@
-﻿using Headway.Core.Notifications;
+﻿using Headway.Blazor.Controls.Flow.Documents;
+using Headway.Core.Dynamic;
+using Headway.Core.Interface;
+using Headway.Core.Notifications;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Headway.Blazor.Controls.Flow.Components
 {
-    public class FlowComponentBase : ComponentBase
+    public class FlowComponentBase<T> : ComponentBase where T : class, new()
     {
-        protected bool OwnerAssigned { get; set; }
+        private bool ownerAssigned;
+        private DynamicModel<T> dynamicModel;
+        private IFlowContext flowContext;
+
+        [Parameter]
+        public FlowTabDocumentBase<T> FlowTabDocument { get; set; }
+
         protected string Comment { get; set; }
         protected string ActionText { get; set; }
+        protected string OwnerAssignedTooltip { get; set; }
+        
+        protected bool OwnerAssigned 
+        {
+            get { return ownerAssigned; }
+            set
+            {
+                if(ownerAssigned != value)
+                {
+                    ownerAssigned = value;
+
+                    OwnerAssignedTooltip = ownerAssigned ? "Relinquish Ownership" : "Take Ownership";
+                }
+            }
+        }
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync().ConfigureAwait(false);
 
             ActionText = FlowConstants.FLOW_ACTION_PROCEED;
+
+            dynamicModel = FlowTabDocument.DynamicModel;
+            flowContext = dynamicModel.FlowContext;
+            OwnerAssigned = true;
         }
 
         protected virtual void OnActionChanged(IEnumerable<string> values)
