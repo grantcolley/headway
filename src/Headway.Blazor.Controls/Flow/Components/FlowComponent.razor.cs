7d7fc5efcc8fe7811e0dd4ce7fd5e52e3831409e
grantcolley <grantcolley@yahoo.com>
2023-04-24 20:25:04 +0100

    Update FlowComponent.razor.cs

--- src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
+++ src/Headway.Blazor.Controls/Flow/Components/FlowComponent.razor.cs
@@ -17,20 +17,23 @@ namespace Headway.Blazor.Controls.Flow.Components
         private DynamicModel<T> dynamicModel;
         protected Core.Model.Flow flow;
         protected Core.Model.State activeState;
-        
+
         [Parameter]
         public FlowTabDocumentBase<T> FlowTabDocument { get; set; }
 
         protected string Comment { get; set; }
         protected string ActionText { get; set; }
+        protected string ActionTarget { get; set; }
+        protected List<string> ActionTextItems { get; set; }
+        protected List<string> ActionTargetItems { get; set; }
         protected string OwnerAssignedTooltip { get; set; }
-        
-        protected bool OwnerAssigned 
+
+        protected bool OwnerAssigned
         {
             get { return ownerAssigned; }
             set
             {
-                if(ownerAssigned != value)
+                if (ownerAssigned != value)
                 {
                     ownerAssigned = value;
 
@@ -41,9 +44,10 @@ namespace Headway.Blazor.Controls.Flow.Components
 
         protected override async Task OnInitializedAsync()
         {
-            await base.OnInitializedAsync().ConfigureAwait(false);
+            ActionTextItems = new List<string>();
+            ActionTargetItems = new List<string>();
 
-            ActionText = FlowConstants.FLOW_ACTION_PROCEED;
+            await base.OnInitializedAsync().ConfigureAwait(false);
 
             dynamicModel = FlowTabDocument.DynamicModel;
             flow = dynamicModel.FlowContext.Flow;
@@ -54,21 +58,74 @@ namespace Headway.Blazor.Controls.Flow.Components
                 flow.Bootstrap(dynamicModel.FlowContext.GetFlowHistory(), true);
             }
 
+            if (activeState.Transitions.Any())
+            {
+                ActionTextItems.Add(FlowConstants.FLOW_ACTION_PROCEED);
+            }
+
+            if (activeState.Regressions.Any())
+            {
+                ActionTextItems.Add(FlowConstants.FLOW_ACTION_REGRESS);
+            }
+
+            ActionText = ActionTextItems.FirstOrDefault();
+
+            SetActiveTargetItems();
+
             OwnerAssigned = !string.IsNullOrWhiteSpace(activeState.Owner);
         }
 
         protected virtual void OnActionChanged(IEnumerable<string> values)
         {
-            if (values != null 
+            if (values != null
                 && values.Count().Equals(1))
             {
-                ActionText = values.Single();
+                var selected = values.Single();
+                if (ActionText != selected)
+                {
+                    ActionText = selected;
+                }
             }
 
             //await InvokeAsync(() =>
             //{
             //    StateHasChanged();
             //});
+        }
+
+        protected virtual void OnActionTargetChanged(IEnumerable<string> values)
+        {
+            if (values != null
+                && values.Count().Equals(1))
+            {
+                ActionTarget = values.Single();
+            }
+
+            //await InvokeAsync(() =>
+            //{
+            //    StateHasChanged();
+            //});
+        }
+
+        private void SetActiveTargetItems()
+        {
+            if (string.IsNullOrWhiteSpace(ActionText))
+            {
+                return;
+            }
+
+            ActionTargetItems.Clear();
+
+            if (ActionText == FlowConstants.FLOW_ACTION_PROCEED)
+            {
+                ActionTargetItems.AddRange(activeState.Transitions.Select(t => t.Name));
+            }
+            else if (ActionText == FlowConstants.FLOW_ACTION_REGRESS)
+            {
+                ActionTargetItems.AddRange(activeState.Regressions.Select(r => r.Name));
+            }
+
+            ActionTarget = ActionTargetItems.FirstOrDefault();
         }
     }
 }
