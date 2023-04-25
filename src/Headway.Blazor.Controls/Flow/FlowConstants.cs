adf4ad5878832637f10c84acd51286a26c1fbcfa
grantcolley <grantcolley@yahoo.com>
2023-04-25 20:31:52 +0100

    Update FlowConstants.cs

--- src/Headway.Blazor.Controls/Flow/FlowConstants.cs
+++ src/Headway.Blazor.Controls/Flow/FlowConstants.cs
@@ -4,7 +4,9 @@
     {
         public const string FLOW_LIST_CONTAINER_LABEL = "FlowList Container Label";
         public const string FLOW_LIST_CONTAINER_WIDTH = "FlowList Container Width";
-        public const string FLOW_ACTION = "Action";
+        public const string FLOW_STATE_ACTION = "State Action";
+        public const string FLOW_TARGET_STATE = "Target State";
+        public const string FLOW_EXECUTE = "Execute";
         public const string FLOW_ACTION_PROCEED = "Proceed";
         public const string FLOW_ACTION_REGRESS = "Regress";
     }
