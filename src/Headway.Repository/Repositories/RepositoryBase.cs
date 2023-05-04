bdd29be1825daec48bf47b6204aa5d91e51bd63e
grantcolley <grantcolley@yahoo.com>
2023-05-04 12:56:21 +0100

    Update RepositoryBase.cs

--- src/Headway.Repository/Repositories/RepositoryBase.cs
+++ src/Headway.Repository/Repositories/RepositoryBase.cs
@@ -1,4 +1,5 @@
 ﻿using Headway.Core.Interface;
+using Headway.Core.Model;
 using Headway.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -31,11 +32,26 @@ namespace Headway.Repository.Repositories
 
         public async Task<bool> IsAuthorisedAsync(string claim, string permission)
         {
-            return await applicationDbContext.Users.AnyAsync(
+            return await applicationDbContext.Users
+                .AsNoTracking()
+                .AnyAsync(
                 u => u.Email.Equals(claim)
                 && (u.Permissions.Any(p => p.Name.Equals(permission))
                 || u.Roles.SelectMany(r => r.Permissions).Any(p => p.Name.Equals(permission))))
                 .ConfigureAwait(false);
+        }
+
+        public async Task<User> GetAuthorisedUserAsync(string claim, string permission)
+        {
+            var user = await applicationDbContext.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(
+                u => u.Email.Equals(claim)
+                && (u.Permissions.Any(p => p.Name.Equals(permission))
+                || u.Roles.SelectMany(r => r.Permissions).Any(p => p.Name.Equals(permission))))
+                .ConfigureAwait(false);
+
+            return user;
         }
 
         protected virtual void Dispose(bool disposing)
