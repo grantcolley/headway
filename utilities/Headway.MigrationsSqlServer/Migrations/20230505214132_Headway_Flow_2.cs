558aee17d6f8af95229bfdf10ae8dfbab1a11711
grantcolley <grantcolley@yahoo.com>
2023-05-05 22:53:39 +0100

    SqlServer migrations

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230505214132_Headway_Flow_2.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class HeadwayFlow2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfigureStatesDuringBootstrap",
+                table: "Flows");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentUserUserId",
+                table: "RedressFlowContexts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RedressFlowContexts_CurrentUserUserId",
+                table: "RedressFlowContexts",
+                column: "CurrentUserUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedressFlowContexts_Users_CurrentUserUserId",
+                table: "RedressFlowContexts",
+                column: "CurrentUserUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedressFlowContexts_Users_CurrentUserUserId",
+                table: "RedressFlowContexts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RedressFlowContexts_CurrentUserUserId",
+                table: "RedressFlowContexts");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentUserUserId",
+                table: "RedressFlowContexts");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfigureStatesDuringBootstrap",
+                table: "Flows",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
