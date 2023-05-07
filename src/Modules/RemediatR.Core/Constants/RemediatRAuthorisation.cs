e25328f911b1a82680aca91104cbd95479135212
grantcolley <grantcolley@yahoo.com>
2023-05-07 15:55:44 +0100

    Update RemediatRAuthorisation.cs

--- src/Modules/RemediatR.Core/Constants/RemediatRAuthorisation.cs
+++ src/Modules/RemediatR.Core/Constants/RemediatRAuthorisation.cs
@@ -6,17 +6,10 @@
         public const string CUSTOMER_READ = "Customer Read";
         public const string CUSTOMER_WRITE = "Customer Write";
         public const string REDRESS_READ = "Redress Read";
-        public const string REDRESS_WRITE = "Redress Write";
-        public const string REDRESS_TRANSITION = "Redress Transition";
-        public const string COMMUNICATION_DISPATCH_TRANSITION = "Communication Dispatch Transition";
-        public const string AWAITING_REPONSE_TRANSITION = "Awaiting Response Transition";
-        public const string REDRESS_REVIEW_TRANSITION = "Redress Review Transition";
-        public const string REDRESS_COMPLETE = "Redress Complete";
-        public const string REFUND_READ = "Refund Read";
-        public const string REFUND_WRITE = "Refund Write";
-        public const string REFUND_CACULATION_COMPLETE = "Refund Calculation Complete";
-        public const string REFUND_VERIFICATION_COMPLETE = "Refund Varification Complete";
-        public const string REFUND_REVIEW_TRANSITION = "Refund Review Transition";
+        public const string REDRESS_CASE_OWNER_WRITE = "Redress Case Owner Write";
+        public const string REDRESS_REVIEWER_WRITE = "Redress Reviewer Write";
+        public const string REFUND_ASSESSOR_WRITE = "Refund Assessor Write";
+        public const string REFUND_REVIEWER_WRITE = "Refund Reviewer Write";
 
         // Roles
         public const string REDRESS_CASE_OWNER = "Redress Case Owner";
