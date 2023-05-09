28d9cbebfb0de7a4f146a720b1e680321de45e10
grantcolley <grantcolley@yahoo.com>
2023-05-09 17:18:05 +0100

    SQL Server Migrations
    SQL Server Migrations

--- utilities/Headway.MigrationsSqlServer/Migrations/20230509120240_Headway_Flow_4.Designer.cs
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230509120240_Headway_Flow_4.Designer.cs
@@ -4,6 +4,7 @@ using Headway.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Headway.MigrationsSqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230509120240_Headway_Flow_4")]
+    partial class HeadwayFlow4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230509120240_Headway_Flow_4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class HeadwayFlow4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Permissions",
+                table: "Flows",
+                newName: "Permission");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Permission",
+                table: "Flows",
+                newName: "Permissions");
+        }
+    }
+}
