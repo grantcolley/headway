bf26853d3f2f32c054d58023d88c077858d5c1cd
grantcolley <grantcolley@yahoo.com>
2023-05-10 19:42:46 +0100

    Create RemediatRFlowCodes.cs

---
+++ src/Modules/RemediatR.Core/Constants/RemediatRFlowCodes.cs
@@ -0,0 +1,47 @@
+﻿namespace RemediatR.Core.Constants
+{
+    public static class RemediatRFlowCodes
+    {
+        public const string REMEDIATR = "RemediatR";
+        public const string REMEDIATR_CODE = "REMEDIATR";
+
+        public const string REDRESS_CREATE = "Redress Create";
+        public const string REDRESS_CREATE_CODE = "REDRESS_CREATE";
+
+        public const string REFUND_ASSESSMENT = "Refund Assessment";
+        public const string REFUND_ASSESSMENT_CODE = "REFUND_ASSESSMENT";
+
+        public const string REFUND_CALCULATION = "Refund Calculation";
+        public const string REFUND_CALCULATION_CODE = "REFUND_CALCULATION";
+
+        public const string REFUND_VERIFICATION = "Refund Verification";
+        public const string REFUND_VERIFICATION_CODE = "REFUND_VERIFICATION";
+
+        public const string REFUND_REVIEW = "Refund Review";
+        public const string REFUND_REVIEW_CODE = "REFUND_REVIEW";
+
+        public const string REDRESS_REVIEW = "Redress Review";
+        public const string REDRESS_REVIEW_CODE = "REDRESS_REVIEW";
+
+        public const string REDRESS_VALIDATION = "Redress Validation";
+        public const string REDRESS_VALIDATION_CODE = "REDRESS_VALIDATION";
+
+        public const string COMMUNICATION_GENERATION = "Communication Generation";
+        public const string COMMUNICATION_GENERATION_CODE = "COMMUNICATION_GENERATION";
+
+        public const string COMMUNICATION_DISPATCH = "Communication Dispatch";
+        public const string COMMUNICATION_DISPATCH_CODE = "COMMUNICATION_DISPATCH";
+
+        public const string RESPONSE_REQUIRED = "Response Required";
+        public const string RESPONSE_REQUIRED_CODE = "RESPONSE_REQUIRED";
+
+        public const string AWAITING_RESPONSE = "Awaiting Response";
+        public const string AWAITING_RESPONSE_CODE = "AWAITING_RESPONSE";
+
+        public const string PAYMENT_GENERATION = "Payment Generation";
+        public const string PAYMENT_GENERATION_CODE = "PAYMENT_GENERATION";
+
+        public const string FINAL_REDRESS_REVIEW = "Final Redress Review";
+        public const string FINAL_REDRESS_REVIEW_CODE = "FINAL_REDRESS_REVIEW";
+    }
+}
