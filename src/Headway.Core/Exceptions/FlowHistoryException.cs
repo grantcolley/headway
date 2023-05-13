a4a1fe762c83b1ae52c9d78c3d231d47e436f8c7
grantcolley <grantcolley@yahoo.com>
2023-05-13 21:52:29 +0100

    Create FlowHistoryException.cs

---
+++ src/Headway.Core/Exceptions/FlowHistoryException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Headway.Core.Model;
+
+namespace Headway.Core.Exceptions
+{
+    public class FlowHistoryException : Exception
+    {
+        public FlowHistoryException(FlowHistory flowHistory, string message = "", Exception innerException = null)
+            : base(message, innerException)
+        {
+            FlowHistory = flowHistory;
+        }
+
+        public FlowHistory FlowHistory { get; private set; }
+    }
+}
