4d6167d477819feaeadaa170651d37676253473e
grantcolley <grantcolley@yahoo.com>
2023-05-15 20:14:32 +0100

    Update FlowHistoryExtensions.cs

--- src/Headway.Core/Extensions/FlowHistoryExtensions.cs
+++ src/Headway.Core/Extensions/FlowHistoryExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Headway.Core.Constants;
-using Headway.Core.Exceptions;
 using Headway.Core.Model;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Headway.Core.Extensions
 {
@@ -64,59 +62,6 @@ namespace Headway.Core.Extensions
                 Owner = state.Owner,
                 Comment = state.Comment
             });
-        }
-
-        /// <summary>
-        /// Replays the <see cref="Flow"/>'s history. 
-        /// </summary>
-        /// <param name="history">The <see cref="FlowHistory"/></param>
-        /// <returns>
-        /// A list of <see cref="State"/>'s. The last <see cref="State"/> is
-        /// the current active <see cref="State"/> in the <see cref="Flow"/>. 
-        /// All preceding <see cref="State"/>'s should be completed. 
-        /// <see cref="State"/>'s that have been reset are removed from the flow.
-        /// </returns>
-        /// <exception cref="FlowException"></exception>
-        public static List<FlowHistory> ReplayHistory(this List<FlowHistory> history)
-        {
-            Stack<FlowHistory> historyStack = new Stack<FlowHistory>();
-
-            foreach (var flowHistory in history)
-            {
-                switch (flowHistory.Event)
-                {
-                    case FlowHistoryEvents.INITIALIZE:
-                    case FlowHistoryEvents.START:
-                    case FlowHistoryEvents.COMPLETE:
-                        if (historyStack.Any())
-                        {
-                            var peekHistory= historyStack.Peek();
-
-                            if(peekHistory.StateCode.Equals(flowHistory.StateCode))
-                            {
-                                _ = historyStack.Pop();
-                            }
-                        }
-
-                        historyStack.Push(flowHistory);
-
-                        break;
-
-                    case FlowHistoryEvents.RESET:
-                        var peekResetHistory = historyStack.Peek();
-
-                        if (peekResetHistory.StateCode.Equals(flowHistory.StateCode))
-                        {
-                            throw new FlowHistoryException(flowHistory, $"Expecting {flowHistory.StateCode} but found {peekResetHistory.StateCode}.");
-                        }
-
-                        _ = historyStack.Pop();
-
-                        break;
-                };
-            }
-
-            return historyStack.ToList();
         }
     }
 }
