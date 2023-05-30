8affc234b3e74780c36183d3b33f075256f8c75b
grantcolley <grantcolley@yahoo.com>
2023-05-30 20:46:48 +0100

    Update SQLServer migrations

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230530193819_Headway_Create_2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Headway_Create_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedressFlowContexts_Users_CurrentUserUserId",
+                table: "RedressFlowContexts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RedressFlowContexts_CurrentUserUserId",
+                table: "RedressFlowContexts");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentUserUserId",
+                table: "RedressFlowContexts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentUserUserId",
+                table: "RedressFlowContexts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RedressFlowContexts_CurrentUserUserId",
+                table: "RedressFlowContexts",
+                column: "CurrentUserUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedressFlowContexts_Users_CurrentUserUserId",
+                table: "RedressFlowContexts",
+                column: "CurrentUserUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
