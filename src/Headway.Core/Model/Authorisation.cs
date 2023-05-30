e8a960bedb075ecb0747b2b5717542e87eddb5b6
grantcolley <grantcolley@yahoo.com>
2023-05-30 20:45:58 +0100

    Create Authorisation.cs

---
+++ src/Headway.Core/Model/Authorisation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Headway.Core.Model
+{
+    public class Authorisation
+    {
+        public Authorisation() 
+        {
+            Permissions = new List<string>();
+        }
+
+        public string User { get; set; }
+        public List<string> Permissions { get; set; }
+    }
+}
