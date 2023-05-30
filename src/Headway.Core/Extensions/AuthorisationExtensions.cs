4a8699ecd0329b565ec86efec0bf321d4a251981
grantcolley <grantcolley@yahoo.com>
2023-05-30 21:07:28 +0100

    Create AuthorisationExtensions.cs

---
+++ src/Headway.Core/Extensions/AuthorisationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Headway.Core.Model;
+using System.Linq;
+
+namespace Headway.Core.Extensions
+{
+    public static class AuthorisationExtensions
+    {
+        public static bool IsUserAuthorised(this Authorisation authorisation, string permission)
+        {
+            if(string.IsNullOrEmpty(permission))
+            {
+                return true;
+            }
+
+            return authorisation.Permissions.Any(p => p.Equals(permission));
+        }
+    }
+}
