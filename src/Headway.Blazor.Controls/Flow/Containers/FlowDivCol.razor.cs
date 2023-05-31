007f116513114c265ecafa01d019585cbc8d5a82
grantcolley <grantcolley@yahoo.com>
2023-05-31 18:04:24 +0100

    Create FlowDivCol.razor.cs

---
+++ src/Headway.Blazor.Controls/Flow/Containers/FlowDivCol.razor.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Dynamic;
+using Microsoft.AspNetCore.Components;
+
+namespace Headway.Blazor.Controls.Flow.Containers
+{
+    public abstract class FlowDivColBase : ComponentBase
+    {
+        [Parameter]
+        public DynamicField Field { get; set; }
+    }
+}
