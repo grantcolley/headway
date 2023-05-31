4a5d97c46d41b9d9e9e83848e87410dab0cd26a0
grantcolley <grantcolley@yahoo.com>
2023-05-31 18:04:19 +0100

    Create FlowDiv.razor.cs

---
+++ src/Headway.Blazor.Controls/Flow/Containers/FlowDiv.razor.cs
@@ -0,0 +1,28 @@
+﻿using Headway.Core.Attributes;
+using Headway.Core.Constants;
+using Headway.Core.Dynamic;
+using Headway.Core.Helpers;
+using Headway.Blazor.Controls.Base;
+using System;
+using System.Threading.Tasks;
+
+namespace Headway.Blazor.Controls.Flow.Containers
+{
+    [DynamicContainer]
+    public abstract class FlowDivBase : DynamicContainerBase
+    {
+        protected bool LayoutHorizontal { get; private set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            var layoutHorizontal = ComponentArgHelper.GetArgValue(Container.DynamicArgs, Args.LAYOUT_HORIZONTAL);
+
+            if(!string.IsNullOrWhiteSpace(layoutHorizontal))
+            {
+                LayoutHorizontal = Convert.ToBoolean(layoutHorizontal);
+            }
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
