b0270a0a5cd699dbc44336acd3ba440930fe3a29
grantcolley <grantcolley@yahoo.com>
2023-06-01 19:08:10 +0100

    Create DropdownMultiSelect component

---
+++ src/Headway.Blazor.Controls/Components/DropdownMultiselect.razor.cs
@@ -0,0 +1,83 @@
+﻿using Headway.Blazor.Controls.Base;
+using Headway.Core.Attributes;
+using Headway.Core.Interface;
+using Headway.Core.Notifications;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Headway.Blazor.Controls.Components
+{
+    [DynamicComponent]
+    public abstract class DropdownMultiSelectBase : DynamicComponentBase
+    {
+        [Inject]
+        public IStateNotification StateNotification { get; set; }
+
+        [Inject]
+        public IOptionsApiRequest OptionsApiRequest { get; set; }
+
+        protected IEnumerable<string> Items = new List<string>();
+        protected IEnumerable<string> SelectedItems = new List<string>();
+
+        public Expression<Func<string>> FieldExpression
+        {
+            get
+            {
+                return Expression.Lambda<Func<string>>(Field.MemberExpression);
+            }
+        }
+
+        public string PropertyValue
+        {
+            get
+            {
+                return Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Field.PropertyInfo.SetValue(Field.Model, null);
+                }
+                else
+                {
+                    Field.PropertyInfo.SetValue(Field.Model, value);
+                }
+            }
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            LinkFieldCheck();
+
+            var result = await OptionsApiRequest.GetOptionTextItemsAsync(ComponentArgs).ConfigureAwait(false);
+
+            Items = GetResponse(result);
+
+            var selectedItems = Field.PropertyInfo.GetValue(Field.Model)?.ToString();
+
+            if (!string.IsNullOrWhiteSpace(selectedItems))
+            {
+                SelectedItems = selectedItems.Split(", ");
+            }
+
+            await base.OnParametersSetAsync().ConfigureAwait(false);
+        }
+
+        protected async virtual void OnValueChanged(IEnumerable<string> values)
+        {
+            if (Field.HasLinkDependents)
+            {
+                StateNotification.NotifyStateHasChanged(Field.ContainerUniqueId);
+            }
+
+            await InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+        }
+    }
+}
