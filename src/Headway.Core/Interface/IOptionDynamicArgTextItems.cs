856566c2671f367633d46a1088a49aa82dd10e5b
grantcolley <grantcolley@yahoo.com>
2023-06-04 10:28:45 +0100

    Create IOptionDynamicArgTextItems.cs

---
+++ src/Headway.Core/Interface/IOptionDynamicArgTextItems.cs
@@ -0,0 +1,11 @@
+﻿using Headway.Core.Args;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headway.Core.Interface
+{
+    public interface IOptionDynamicArgTextItems
+    {
+        Task<IEnumerable<string>> GetOptionDynamicArgTextItemsAsync(IEnumerable<DynamicArg> dynamicArgs);
+    }
+}
