9201a949e941b887c26a802a71aed9ff793c9360
grantcolley <grantcolley@yahoo.com>
2023-06-04 10:28:17 +0100

    Create HeadwayArgsException.cs

---
+++ src/Headway.Core/Exceptions/HeadwayArgsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Headway.Core.Exceptions
+{
+    public class HeadwayArgsException : Exception
+    {
+        public HeadwayArgsException(string message = "", Exception innerException = null)
+            : base(message, innerException)
+        {
+        }
+    }
+}
