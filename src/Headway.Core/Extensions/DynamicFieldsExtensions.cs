961484503511e176c3b4e156f06469f4d239a62b
grantcolley <grantcolley@yahoo.com>
2023-06-06 19:30:59 +0100

    Create DynamicFieldsExtensions.cs

---
+++ src/Headway.Core/Extensions/DynamicFieldsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Headway.Core.Args;
+using Headway.Core.Constants;
+using Headway.Core.Dynamic;
+using Headway.Core.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Headway.Core.Extensions
+{
+    public static class DynamicFieldsExtensions
+    {
+        public static void PropagateDynamicArgs(this List<DynamicField> dynamicFields, List<DynamicArg> sourceArgs)
+        {
+            foreach (var dynamicField in dynamicFields)
+            {
+                var componentArg = dynamicField.Parameters.FirstOrDefault(a => a.Key.Equals(Parameters.COMPONENT_ARGS)).Value;
+                if (componentArg is List<DynamicArg> dynamicArg)
+                {
+                    var linkedSourceArg = dynamicArg.FirstDynamicArgOrDefault(Constants.Args.LINK_SOURCE);
+                    if (linkedSourceArg != null
+                        && linkedSourceArg.Value != null)
+                    {
+                        DynamicLinkHelper.LinkFields(dynamicField, sourceArgs, linkedSourceArg.Value.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
