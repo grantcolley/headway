965b68781aaa398b69e2ee5731b6081417ec8e10
grantcolley <grantcolley@yahoo.com>
2023-06-06 19:36:56 +0100

    Create DynamicContainerExtensions.cs

---
+++ src/Headway.Core/Extensions/DynamicContainerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Headway.Core.Dynamic;
+using Headway.Core.Helpers;
+using System.Collections.Generic;
+
+namespace Headway.Core.Extensions
+{
+    public static class DynamicContainerExtensions
+    {
+        public static void AddDynamicArgs(this DynamicContainer dynamicContainer, List<DynamicField> dynamicFields)
+        {
+            var dynamicArgs = ComponentArgHelper.ExtractDynamicArgs(dynamicContainer.ComponentArgs, dynamicFields);
+            dynamicContainer.DynamicArgs.AddRange(dynamicArgs);
+        }
+    }
+}
