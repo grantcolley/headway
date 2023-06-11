7a8465dbd0e9b9659eee044de19a402b21787be4
grantcolley <grantcolley@yahoo.com>
2023-06-11 19:50:18 +0100

    Create AuthorisationUserHelper.cs

---
+++ tests/Headway.Core.Tests/Helpers/AuthorisationUserHelper.cs
@@ -0,0 +1,16 @@
+﻿using Headway.Core.Model;
+
+namespace Headway.Core.Tests.Helpers
+{
+    public class AuthorisationHelper
+    {
+        public static Authorisation CreateAuthorisation(List<string> permissions)
+        {
+            return new Authorisation
+            {
+                User = Environment.UserName,
+                Permissions = permissions
+            };
+        }
+    }
+}
