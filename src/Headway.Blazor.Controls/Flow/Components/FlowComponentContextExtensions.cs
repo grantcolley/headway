c1a309c18cf0c5649bc04d329350bbcccdf906bb
grantcolley <grantcolley@yahoo.com>
2023-06-12 17:45:54 +0100

    Create FlowComponentContextExtensions.cs

---
+++ src/Headway.Blazor.Controls/Flow/Components/FlowComponentContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Headway.Core.Interface;
+
+namespace Headway.Blazor.Controls.Flow.Components
+{
+    public static class FlowComponentContextExtensions
+    {
+        public static FlowComponentContext GetFlowComponentContext(this IFlowContext flowContext)
+        {
+            return new FlowComponentContext(flowContext);
+        }
+    }
+}
