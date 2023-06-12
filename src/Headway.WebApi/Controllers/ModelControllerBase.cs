d040c7797feffe31852969fa485e9e3923afc6b3
grantcolley <grantcolley@yahoo.com>
2023-06-12 19:45:43 +0100

    Create ModelControllerBase.cs

---
+++ src/Headway.WebApi/Controllers/ModelControllerBase.cs
@@ -0,0 +1,32 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    public abstract class ModelControllerBase<T,K> : ApiControllerBase<K>
+    {
+        public ModelControllerBase(
+            IRepository authorisationRepository,
+            ILogger<K> logger)
+            : base(authorisationRepository, logger)
+        {
+        }
+
+        [HttpGet]
+        public abstract Task<IActionResult> Get();
+
+        [HttpGet]
+        public abstract Task<IActionResult> Get(int id);
+
+        [HttpPost]
+        public abstract Task<IActionResult> Post([FromBody] T model);
+
+        [HttpPut]
+        public abstract Task<IActionResult> Put([FromBody] T model);
+
+        [HttpDelete]
+        public abstract Task<IActionResult> Delete(int id);
+    }
+}
