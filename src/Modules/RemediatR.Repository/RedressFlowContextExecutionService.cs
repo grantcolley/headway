0623faac1896a71458abbb4b504d89ac5e1791ee
grantcolley <grantcolley@yahoo.com>
2023-06-15 06:53:15 +0100

    Create RedressFlowContextExecutionService.cs

---
+++ src/Modules/RemediatR.Repository/RedressFlowContextExecutionService.cs
@@ -0,0 +1,28 @@
+﻿using RemediatR.Core.Interface;
+using RemediatR.Core.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace RemediatR.Repository
+{
+    public class RedressFlowContextExecutionService : IRedressFlowContextExecutionService
+    {
+        private readonly IRemediatRRepository<RedressFlowContext> remediatRRepository;
+
+        public RedressFlowContextExecutionService(IRemediatRRepository<RedressFlowContext> remediatRRepository) 
+        {
+            this.remediatRRepository = remediatRRepository;
+        }
+
+        public async Task<RedressFlowContext> Execute(RedressFlowContext flowContext)
+        {
+            //var currentFlowContext = await remediatRRepository
+            //     .GetFlowContextAsync(flowContext.RedressFlowContextId)
+            //     .ConfigureAwait(false);
+
+            //var executedFlowContext = flowContext.Execute(currentFlowContext);
+
+            throw new NotImplementedException();
+        }
+    }
+}
