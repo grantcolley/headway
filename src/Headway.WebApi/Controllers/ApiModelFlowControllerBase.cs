635bd11804276d889f449573e765de79d2f91cab
grantcolley <grantcolley@yahoo.com>
2023-06-15 06:53:58 +0100

    Create ApiModelFlowControllerBase.cs

---
+++ src/Headway.WebApi/Controllers/ApiModelFlowControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using Headway.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Headway.WebApi.Controllers
+{
+    public abstract class ApiModelFlowControllerBase<TModel, TController, TFlowContext> : ApiModelControllerBase<TModel, TController>
+    {
+        protected readonly IRepositoryFlowable<TFlowContext> repositoryFlowable;
+
+        public ApiModelFlowControllerBase(
+            IRepositoryFlowable<TFlowContext> repositoryFlowable,
+            ILogger<TController> logger)
+            : base(repositoryFlowable, logger)
+        {
+            this.repositoryFlowable = repositoryFlowable;
+        }
+
+        [HttpPut("[action]")]
+        public abstract Task<IActionResult> FlowExecution([FromBody] TModel model);
+
+        protected bool IsFlowUserAuthenticatedAsync(IFlowContext flowContext)
+        {
+            var claim = GetUserClaim();
+
+            return flowContext.Authorisation.User.Equals(claim);
+        }
+    }
+}
