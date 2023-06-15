4a6a3ab4767929d12267e0604971debe17e3d8da
grantcolley <grantcolley@yahoo.com>
2023-06-15 06:54:26 +0100

    Create FlowContextExtensions.cs

---
+++ src/Headway.Core/Extensions/FlowContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Headway.Core.Interface;
+using System;
+
+namespace Headway.Core.Extensions
+{
+    public static class FlowContextExtensions
+    {
+        public static IFlowContext Execute(this IFlowContext flowContext, IFlowContext target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
