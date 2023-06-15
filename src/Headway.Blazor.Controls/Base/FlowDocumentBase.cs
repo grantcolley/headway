493c85fb196dcd209d9acdfe479ef5e003722d8e
grantcolley <grantcolley@yahoo.com>
2023-06-15 07:35:04 +0100

    Update FlowDocumentBase.cs

--- src/Headway.Blazor.Controls/Base/FlowDocumentBase.cs
+++ src/Headway.Blazor.Controls/Base/FlowDocumentBase.cs
@@ -1,6 +1,4 @@
-﻿using Headway.Core.Dynamic;
-using Headway.Core.Extensions;
-using Headway.Core.Interface;
+﻿using Headway.Core.Extensions;
 using System.Threading.Tasks;
 
 namespace Headway.Blazor.Controls.Base
@@ -19,31 +17,16 @@ namespace Headway.Blazor.Controls.Base
             }
         }
 
-        protected virtual async Task FlowExecuteAsync()
+        public virtual async Task FlowExecutionAsync()
         {
             isSaveInProgress = true;
 
             if (CurrentEditContext != null
                 && CurrentEditContext.Validate())
             {
-                IResponse<DynamicModel<T>> response;
-
-                if (DynamicModel.Id.Equals(0))
-                {
-                    response = null;
-                    // TODO: execute flow action on new model i.e. model Id is 0
-                    response = await DynamicApiRequest
-                        .AddDynamicModelAsync<T>(DynamicModel)
-                        .ConfigureAwait(false);
-                }
-                else
-                {
-                    response = null;
-                    // TODO: execute flow action on existing model
-                    response = await DynamicApiRequest
-                        .UpdateDynamicModelAsync<T>(DynamicModel)
-                        .ConfigureAwait(false);
-                }
+                var response = await DynamicApiRequest
+                    .FlowExecutionAsync<T>(DynamicModel)
+                    .ConfigureAwait(false);
 
                 GetResponse(response);
             }
