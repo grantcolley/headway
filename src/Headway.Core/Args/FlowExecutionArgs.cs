0d3825cab102ddc990af65ca83ff7290ff20b331
grantcolley <grantcolley@yahoo.com>
2023-06-18 18:19:18 +0100

    Create FlowExecutionArgs.cs

---
+++ src/Headway.Core/Args/FlowExecutionArgs.cs
@@ -0,0 +1,13 @@
+﻿using Headway.Core.Enums;
+
+namespace Headway.Core.Args
+{
+    public class FlowExecutionArgs
+    {
+        public FlowActionEnum FlowAction { get; set; }
+        public StateStatus StateStatus { get; set; }
+        public string StateCode { get; set; }
+        public string TargetStateCode { get; set; }
+        public string Comment { get; set; }
+    }
+}
