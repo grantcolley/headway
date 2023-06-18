0558195fd3fc7f9e6cffd51ddacc58f2b5b38516
grantcolley <grantcolley@yahoo.com>
2023-06-18 18:19:12 +0100

    Create FlowActionEnum.cs

---
+++ src/Headway.Core/Enums/FlowActionEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Headway.Core.Enums
+{
+    public enum FlowActionEnum
+    {
+        Unknown = 0,
+        Initialise,
+        Start,
+        TakeOwnership,
+        RelinquishOwnership,
+        Complete,
+        Reset
+    }
+}
