3f6de9ce3a5c21143cec6067f8e433c19ef085d5
grantcolley <grantcolley@yahoo.com>
2023-06-19 18:12:33 +0100

    Update FlowContextExtensions.cs

--- src/Headway.Core/Extensions/FlowContextExtensions.cs
+++ src/Headway.Core/Extensions/FlowContextExtensions.cs
@@ -1,10 +1,47 @@
-﻿using Headway.Core.Interface;
+﻿using Headway.Core.Enums;
+using Headway.Core.Interface;
 using System;
+using System.Linq;
 
 namespace Headway.Core.Extensions
 {
     public static class FlowContextExtensions
     {
+        public static bool IsStateReadOnly(this IFlowContext flowContext, string stateCode)
+        {
+            bool readOnly = false;
+
+            if (!string.IsNullOrWhiteSpace(stateCode))
+            {
+                var state = flowContext.Flow.StateDictionary[stateCode];
+
+                var flowHistory = flowContext.Flow.ReplayHistory.FirstOrDefault(f => f.StateCode.Equals(stateCode));
+
+                if (flowHistory == null
+                    && state.IsOwnerRestricted)
+                {
+                    readOnly = true;
+                }
+                else if (flowHistory != null)
+                {
+                    if (state.IsOwnerRestricted
+                        && flowHistory.StateStatus.Equals(StateStatus.InProgress)
+                        && !string.IsNullOrWhiteSpace(flowHistory.Owner)
+                        && flowHistory.Owner.Equals(flowContext.Authorisation.User))
+                    {
+                        readOnly = true;
+                    }
+                    else if (!state.IsOwnerRestricted
+                        && flowHistory.StateStatus.Equals(StateStatus.InProgress))
+                    {
+                        readOnly = true;
+                    }
+                }
+            }
+
+            return readOnly;
+        }
+
         public static IFlowContext Execute(this IFlowContext flowContext, IFlowContext target)
         {
             throw new NotImplementedException();
