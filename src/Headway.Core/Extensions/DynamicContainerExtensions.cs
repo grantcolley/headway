34cf98e1c0ec8d793d3953685f4af0964365aff7
grantcolley <grantcolley@yahoo.com>
2023-06-19 18:12:29 +0100

    Update DynamicContainerExtensions.cs

--- src/Headway.Core/Extensions/DynamicContainerExtensions.cs
+++ src/Headway.Core/Extensions/DynamicContainerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Headway.Core.Dynamic;
+﻿using Headway.Core.Constants;
+using Headway.Core.Dynamic;
 using Headway.Core.Helpers;
 using System.Collections.Generic;
 
@@ -12,6 +13,27 @@ namespace Headway.Core.Extensions
             dynamicArgs.AddRange(ComponentArgHelper.ExtractDynamicArgs(dynamicContainer.FlowArgs, dynamicFields));
 
             dynamicContainer.DynamicArgs.AddRange(dynamicArgs);
+        }
+
+        public static void ApplyStateReadOnlyFlag(this DynamicContainer dynamicContainer)
+        {
+            if (dynamicContainer.FlowContext != null)
+            {
+                var stateCode = dynamicContainer.DynamicArgs.FirstDynamicArgValueToStringOrDefault(FlowConstants.FLOW_STATE_CODE);
+
+                if (!string.IsNullOrWhiteSpace(stateCode))
+                {
+                    bool readOnly = dynamicContainer.FlowContext.IsStateReadOnly(stateCode);
+
+                    if (readOnly)
+                    {
+                        foreach (var field in dynamicContainer.DynamicFields)
+                        {
+                            field.ReadOnly = readOnly;
+                        }
+                    }
+                }
+            }
         }
     }
 }
