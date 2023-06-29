7574bacc9fefdb8a2b26b43f233f72cb6a02082a
grantcolley <grantcolley@yahoo.com>
2023-06-29 16:34:06 +0100

    Update FlowContextExtensions.cs

--- src/Headway.Core/Extensions/FlowContextExtensions.cs
+++ src/Headway.Core/Extensions/FlowContextExtensions.cs
@@ -1,19 +1,45 @@
-﻿using Headway.Core.Enums;
+﻿using Headway.Core.Args;
+using Headway.Core.Enums;
 using Headway.Core.Interface;
 using Headway.Core.Model;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Headway.Core.Extensions
 {
     public static class FlowContextExtensions
     {
-        public static IFlowContext Execute(this IFlowContext flowContext, IFlowContext target)
+        public static async Task ExecuteAsync(this IFlowContext flowContext, FlowExecutionArgs flowExecutionArgs)
         {
-            throw new NotImplementedException();
+            flowContext.Flow.Bootstrap(flowContext.GetFlowHistory());
+
+            switch(flowExecutionArgs.FlowAction)
+            {
+                case FlowActionEnum.Initialise:
+                    await flowContext.Flow.ActiveState.InitialiseAsync().ConfigureAwait(false);
+                    break;
+                case FlowActionEnum.Start:
+                    await flowContext.Flow.ActiveState.StartAsync().ConfigureAwait(false);
+                    break;
+                case FlowActionEnum.TakeOwnership:
+                    await flowContext.Flow.ActiveState.TakeOwnershipAsync(flowExecutionArgs.Authorisation).ConfigureAwait(false);
+                    break;
+                case FlowActionEnum.RelinquishOwnership:
+                    flowContext.Flow.ActiveState.RelinquishOwnership(flowExecutionArgs.Authorisation);
+                    break;
+                case FlowActionEnum.Complete:
+                    await flowContext.Flow.ActiveState.CompleteAsync().ConfigureAwait(false);
+                    break;
+                case FlowActionEnum.Reset:
+                    await flowContext.Flow.ActiveState.ResetAsync().ConfigureAwait(false);
+                    break;
+                default: 
+                    throw new ArgumentException(nameof(FlowActionEnum.Unknown));
+            }
         }
 
-        public static bool ValidateActiveStateReadOnly(this IFlowContext flowContext)
+        public static bool IsActiveStateForReadOnlyForValidator(this IFlowContext flowContext)
         {
             if (flowContext == null)
             {
@@ -25,12 +51,25 @@ namespace Headway.Core.Extensions
                 throw new ArgumentNullException(nameof(flowContext.Flow));
             }
 
-            if (flowContext.Flow?.ActiveState != null)
+            if (flowContext.Flow?.ActiveState == null)
             {
-                return flowContext.IsStateReadOnly(flowContext.Flow?.ActiveState);
+                flowContext.Flow.Bootstrap(flowContext.GetFlowHistory(), true);
             }
 
-            flowContext.Flow.Bootstrap(flowContext.GetFlowHistory(), true);
+            return flowContext.IsActiveStateReadOnly();
+        }
+
+        public static bool IsActiveStateReadOnly(this IFlowContext flowContext)
+        {
+            if (flowContext == null)
+            {
+                throw new ArgumentNullException(nameof(flowContext));
+            }
+
+            if (flowContext.Flow == null)
+            {
+                throw new ArgumentNullException(nameof(flowContext.Flow));
+            }
 
             return flowContext.IsStateReadOnly(flowContext.Flow?.ActiveState);
         }
