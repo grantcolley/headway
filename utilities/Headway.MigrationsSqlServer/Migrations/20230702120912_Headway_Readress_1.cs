f23a5922a8b3830b5d6e7f5167aad871ab3e8c71
grantcolley <grantcolley@yahoo.com>
2023-07-02 20:30:38 +0100

    Update SQL Server migrations

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230702120912_Headway_Readress_1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Headway_Readress_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Redresses_Programs_ProgramId",
+                table: "Redresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramId",
+                table: "Redresses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Redresses_Programs_ProgramId",
+                table: "Redresses",
+                column: "ProgramId",
+                principalTable: "Programs",
+                principalColumn: "ProgramId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Redresses_Programs_ProgramId",
+                table: "Redresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramId",
+                table: "Redresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Redresses_Programs_ProgramId",
+                table: "Redresses",
+                column: "ProgramId",
+                principalTable: "Programs",
+                principalColumn: "ProgramId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
