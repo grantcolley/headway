9cff89b410e53894ea51f77cf5bbb5e5ea3d71cf
grantcolley <grantcolley@yahoo.com>
2023-07-02 12:55:10 +0100

    Update IRepositoryFlowable.cs

--- src/Headway.Core/Interface/IRepositoryFlowable.cs
+++ src/Headway.Core/Interface/IRepositoryFlowable.cs
@@ -2,7 +2,7 @@
 
 namespace Headway.Core.Interface
 {
-    public interface IRepositoryFlowable<T> : IRepository
+    public interface IRepositoryFlowable<T> : IRepository where T : class, IFlowContext
     {
         Task<T> GetFlowContextAsync(int id);
         Task<T> UpdateFlowHistoryAsync(T model);
