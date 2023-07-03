745ae680c25bfe09f62327d3d12b2d66b411f6e1
grantcolley <grantcolley@yahoo.com>
2023-07-03 16:14:46 +0100

    Sql Server migrations

---
+++ utilities/Headway.MigrationsSqlServer/Migrations/20230703062109_Headway_Flow_1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headway.MigrationsSqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Headway_Flow_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "RedressFlowHistory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "RedressFlowHistory");
+        }
+    }
+}
